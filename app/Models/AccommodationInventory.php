0cdd30cfa19637b03ff4649b9aafcb4f5226823a
Ryan Catlin <ryan@octopustravelmatrix.com>
2020-10-15 16:04:12 +0100

    Model Logging Initialised

--- app/Models/AccommodationInventory.php
+++ app/Models/AccommodationInventory.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Jahondust\ModelLog\Traits\ModelLogging;
+
 
 class AccommodationInventory extends Model
 {
-    use HasFactory;
+    use HasFactory, ModelLogging;
 }
+    $logFields = ['accommodation_id','purchase_price'];
+
+
