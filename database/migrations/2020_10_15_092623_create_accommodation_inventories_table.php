54babba544f21b9db4c446f75a154ee048eba92d
Ryan Catlin <rycatlin62@gmail.com>
2020-10-15 10:36:25 +0100

    Initialised Accommodation-Inventory & migration

---
+++ database/migrations/2020_10_15_092623_create_accommodation_inventories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccommodationInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accommodation_inventories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('accommodation_id')->nullable();
+            $table->dateTime('check_in_date_time')->nullable();
+            $table->dateTime('check_out_date_time')->nullable();
+            $table->integer('room_type_id')->nullable();
+            $table->integer('board_type_id')->nullable();
+            $table->binary('fit_selectable')->nullable();
+            $table->integer('stock')->nullable();
+            $table->float('purchase_price', 10, 0)->nullable();
+            $table->float('sales_price', 10, 0)->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accommodation_inventories');
+    }
+}
