a6abba4f2828be5a7e1645a24641b28665d4c967
Ryan Catlin <rycatlin62@gmail.com>
2020-11-04 19:13:34 +0000

    Revert "Merge remote-tracking branch 'origin/main' into activity-tables"
    This reverts commit 6c245927a00b4385e26bea35b67b2e59c2a3b705.

--- database/migrations/2020_11_03_190142_create_accommodations_table.php
+++ database/migrations/2020_11_03_190142_create_accommodations_table.php
@@ -16,7 +16,7 @@ class CreateAccommodationsTable extends Migration
         Schema::create('accommodations', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('region_id');
-            $table->string('title', 160);
+            $table->string('title', 255)->index();
             $table->text('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
