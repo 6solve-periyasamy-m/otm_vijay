16d2280d2539a922c41cbe84b483976cd08beef6
Ryan Catlin <ryan@octopustravelmatrix.com>
2020-12-03 08:42:40 +0000

    Add order tables

---
+++ database/migrations/2020_12_03_084130_create_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id')->nullable();
+            $table->text('invoice_url')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->text('notes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
