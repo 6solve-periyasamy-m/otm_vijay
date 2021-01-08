7aa02d6558d4ef825bd9a0f53c889c335cb59862
JakeMT04 <jake@jakemt04.xyz>
2021-01-08 12:28:07 +0000

    Change message in footer

---
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="app-footer">
+    <div class="site-footer-right">
+        Powered by <a href="http://octopustravelmatrix.com" target="_blank">Octopus Travel Matrix</a>
+    </div>
+</footer>
