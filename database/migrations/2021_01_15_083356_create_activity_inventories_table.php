0d025e57f86d43a696e74ccff8328a400ca06a15
Ryan Catlin <ryan@octopustravelmatrix.com>
2021-01-15 08:36:04 +0000

    Add activity migrations

---
+++ database/migrations/2021_01_15_083356_create_activity_inventories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivityInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity_inventories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('activity_id')->nullable();
+            $table->dateTime('activity_start_date_time')->nullable();
+            $table->dateTime('activity_end_date_time')->nullable();
+            $table->tinyInteger('fit_selectable')->nullable();
+            $table->integer('ticket_type_id')->nullable();
+            $table->integer('stock')->nullable();
+            $table->double('purchase_price')->nullable();
+            $table->double('sales_price')->nullable();
+            $table->string('currency', 255)->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activity_inventories');
+    }
+}
