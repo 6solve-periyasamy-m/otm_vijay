cbdf163068fe7b75c2f01b55f7c74ad73121fe0f
Ryan Catlin <ryan@octopustravelmatrix.com>
2021-01-15 08:53:12 +0000

    Update migrations

--- database/migrations/2021_01_15_083356_create_activity_types_table.php
+++ database/migrations/2021_01_15_083356_create_activity_types_table.php
@@ -15,7 +15,7 @@ class CreateActivityTypesTable extends Migration
     {
         Schema::create('activity_types', function (Blueprint $table) {
             $table->increments('id');
-            $table->text('activity_type_title')->index();
+            $table->text('activity_type_title');
             $table->timestamps();
             $table->softDeletes();
         });
