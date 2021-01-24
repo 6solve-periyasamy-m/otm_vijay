c4eea2d8b33a7006e68a84ce7f39cbdad7abc22b
Nicholas Alexander <nalex@sfsw.net>
2021-01-24 14:31:41 +0000

    [bookings] add Quote model, add relationships to connect orders to its dependent tables to assist in queries

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-
 class Order extends Model
 {
-    
+    public function quote() 
+    {
+        return $this->hasOne(Quote::class);
+    }
+
+    public function tour()
+    {
+        return $this->hasOne(Tour::class);
+    }
+
+    public function orderStatus()
+    {
+        return $this->hasOne(OrderStatus::class);
+    }
 }
