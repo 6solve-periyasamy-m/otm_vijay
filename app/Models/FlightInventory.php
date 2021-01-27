fb107a75f104dc4c765720b2f3e90835ee623305
Nicholas Alexander <nalex@sfsw.net>
2021-01-27 22:53:54 +0000

    [admin-fixes] migration to add flight times to flights table

--- app/Models/FlightInventory.php
+++ app/Models/FlightInventory.php
@@ -41,12 +41,12 @@ class FlightInventory extends Model
 
         // Because the date and time are in seperate variables, I can't have these Carbon objects be dynamically created by the model
         // the same way as I've done the others. This one will have to stay this way.
-	// DEBUG var_dump(Carbon::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s'))->format('d/m/Y H:i'));dd();
-	//
-	$departure_date = Carbon::createFromFormat('Y-m-d H:i:s', $this->flight->departure_date.' '.$this->flight->departure_time)->format('d/m/Y H:i');
- 	$arrival_date = Carbon::createFromFormat('Y-m-d H:i:s', $this->flight->arrival_date.' '.$this->flight->arrival_time)->format('d/m/Y H:i');
+        // DEBUG var_dump(Carbon::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s'))->format('d/m/Y H:i'));dd();
+        //
+        $departure_date = Carbon::createFromFormat('Y-m-d H:i:s', $this->flight->departure_date.' '.$this->flight->departure_time)->format('d/m/Y H:i');
+        $arrival_date = Carbon::createFromFormat('Y-m-d H:i:s', $this->flight->arrival_date.' '.$this->flight->arrival_time)->format('d/m/Y H:i');
 
-	//print '<code>';var_dump($this->arrival_time);dd();
+        //print '<code>';var_dump($this->arrival_time);dd();
 
        return "{$this->flight->airline->airline_name}｜Departs from: {$departure_airport->location->location_name} - Arrives at: {$arrival_airport->location->location_name}｜Departs: {$departure_date} - Arrives: {$arrival_date}｜Travel Class: {$this->travelClass->title}";
     }
