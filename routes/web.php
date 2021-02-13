1715d01178e50de62bd45e57cd51b1f1dbceebdb
Nicholas Alexander <nalex@sfsw.net>
2021-02-13 10:55:04 +0000

    [backend] upate routes and README for api paths

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TourController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Auth\LoginController;
+// use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\OrderCustomerController;
 use App\Http\Controllers\BookingController;
 
