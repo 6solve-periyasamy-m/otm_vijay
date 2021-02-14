1515eca5bc4799ebbf1bfc6d1445c0719c89facb
Nicholas Alexander <nalex@sfsw.net>
2021-02-14 10:28:50 +0000

    [payment-schedule] add in other sections to check

--- resources/assets/js/components/BookingForm.vue
+++ resources/assets/js/components/BookingForm.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card card-default">
                     <div class="card-header">Booking Form</div>
 
@@ -11,6 +11,9 @@
                             <tour-menu></tour-menu>
                             <bookingform-lead></bookingform-lead>
                             <bookingform-additional></bookingform-additional>
+                            <tour-menu></tour-menu>
+                            <finance-menu></finance-menu>
+                            <extras-menu></extras-menu>
                             <bookingform-terms></bookingform-terms>
                         <bookingform-footer></bookingform-footer>
                     </div>
@@ -22,7 +25,9 @@
 
 <script>
 import DonutMenu from './donut-menu.vue'
+import financeMenu from './finance-menu.vue'
     export default {
+  components: { financeMenu },
         mounted() {
             console.log('BookingForm mounted.')
         }
