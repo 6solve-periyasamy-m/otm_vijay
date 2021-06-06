978a103fa4a3e7148014fd2fd03401f58bdc8e1f
Nicholas Alexander <nalex@sfsw.net>
2021-06-06 16:52:23 +0000

    [bfo] debugging routes

--- resources/views/otm.blade.php
+++ resources/views/otm.blade.php
@@ -4,7 +4,7 @@
     <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
         <div class="grid grid-cols-1 md:grid-cols-2">
             <div class="p-6">
-                <div class="flex">
+                <div class="flex" style="max-width: 40px;">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
                     <div class="ml-4 text-lg leading-7 font-semibold"><a href="/documentation" class="underline text-gray-900 dark:text-white">Documentation</a></div>
                 </div>
@@ -38,6 +38,10 @@
                                     <dd class="definition__body"><a target="newtab" href="/booking">Booking Form</a> - customer selects event/tour on the form.</dd>
                                     <dd class="definition__body"><a target="newtab" href="/booking/tour/example-tour">Flexible Tour Bookings</a> - specific tour URL to list on your site, or email to customers for a specific tour.</dd>
                                     <dd class="definition__body"><a target="newtab" href="/booking/order-number">Easy Tour Booking Form Secure Revisit</a> - a unique single-use URL that allows your customer to securely access their booking.</dd>
+                                    <dd class="definition__body"><a target="newtab" href="/booking/world-cup">Booking Form</a> tour for world-cup event</dd>
+                                    <dd class="definition__body"><a target="newtab" href="/booking/check/tour/1">Check Tour for event 1</a> </dd>
+                                    <dd class="definition__body"><a target="newtab" href="/booking/check/events">Check events</a></dd>
+
                                 </dd>
                         </dl>
                     </div>
@@ -45,7 +49,7 @@
             </div>
 
             <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-                <div class="flex items-center">
+                <div class="flex items-center"  style="max-width: 40px;">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                     <div class="ml-4 text-lg leading-7 font-semibold">
                         <a href="/video" class="underline text-gray-900 dark:text-white">
@@ -66,7 +70,7 @@
             </div>
 
             <div class="p-6 border-t border-gray-200 dark:border-gray-700">
-                <div class="flex items-center">
+                <div class="flex items-center" style="max-width: 40px;">>
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
                     <div class="ml-4 text-lg leading-7 font-semibold">
                         <a href="/news" class="underline text-gray-900 dark:text-white">News</a>
@@ -81,7 +85,7 @@
             </div>
 
             <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
-                <div class="flex items-center">
+                <div class="flex items-center" style="max-width: 40px;">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                     <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Testimonials</div>
                 </div>
@@ -97,7 +101,7 @@
 
     <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
         <div class="text-center text-sm text-gray-500 sm:text-left">
-            <div class="flex items-center">
+            <div class="flex items-center" style="max-width: 40px;">
 
                 <a href="">
                     <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
