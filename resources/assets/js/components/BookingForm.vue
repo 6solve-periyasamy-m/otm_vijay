c0a8efc6a7f5c23ea91904cf332942c3131c757b
Nicholas Alexander <nalex@sfsw.net>
2021-07-07 23:29:51 +0000

    [bfo-accom] version patch

--- resources/assets/js/components/BookingForm.vue
+++ resources/assets/js/components/BookingForm.vue
@@ -4,7 +4,7 @@
             <div class="col-md-12">
                 <div class="card card-default">
                     <div class="card-header">
-                        OTM Booking Form version 0.4.1 PRERELEASE - Lead/Additionals/Flights/Accommodation
+                        OTM Booking Form version 0.4.2 PRERELEASE - Lead/Additionals/Flights/Accommodation
                         <button class="btn btn-small btn-themed default" @click="changeTheme('')">None</button>
                         <button class="btn btn-small btn-themed cool" @click="changeTheme('cool')">Cool</button>
                         <button class="btn btn-small btn-themed warm" @click="changeTheme('warm')">Warm</button>
