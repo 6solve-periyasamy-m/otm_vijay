1851adf9951b63923c0f985f9c510986c55449d2
Celeste Gateley <celeste@octopustravelmatrix.com>
2021-08-23 13:29:05 +0100

    Re-ordered fields in accommodation inventory tours table

--- database/migrations/2020_12_23_114623_create_accommodation_inventory_tours_table.php
+++ database/migrations/2020_12_23_114623_create_accommodation_inventory_tours_table.php
@@ -14,14 +14,14 @@ class CreateAccommodationInventoryToursTable extends Migration
     public function up()
     {
         Schema::create('accommodation_inventory_tours', function (Blueprint $table) {
+            $table->integer('id', false, true);
             $table->integer('tour_id');
             $table->integer('accommodation_inventory_id');
-            $table->timestamps();
-            $table->softDeletes();
             $table->float('sales_price', 10, 0)->nullable();
             $table->char('tour_component_type')->nullable();
-            $table->integer('id', false, true);
             $table->string('booking_policy', 12);
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
