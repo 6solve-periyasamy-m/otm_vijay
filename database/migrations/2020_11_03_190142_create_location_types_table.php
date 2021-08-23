57ec49c6f2be7e3aa9cb89a5d19db159d0f369aa
Celeste Gateley <celeste@octopustravelmatrix.com>
2021-08-23 14:35:11 +0100

    Removed unique from location_type_name as it caused errors during migration

--- database/migrations/2020_11_03_190142_create_location_types_table.php
+++ database/migrations/2020_11_03_190142_create_location_types_table.php
@@ -15,7 +15,7 @@ class CreateLocationTypesTable extends Migration
     {
         Schema::create('location_types', function (Blueprint $table) {
             $table->increments('id');
-            $table->text('location_type_name')->unique();
+            $table->text('location_type_name');
             $table->timestamps();
             $table->softDeletes();
         });
