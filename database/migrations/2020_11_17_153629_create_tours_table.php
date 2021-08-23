c9df7ed8c09db40d7790985c27ce76088054c7c6
Celeste Gateley <celeste@octopustravelmatrix.com>
2021-08-23 14:25:16 +0100

    Changed stock_control_active to a boolean which defaults to true in tours table

--- database/migrations/2020_11_17_153629_create_tours_table.php
+++ database/migrations/2020_11_17_153629_create_tours_table.php
@@ -22,7 +22,7 @@ class CreateToursTable extends Migration
             $table->float('base_price_per_person', 10, 0)->nullable();
             $table->float('margin', 10, 0)->nullable();
             $table->float('single_occupancy_surcharge', 10, 0)->nullable();
-            $table->integer('stock_control_active')->nullable();
+            $table->boolean('stock_control_active')->default(true);
             $table->integer('stock')->nullable();
             $table->string('booking_form_url', 255)->nullable();
             $table->integer('tour_colour_id')->nullable();
