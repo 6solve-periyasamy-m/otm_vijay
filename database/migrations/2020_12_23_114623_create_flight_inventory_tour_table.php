0eca5a7d451c209ffcc38e5aebb7901d75f48e1f
Celeste Gateley <celeste@octopustravelmatrix.com>
2021-08-23 13:32:29 +0100

    Re-ordered fields in flight inventory tour table

--- database/migrations/2020_12_23_114623_create_flight_inventory_tour_table.php
+++ database/migrations/2020_12_23_114623_create_flight_inventory_tour_table.php
@@ -14,14 +14,14 @@ class CreateFlightInventoryTourTable extends Migration
     public function up()
     {
         Schema::create('flight_inventory_tour', function (Blueprint $table) {
+            $table->integer('id', true);
             $table->integer('tour_id');
             $table->integer('flight_inventory_id');
-            $table->timestamps();
-            $table->softDeletes();
             $table->integer('tour_component_type_id')->nullable();
-            $table->integer('id', true);
             $table->float('sales_price', 10, 0)->nullable();
             $table->string('flight_type', 20)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
