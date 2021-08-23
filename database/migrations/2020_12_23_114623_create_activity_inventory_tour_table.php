e9b4b01661345870efb5345c5cd15317fbc208c7
Celeste Gateley <celeste@octopustravelmatrix.com>
2021-08-23 13:31:56 +0100

    Updated ID to use standard increments definition in activity inventory tour table

--- database/migrations/2020_12_23_114623_create_activity_inventory_tour_table.php
+++ database/migrations/2020_12_23_114623_create_activity_inventory_tour_table.php
@@ -14,7 +14,7 @@ class CreateActivityInventoryTourTable extends Migration
     public function up()
     {
         Schema::create('activity_inventory_tour', function (Blueprint $table) {
-            $table->integer('id', true);
+            $table->increments('id');
             $table->integer('tour_id');
             $table->integer('activity_inventory_id');
             $table->integer('tour_component_type')->nullable();
