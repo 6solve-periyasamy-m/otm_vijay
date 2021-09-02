a492ed8acd8eb1eab48fdd2b093449c13ee62b6f
Nicholas Alexander <nalex@sfsw.net>
2021-09-02 05:58:23 +0000

    [bf02] make customer authenticatable

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -303,6 +303,8 @@ class BookingController extends ApiController
 
     /**
      * storeOrUpdateCustomer
+     * NB: customer table as a password field, this is not changed or affected by this function
+     * as it is not used UNTIL the customer has a login (this may be managed by the user table)
      *
      * @param [type] $request
      * @param boolean $isLead
