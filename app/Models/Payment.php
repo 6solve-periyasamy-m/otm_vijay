8fd673bee3dbf991b7779808b8ee2f7cf7ca549e
Celeste Gateley <celeste@octopustravelmatrix.com>
2021-09-06 14:52:22 +0100

    Added relationships to payment table

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -9,4 +9,12 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Payment extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function order() {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
+
+    public function paymentMethod() {
+        return $this->belongsTo(PaymentMethod::class, 'payment_method_id');
+    }
 }
