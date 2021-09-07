0313b7705c0c0c2265ad11f2366cba8f38be2e22
Celeste Gateley <celeste@octopustravelmatrix.com>
2021-09-07 09:44:51 +0100

    Added seeder for customer table

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Customer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->title,
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'date_of_birth' => $this->faker->date,
+            'mobile_number' => $this->faker->phoneNumber,
+            'email_address' => $this->faker->email,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+            'gender' => ['male', 'female', 'other'][array_rand(['male', 'female', 'other'])],
+            'address_line_1' => $this->faker->streetAddress,
+            'postcode' => $this->faker->postcode,
+            'same_address' => true,
+        ];
+    }
+}
