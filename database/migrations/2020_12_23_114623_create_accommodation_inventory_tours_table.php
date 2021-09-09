f92c89a8f7a25a397843a0d51027ee1ef719bbd1
Celeste Gateley <celeste@octopustravelmatrix.com>
2021-09-09 10:58:11 +0100

    Fixed potential method not found when migrating, caused by varchar method

--- database/migrations/2020_12_23_114623_create_accommodation_inventory_tours_table.php
+++ database/migrations/2020_12_23_114623_create_accommodation_inventory_tours_table.php
@@ -19,7 +19,7 @@ class CreateAccommodationInventoryToursTable extends Migration
             $table->integer('accommodation_inventory_id');
             $table->float('sales_price', 10, 0)->nullable();
             $table->enum('tour_component_type', ['Included', 'Add-on', 'Upgrade'])->default('Included');
-            $table->varchar('booking_policy',12)->default('overbook');
+            $table->string('booking_policy',12)->default('overbook');
             $table->timestamps();
             $table->softDeletes();
         });
