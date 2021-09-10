5b9a09d3d09777a5dcf1f05040b31f7d2c5d214c
Celeste Gateley <celeste@octopustravelmatrix.com>
2021-09-10 14:43:45 +0100

    Added method to order repository to get customer details

--- app/Repository/OrderRepository.php
+++ app/Repository/OrderRepository.php
@@ -3,11 +3,13 @@
 namespace App\Repository;
 
 use App\Models\Order;
+use App\Models\OrdersCustomer;
 use Illuminate\Support\Facades\DB;
 
 interface OrderRepositoryInterface {
     public static function getSearchOrders($searchTerm = "", $archived = false);
     public static function getOrderDetails($orderId);
+    public static function getOrderCustomerDetails($id);
 
 }
 
@@ -58,35 +60,35 @@ class OrderRepository implements OrderRepositoryInterface
         foreach ($order->orderCustomers as $customer) {
             $customers[] = $customer;
             // Possible TODO: Find a more elegant way to do this?
-            foreach($customer->orderAccommodation as $orderAccommodation) {
+            foreach ($customer->orderAccommodation as $orderAccommodation) {
                 if ($orderAccommodation->accommodationInventoryTour->tour_component_type == OrderRepository::$addonId) {
                     $addons[] = $orderAccommodation->accommodationInventoryTour;
                     $totalOrderValue += $orderAccommodation->accommodationInventoryTour->sales_price;
                 }
             }
-            foreach($customer->orderActivities as $orderActivity) {
+            foreach ($customer->orderActivities as $orderActivity) {
                 if ($orderActivity->activityInventoryTour->tour_component_type == OrderRepository::$addonId) {
                     $addons[] = $orderActivity->activityInventoryTour;
                     $totalOrderValue += $orderActivity->activityInventoryTour->sales_price;
                 }
             }
-            foreach($customer->orderFlights as $orderFlight) {
+            foreach ($customer->orderFlights as $orderFlight) {
                 if ($orderFlight->flightInventoryTour->tour_component_type == OrderRepository::$addonId) {
                     $addons[] = $orderFlight->flightInventoryTour;
                     $totalOrderValue += $orderFlight->flightInventoryTour->sales_price;
                 }
             }
-            foreach($customer->orderTransports as $orderTransport) {
+            foreach ($customer->orderTransports as $orderTransport) {
                 if ($orderTransport->transportInventoryTour->tour_component_type == OrderRepository::$addonId) {
                     $addons[] = $orderTransport->transportInventoryTour;
                     $totalOrderValue += $orderTransport->transportInventoryTour->sales_price;
                 }
             }
-            foreach($customer->adjustments as $adjustment) {
+            foreach ($customer->adjustments as $adjustment) {
                 $totalOrderValue += $adjustment->amount;
             }
         }
-        foreach($order->adjustments as $adjustment) {
+        foreach ($order->adjustments as $adjustment) {
             $totalOrderValue += $adjustment->amount;
         }
         $details['customers'] = $customers;
@@ -94,13 +96,64 @@ class OrderRepository implements OrderRepositoryInterface
         $details['totalOrderValue'] = $totalOrderValue;
         $payments = [];
         $totalPaid = 0;
-        foreach($order->payments as $payment) {
+        foreach ($order->payments as $payment) {
             $payments[] = $payment;
             $totalPaid += $payment->amount;
         }
 
         $details['totalPaid'] = $totalPaid;
         $details['payments'] = $payments;
+        return $details;
+    }
+
+    public static function getOrderCustomerDetails($id)
+    {
+        $orderCustomer = OrdersCustomer::findOrFail($id);
+        $details = ['order-customer' => $orderCustomer, 'customer' => $orderCustomer->customer, 'order' => $orderCustomer->order,];
+        $accommodationArr = [];
+        foreach ($orderCustomer->orderAccommodation as $orderAccommodation) {
+            $data = [];
+            $data['order'] = $orderAccommodation;
+            $data['tour'] = $orderAccommodation->accommodationInventoryTour;
+            $data['inventory'] = $orderAccommodation->accommodationInventory;
+            $data['component'] = $orderAccommodation->accommodation;
+            $accommodationArr[] = $data;
+        }
+        $details['accommodation'] = $accommodationArr;
+
+        $activities = [];
+        foreach ($orderCustomer->orderActivities as $orderActivity) {
+            $data = [];
+            $data['order'] = $orderActivity;
+            $data['tour'] = $orderActivity->activityInventoryTour;
+            $data['inventory'] = $orderActivity->activityInventory;
+            $data['component'] = $orderActivity->activity;
+            $activities[] = $data;
+        }
+        $details['activities'] = $activities;
+
+        $flights = [];
+        foreach ($orderCustomer->orderflights as $orderFlight) {
+            $data = [];
+            $data['order'] = $orderFlight;
+            $data['tour'] = $orderFlight->flightInventoryTour;
+            $data['inventory'] = $orderFlight->flightInventory;
+            $data['component'] = $orderFlight->flight;
+            $flights[] = $data;
+        }
+        $details['flights'] = $flights;
+
+        $transports = [];
+        foreach ($orderCustomer->ordertransports as $orderTransport) {
+            $data = [];
+            $data['order'] = $orderTransport;
+            $data['tour'] = $orderTransport->transportInventoryTour;
+            $data['inventory'] = $orderTransport->transportInventory;
+            $data['component'] = $orderTransport->transport;
+            $transports[] = $data;
+        }
+        $details['transports'] = $transports;
+
         return $details;
     }
 }
