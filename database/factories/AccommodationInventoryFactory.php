d82ea12352e0018567b7610c836218134aa98114
Celeste Gateley <celeste@octopustravelmatrix.com>
2021-09-10 10:05:53 +0100

    Added accommodation inventory factory

---
+++ database/factories/AccommodationInventoryFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AccommodationInventory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AccommodationInventoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = AccommodationInventory::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'board_type' => $this->faker->numberBetween(1, 4),
+            'room_type' => $this->faker->numberBetween(1, 3),
+            'check_in_date_time' => now(),
+            'checkin_confirmed' => true,
+            'check_out_date_time' => now(),
+            'checkout_confirmed' => false,
+            'fit_selectable' => true,
+            'stock' => $this->faker->numberBetween(1, 10),
+            'purchase_price' => $this->faker->numberBetween(10, 50),
+            'sales_price' => $this->faker->numberBetween(50, 100),
+            'notes' => $this->faker->sentence,
+        ];
+    }
+}
