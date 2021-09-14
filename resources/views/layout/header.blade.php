e297c7b191b405635558f0d8a2766682d040c918
Celeste Gateley <celeste.gateley@gmail.com>
2021-09-14 12:13:48 +0100

    Adjusted styling on order customer screen

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -13,5 +13,9 @@
     <link href="{{ asset('/css/mdb.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/app.css?v=').time()}}" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.6.0/dt-1.11.2/fh-3.1.9/r-2.2.9/sl-1.3.3/datatables.min.css"/>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.6.0/dt-1.11.2/fh-3.1.9/r-2.2.9/sl-1.3.3/datatables.min.js"></script>
 </head>
 <body>
--- resources/views/orders/customer.blade.php
+++ resources/views/orders/customer.blade.php
@@ -1,32 +1,64 @@
 @extends('layout.main')
 
 @section('content')
-<script type="text/javascript">
-function hide(table) {
-    let element = document.getElementById(table + "-table");
-    if (element.style.display === "none") {
-        element.style.display = "block";
-    } else {
-        element.style.display = "none";
-    }
+<style>
+.header-cell {
+    border: 1px solid black !important;
+    font-size: 24px;
 }
+.customer-details {
+    min-width: 40%;
+    max-width: 40%;
+    display:inline-block;
+    vertical-align: bottom;
+}
+.header {
+    max-width: 20%;
+    font-size: 24px;
+    border: 1px solid black;
+    border-top: 0;
+    border-left: 0;
+    margin-bottom: 2px;
+}
+.select {
+    min-width: 95%;
+    max-width: 95%;
+    display: inline-block;
+}
+.add-btn {
+    min-width: 4%;
+    max-width: 4%;
+    display: inline-block;
+}
+.new-section {
+    border-bottom: 1px solid black;
+    margin-bottom: 5px;
+}
+</style>
+<script>
+    $(document).ready( function () {
+        $('#accommodation-table').DataTable({fixedHeader: true});
+        $('#activities-table').DataTable({fixedHeader: true});
+        $('#flights-table').DataTable({fixedHeader: true});
+        $('#transports-table').DataTable({fixedHeader: true});
+    } );
 </script>
 <div style="padding-left: 5%; padding-right: 5%; padding-top: 0.1%;">
 <div class="text-dark" style="padding: 1% 100px; border: 5px solid black; border-radius: 25px;">
     {{-- Header Details --}}
     <div id="header-details">
-        <table class="table" style="border-bottom: 1px solid black; font-size: 32px">
+        <table class="table" style="font-size: 32px">
             <tr>
-                <td style="border: 1px solid black; font-size: 24px">{{ $order->booking_reference }}</td>
-                <td style="border: 1px solid black; font-size: 24px">{{ $order->tour->title }}</td>
-                <td style="border: 1px solid black; font-size: 24px">{{ $order->tour->date_from . " to " . $order->tour->date_to }}</td>
-                <td style="border: 1px solid black; background-color: {{ true ? "#33ff99" : "#ffff99" }}; font-size: 24px">{{ true ? "Paid in Full" : "Balance Outstanding" }}</td>
+                <td class="header-cell">{{ $order->booking_reference }}</td>
+                <td class="header-cell">{{ $order->tour->title }}</td>
+                <td class="header-cell">{{ $order->tour->date_from . " to " . $order->tour->date_to }}</td>
+                <td class="header-cell" style="background-color: {{ true ? "#33ff99" : "#ffff99" }}">{{ true ? "Paid in Full" : "Balance Outstanding" }}</td>
             </tr>
         </table>
     </div>
     {{-- Customer Details Section --}}
     <div id="section-1" style="margin-bottom: 5px; border: 1px solid black;">
-        <table class="table table-striped" style="min-width: 40%; max-width: 40%; display:inline-block; vertical-align: bottom; horiz-align: left">
+        <table class="table table-striped customer-details" style="horiz-align: left">
             <thead>
             <tr>
                 <th scope="col">Detail</th>
@@ -50,7 +82,7 @@ function hide(table) {
                 <td>{{ $customer->passport_expiry_date }}</td>
             </tr>
         </table>
-        <table class="table table-striped" style="min-width: 40%; max-width: 40%; display:inline-block; vertical-align: bottom; horiz-align: right">
+        <table class="table table-striped customer-details" style="horiz-align: right">
             <thead>
                 <tr>
                     <th scope="col"></th>
@@ -80,118 +112,168 @@ function hide(table) {
             </tr>
         </table>
     </div>
+    {{-- Components Section --}}
     <div id="billing-section" style="border: 1px solid black">
-        <div id="billing-header" style="max-width: 20%; font-size: 24px; border: 1px solid black; border-top: 0; border-left: 0; margin-bottom: 2px;">
+        <div id="billing-header" class="header">
             Tour Components
         </div>
-        {{-- Accommodation Table --}}
-        <div id="components-section">
-            <div id="accommodation-details" style="min-width: 60%; max-width: 60%; display: inline-block; vertical-align: bottom;">
-                <div id="accommodation-header" style="max-width: 35%; font-size: 24px; border: 1px solid black; border-bottom: 0;">
-                    <a href="javascript:hide('accommodation');" class="link-secondary">Accommodations</a>
+        {{-- Tabs Definition --}}
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#accommodation">Accommodation</button>
+            </li>
+            <li class="nav-item">
+                <button class="nav-link" data-bs-toggle="tab" data-bs-target="#activities">Activities</button>
+            </li>
+            <li class="nav-item">
+                <button class="nav-link" data-bs-toggle="tab" data-bs-target="#flights">Flights</button>
+            </li>
+            <li class="nav-item">
+                <button class="nav-link" data-bs-toggle="tab" data-bs-target="#transports">Transports</button>
+            </li>
+        </ul>
+        {{-- Tables Definition --}}
+        <div id="tables" class="tab-content" style="padding: 5px">
+            {{-- Accommodation Table --}}
+            <div id="accommodation" role="tabpanel" class="tab-pane fade show active">
+                <div id="accommodation-new" class="new-section">
+                    <select id="accommodation-select" class="form-select form-select-lg select">
+                        <option selected>Please choose an option</option>
+                        <option value="1">Addon 1</option>
+                        <option value="2">Addon 2</option>
+                        <option value="3">Addon 3</option>
+                    </select>
+                    <button class="btn btn-success add-btn">Add</button>{{-- TODO: Implement --}}
                 </div>
-                <table id="accommodation-table" class="table table-striped" style="min-width: 100%; margin-bottom: 1px; border: 1px solid black">
-                    <thead>
-                    <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Room Type</th>
-                        <th scope="col">Shared With</th>
-                        <th scope="col">Component Type</th>
-                    </tr>
-                    </thead>
-                    @foreach($accommodation as $accommodationEntry)
-                        <tr>
-                            <td>{{ $accommodationEntry["inventory"]->check_in_date_time }} to {{ $accommodationEntry["inventory"]->check_out_date_time }}</td>
-                            <td>{{ $accommodationEntry["component"]->title }}</td>
-                            <td>{{ $accommodationEntry["inventory"]->roomType->room_type_name }}</td>
-                            <td>TBI</td> {{-- TODO: Discuss and Implement--}}
-                            <td>{{ $accommodationEntry["tour"]->tour_component_type }}</td>
-                        </tr>
-                    @endforeach
-                </table>
+                <div id="accommodation-details">
+                    <table id="accommodation-table" class="table table-striped table-responsive-sm">
+                            <thead>
+                            <tr>
+                                <th scope="col">Date</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Room Type</th>
+                                <th scope="col">Shared With</th>
+                                <th scope="col">Component Type</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                            </thead>
+                            @foreach($accommodation as $accommodationEntry)
+                                <tr>
+                                    <td>{{ $accommodationEntry["inventory"]->check_in_date_time }} to {{ $accommodationEntry["inventory"]->check_out_date_time }}</td>
+                                    <td>{{ $accommodationEntry["component"]->title }}</td>
+                                    <td>{{ $accommodationEntry["inventory"]->roomType->room_type_name }}</td>
+                                    <td>TBI</td> {{-- TODO: Discuss and Implement--}}
+                                    <td>{{ $accommodationEntry["tour"]->tour_component_type }}</td>
+                                    <td><a href="#" class="btn btn-danger">Delete</a></td>{{-- TODO: Implement --}}
+                                </tr>
+                            @endforeach
+                        </table>
+                </div>
             </div>
-        </div>
-        {{-- Activities Table --}}
-        <div id="components-section">
-            <div id="activities-details" style="min-width: 60%; max-width: 60%; display: inline-block; vertical-align: bottom;">
-                <div id="activities-header" style="max-width: 35%; font-size: 24px; border: 1px solid black; border-bottom: 0;">
-                    <a href="javascript:hide('activities');" class="link-secondary">Activities</a>
+            {{-- Activities Table --}}
+            <div id="activities" role="tabpanel" class="tab-pane fade">
+                <div id="activities-new" class="new-section">
+                    <select id="activities-select" class="form-select form-select-lg select">
+                        <option selected>Please choose an option</option>
+                        <option value="1">Addon 1</option>
+                        <option value="2">Addon 2</option>
+                        <option value="3">Addon 3</option>
+                    </select>
+                    <button class="btn btn-success add-btn">Add</button>{{-- TODO: Implement --}}
                 </div>
-                <table id="activities-table" class="table table-striped" style="min-width: 100%; margin-bottom: 1px; border: 1px solid black">
-                    <thead>
-                    <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Activity Type</th>
-                        <th scope="col">Component Type</th>
-                    </tr>
-                    </thead>
-                    @foreach($activities as $activity)
+                <div id="activities-details">
+                    <table id="activities-table" class="table table-striped table-responsive-sm">
+                        <thead>
                         <tr>
-                            <td>{{ $activity["inventory"]->activity_start_date_time }} to {{ $activity["inventory"]->activity_end_date_time }}</td>
-                            <td>{{ $activity["component"]->title }}</td>
-                            <td>{{ $activity["component"]->activityType->activity_type_title }}</td>
-                            <td>{{ $activity["tour"]->tour_component_type }}</td>
+                            <th scope="col">Date</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Activity Type</th>
+                            <th scope="col">Component Type</th>
+                            <th scope="col">Actions</th>
                         </tr>
-                    @endforeach
-                </table>
+                        </thead>
+                        @foreach($activities as $activity)
+                            <tr>
+                                <td>{{ $activity["inventory"]->activity_start_date_time }} to {{ $activity["inventory"]->activity_end_date_time }}</td>
+                                <td>{{ $activity["component"]->title }}</td>
+                                <td>{{ $activity["component"]->activityType->activity_type_title }}</td>
+                                <td>{{ $activity["tour"]->tour_component_type }}</td>
+                                <td><a href="#" class="btn btn-danger">Delete</a></td>{{-- TODO: Implement --}}
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
             </div>
-        </div>
-        {{-- Flights Table --}}
-        <div id="components-section">
-            <div id="activities-details" style="min-width: 60%; max-width: 60%; display: inline-block; vertical-align: bottom;">
-                <div id="activities-header" style="max-width: 35%; font-size: 24px; border: 1px solid black; border-bottom: 0;">
-                    <a href="javascript:hide('flights');" class="link-secondary">Flights</a>
+            {{-- Flights Table --}}
+            <div id="flights" role="tabpanel" class="tab-pane fade">
+                <div id="flights-new" class="new-section">
+                    <select id="flights-select" class="form-select form-select-lg select">
+                        <option selected>Please choose an option</option>
+                        <option value="1">Addon 1</option>
+                        <option value="2">Addon 2</option>
+                        <option value="3">Addon 3</option>
+                    </select>
+                    <button class="btn btn-success add-btn">Add</button>{{-- TODO: Implement --}}
                 </div>
-                <table id="flights-table" class="table table-striped" style="min-width: 100%; margin-bottom: 1px; border: 1px solid black">
-                    <thead>
-                    <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Travel Class</th>
-                        <th scope="col">Component Type</th>
-                    </tr>
-                    </thead>
-                    @foreach($flights as $flight)
+                <div id="flights-details">
+                    <table id="flights-table" class="table table-striped table-responsive-sm">
+                        <thead>
                         <tr>
-                            <td>{{ $flight["inventory"]->departure_date_time }} to {{ $flight["inventory"]->arrival_date_time }}</td>
-                            <td>{{ $flight["inventory"]->flight_number }}</td>
-                            <td>{{ $flight["inventory"]->travelClass->title }}</td>
-                            <td>{{ $flight["tour"]->tour_component_type }}</td>
+                            <th scope="col">Date</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Travel Class</th>
+                            <th scope="col">Component Type</th>
+                            <th scope="col">Actions</th>
                         </tr>
-                    @endforeach
-                </table>
+                        </thead>
+                        @foreach($flights as $flight)
+                            <tr>
+                                <td>{{ $flight["inventory"]->departure_date_time }} to {{ $flight["inventory"]->arrival_date_time }}</td>
+                                <td>{{ $flight["inventory"]->flight_number }}</td>
+                                <td>{{ $flight["inventory"]->travelClass->title }}</td>
+                                <td>{{ $flight["tour"]->tour_component_type }}</td>
+                                <td><a href="#" class="btn btn-danger">Delete</a></td>{{-- TODO: Implement --}}
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
             </div>
-        </div>
-        {{-- Transports Table --}}
-        <div id="components-section">
-            <div id="activities-details" style="min-width: 60%; max-width: 60%; display: inline-block; vertical-align: bottom;">
-                <div id="activities-header" style="max-width: 35%; font-size: 24px; border: 1px solid black; border-bottom: 0;">
-                    <a href="javascript:hide('transports');" class="link-secondary">Transports</a>
+            {{-- Transports Table --}}
+            <div id="transports" role="tabpanel" class="tab-pane fade">
+                <div id="transports-new" class="new-section">
+                    <select id="transports-select" class="form-select form-select-lg select">
+                        <option selected>Please choose an option</option>
+                        <option value="1">Addon 1</option>
+                        <option value="2">Addon 2</option>
+                        <option value="3">Addon 3</option>
+                    </select>
+                    <button class="btn btn-success add-btn">Add</button>{{-- TODO: Implement --}}
                 </div>
-                <table id="transports-table" class="table table-striped" style="min-width: 100%; margin-bottom: 1px; border: 1px solid black">
-                    <thead>
-                    <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Travel Class</th>
-                        <th scope="col">Component Type</th>
-                    </tr>
-                    </thead>
-                    @foreach($transports as $transport)
+                <div id="transports-details">
+                    <table id="transports-table" class="table table-striped table-responsive-sm">
+                        <thead>
                         <tr>
-                            <td>{{ $transport["inventory"]->departure_date_time }} to {{ $transport["inventory"]->arrival_date_time }}</td>
-                            <td>{{ $transport["component"]->name }}</td>
-                            <td>{{ $transport["inventory"]->travelClass->title }}</td>
-                            <td>{{ $transport["tour"]->tour_component_type }}</td>
+                            <th scope="col">Date</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Travel Class</th>
+                            <th scope="col">Component Type</th>
+                            <th scope="col">Actions</th>
                         </tr>
-                    @endforeach
-                </table>
+                        </thead>
+                        @foreach($transports as $transport)
+                            <tr>
+                                <td>{{ $transport["inventory"]->departure_date_time }} to {{ $transport["inventory"]->arrival_date_time }}</td>
+                                <td>{{ $transport["component"]->name }}</td>
+                                <td>{{ $transport["inventory"]->travelClass->title }}</td>
+                                <td>{{ $transport["tour"]->tour_component_type }}</td>
+                                <td><a href="#" class="btn btn-danger">Delete</a></td>{{-- TODO: Implement --}}
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-
     {{-- Closing Container---}}
 </div>
 </div>
