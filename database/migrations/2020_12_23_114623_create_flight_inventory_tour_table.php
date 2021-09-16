be7dbf6e84847dba56989cd1ab411ae08118f3b4
Celeste Gateley <celeste.gateley@gmail.com>
2021-09-16 14:03:03 +0100

    Renamed sales_price to tour_sales_price in Flight-Inventory-Tour

--- database/migrations/2020_12_23_114623_create_flight_inventory_tour_table.php
+++ database/migrations/2020_12_23_114623_create_flight_inventory_tour_table.php
@@ -18,7 +18,7 @@ class CreateFlightInventoryTourTable extends Migration
             $table->integer('tour_id');
             $table->integer('flight_inventory_id');
             $table->enum('tour_component_type', ['Included', 'Add-on', 'Upgrade'])->default('Included');
-            $table->float('sales_price', 10, 0)->nullable();
+            $table->float('tour_sales_price', 10, 0)->nullable();
             $table->string('flight_type', 20)->nullable();
             $table->timestamps();
             $table->softDeletes();
