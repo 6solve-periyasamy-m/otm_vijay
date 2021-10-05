c32cfb9f98de059dd37233ce9f553ce4be8acc3b
Celeste Gateley <celeste.gateley@gmail.com>
2021-10-05 15:35:29 +0100

    Fix for incorrect display value on datetime-local on activity inventory

--- resources/views/partials/models/activity_inventories/form.blade.php
+++ resources/views/partials/models/activity_inventories/form.blade.php
@@ -32,13 +32,13 @@
     <p></p>
     <div id="form-group">
         <label for="activity_start_date_time-input">Activity Start Date Time</label>
-        <input type="datetime-local" name="activity_start_date_time" value="{{ $activity_start_date_time ?? "" }}" class="form-control"
+        <input type="datetime-local" name="activity_start_date_time" value="{{ isset($activity_start_date_time) ? $activity_start_date_time->format('Y-m-d\TH:i') : "" }}" class="form-control"
                id="activity_start_date_time-input">
     </div>
     <p></p>
     <div id="form-group">
         <label for="activity_end_date_time-input">Activity End Date Time</label>
-        <input type="datetime-local" name="activity_end_date_time" value="{{ $activity_end_date_time ?? "" }}"
+        <input type="datetime-local" name="activity_end_date_time" value="{{ isset($activity_end_date_time) ? $activity_end_date_time->format('Y-m-d\TH:i') : "" }}"
                class="form-control" id="activity_end_date_time-input">
     </div>
     <p></p>
