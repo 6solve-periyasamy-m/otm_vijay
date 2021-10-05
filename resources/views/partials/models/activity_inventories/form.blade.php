25e534a632ed5a9018cb343426c13105a083075b
Celeste Gateley <celeste.gateley@gmail.com>
2021-10-05 16:37:18 +0100

    Changed styling on 'fit_selectable' checkbox in activity inventories table

--- resources/views/partials/models/activity_inventories/form.blade.php
+++ resources/views/partials/models/activity_inventories/form.blade.php
@@ -43,8 +43,8 @@
     </div>
     <p></p>
     <div id="form-group">
+        <input type="checkbox" name="fit_selectable" class="form-check-input" id="fit_selectable-input" @if(isset($fit_selectable) && $fit_selectable == 1) checked @endif>
         <label for="fit_selectable-input">Fit Selectable</label>
-        <input type="checkbox" name="fit_selectable" class="form-control" id="fit_selectable-input" @if(isset($fit_selectable) && $fit_selectable == 1) checked @endif>
     </div>
     <p></p>
     <div id="form-group">
