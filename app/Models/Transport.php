ebea88eb6fc155c27ff04536912f99ace3d3300c
Celeste Gateley <celeste.gateley@gmail.com>
2021-10-07 13:09:40 +0100

    Added cascade soft-deletes to transport models

--- app/Models/Transport.php
+++ app/Models/Transport.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,9 +10,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Transport extends Model
 {
     use HasFactory;
-    use SoftDeletes;
+    use SoftDeletes, CascadeSoftDeletes;
 
     protected $fillable = ['transport_type_id','operator_id','departure_location_id','arrival_location_id','name','description','currency','is_domestic','notes',];
+    protected $cascadeDeletes = ['transportInventory'];
 
     public function transportInventory()
     {
--- app/Models/TransportInventoryTour.php
+++ app/Models/TransportInventoryTour.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,9 +11,10 @@ class TransportInventoryTour extends Model
 {
     protected $table = 'transport_inventory_tour';
     use HasFactory;
-    use SoftDeletes;
+    use SoftDeletes, CascadeSoftDeletes;
 
     protected $fillable = ['tour_id','transport_inventory_id',];
+    protected $cascadeDeletes = ['orders'];
 
     public function transportInventory() {
         return $this->belongsTo(TransportInventory::class, 'transport_inventory_id');
