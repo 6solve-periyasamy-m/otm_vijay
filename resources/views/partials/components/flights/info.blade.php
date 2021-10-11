18df7332ae56029fbf5d6f06adb741e217a29c6b
Celeste Gateley <celeste.gateley@gmail.com>
2021-10-11 09:13:40 +0100

    Moved info column style to class

--- resources/views/partials/components/flights/info.blade.php
+++ resources/views/partials/components/flights/info.blade.php
@@ -1,9 +1,17 @@
+<style>
+    .info-col {
+        width: 25%;
+        text-align: center;
+        border: 1px solid black;
+    }
+</style>
+
 <table style="width: 100%;">
     <tr>
-        <td style="width: 25%; text-align: center; border: 1px solid black"><h2>{{ $flight->airline->name }}</h2></td>
-        <td style="width: 25%; text-align: center; border: 1px solid black"><h2>{{ $flight->arrivalAirport->name }}</h2></td>
-        <td style="width: 25%; text-align: center; border: 1px solid black"><h2>{{ $flight->departureAirport->name }}</h2></td>
-        <td style="width: 25%; text-align: center; border: 1px solid black"><h2>{{ $flight->is_domestic ? "Domestic" : "International" }}</h2></td>
+        <td class="info-col"><h2>{{ $flight->airline->name }}</h2></td>
+        <td class="info-col"><h2>{{ $flight->arrivalAirport->name }}</h2></td>
+        <td class="info-col"><h2>{{ $flight->departureAirport->name }}</h2></td>
+        <td class="info-col"><h2>{{ $flight->is_domestic ? "Domestic" : "International" }}</h2></td>
     </tr>
 </table>
 {{ $flight->notes }}
