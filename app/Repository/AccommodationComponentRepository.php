0bddb554fde03603ec7a4709bc13cbd556a68411
Celeste Gateley <celeste.gateley@gmail.com>
2021-10-12 14:45:26 +0100

    Set accommodation inventory between dates to return an array, with a data key, containing the results of the query

--- app/Repository/AccommodationComponentRepository.php
+++ app/Repository/AccommodationComponentRepository.php
@@ -101,6 +101,6 @@ class AccommodationComponentRepository implements AccommodationComponentReposito
         );
         if (isset($dateFrom)) $query = $query->whereRaw("'" . $dateFrom->format('Y-m-d') . "' BETWEEN `accommodation_inventories`.`check_in_date_time` AND `accommodation_inventories`.`check_out_date_time`");
         if (isset($dateTo)) $query = $query->whereRaw("'" . $dateTo->format('Y-m-d') . "' BETWEEN `accommodation_inventories`.`check_in_date_time` AND `accommodation_inventories`.`check_out_date_time`");
-        return $query->get();
+        return  ["data" => $query->get(),];
     }
 }
