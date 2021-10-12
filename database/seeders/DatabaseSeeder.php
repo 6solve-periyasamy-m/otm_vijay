2c5a46ada11d42e42dd0db71f61e4486ec291289
Nicholas Alexander <nalex@sfsw.net>
2021-10-12 22:32:53 +0000

    remove-voyager: remove merge conflict

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         $this->call(RolesTableSeeder::class);
-=======
->>>>>>> main
         $this->call(LocationTypesTableSeeder::class);
         $this->call(LocationsTableSeeder::class);
         $this->call(AirportsTableSeeder::class);
