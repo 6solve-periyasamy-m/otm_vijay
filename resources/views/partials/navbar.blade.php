0290e78a983d202ad216ccb191597f903082032b
Eddy <kelvineddyimache@gmail.com>
2021-10-22 15:50:22 +0100

    Insert logo to the left of Nav bar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,9 @@
 <div class="navbar navbar-expand text-light sticky-top otm-navbar px-md-3 flex-column flex-md-row">
     <div class="navbar-brand abs">
-        <a class="text-light fw-bold" href="#">Octopus Travel Matrix</a>
+        <a class="text-light fw-bold" href="#">
+            <img src="{{ asset('images/logo.png') }}" class='logo'>
+            <span>Octopus Travel Matrix</span>
+        </a>
     </div> 
     <div class="navbar-nav flex-row ms-auto">
         <div class="nav-item">
