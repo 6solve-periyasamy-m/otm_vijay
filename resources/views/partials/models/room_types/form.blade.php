613ea14752ec5a48c31b2e01fdefc1e74503514f
Eddy <kelvineddyimache@gmail.com>
2021-10-25 09:17:31 +0100

    Add Room Type Form

--- resources/views/partials/models/room_types/form.blade.php
+++ resources/views/partials/models/room_types/form.blade.php
@@ -1,17 +1,23 @@
-<form action="{{ $action }}" method="post">
-    @csrf
-    <div class="form-group">
-        <label for="room_type_name-input">Room Type Name</label>
-        <input name="room_type_name" value="{{ $room_type_name ?? "" }}" class="form-control" id="room_type_name-input">
+<div class="card">
+    <div class="card-body">
+        <form action="{{ $action }}" method="post">
+            @csrf
+            <div class="row">
+                <div class="form-group col-12">
+                    <label for="room_type_name-input">Room Type Name</label>
+                    <input name="room_type_name" value="{{ $room_type_name ?? "" }}" class="form-control" id="room_type_name-input">
+                </div>
+                <p></p>
+                <div class="form-group col-12">
+                    <label for="maximum_occupancy-input">Maximum Occupancy</label>
+                    <input name="maximum_occupancy" value="{{ $maximum_occupancy ?? "" }}" class="form-control"
+                        id="maximum_occupancy-input">
+                </div>
+                <p></p>
+                <div class="form-group col-12">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
     </div>
-    <p></p>
-    <div class="form-group">
-        <label for="maximum_occupancy-input">Maximum Occupancy</label>
-        <input name="maximum_occupancy" value="{{ $maximum_occupancy ?? "" }}" class="form-control"
-               id="maximum_occupancy-input">
-    </div>
-    <p></p>
-    <div class="form-group">
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-</form>
+</div>
