93acc7e844b48364798b9372cd30e1f33dd62369
Celeste Gateley <celeste.gateley@gmail.com>
2021-10-26 10:16:45 +0100

    Changed customer view to reduce information density

--- resources/views/pages/models/customers/view.blade.php
+++ resources/views/pages/models/customers/view.blade.php
@@ -3,48 +3,73 @@
 @section('title', 'View Customer')
 
 @section('content')
-    <div class="id-card w-100">
+    <div class="id-card w-100" style="border: 1px solid black; border-radius: 15px;">
         <div class="d-inline align-top">
-            <img src="{{ asset('images/exampleavatar.jpg') }}" style="width: 150px; height: 150px;">
+            <img src="{{ asset('images/exampleavatar.jpg') }}" style="width: 225px; height: 225px; border-radius: 15px 0 0 15px">
         </div>
-        <div class="d-inline-flex align-top" style="font-size: 16px; font-weight: 1000;">
+        <div class="d-inline-flex align-top">
             <table>
                 <tr class="border-bottom">
-                    <th scope="row">Personal Details:</th>
+                    <th style="padding-right: 2px;" scope="row"><strong>Personal Details:</strong></th>
                     <td>
                         {{ $customer->title }} {{ $customer->first_name }} {{ $customer->middle_names }} {{ $customer->last_name }} ({{ $customer->gender }})
                     </td>
                 </tr>
                 <tr class="border-bottom">
-                    <th scope="row">Contact Information:</th>
+                    <th style="padding-right: 2px;" scope="row"><strong>Email Address:</strong></th>
                     <td>
-                        <a href="mailto:{{ $customer->email_address }}">{{ $customer->email_address }}</a>, {{ $customer->mobile_number }}
+                        <a href="mailto:{{ $customer->email_address }}">{{ $customer->email_address }}</a>,
+                    </td>
+                </tr>
+                <tr class="border-bottom">
+                    <th style="padding-right: 2px;" scope="row"><strong>Phone Number:</strong></th>
+                    <td>
+                        <a href="tel:{{ $customer->mobile_number }}">{{ $customer->mobile_number }}</a>
                         @if(isset($customer->other_phone_number))
-                            ({{ $customer->other_phone_number }})
+                            (<a href="tel:{{ $customer->other_phone_number }}">{{ $customer->other_phone_number }}</a>)
                         @endif
                     </td>
                 </tr>
+                @if($customer->home_address_id != $customer->billing_address_id)
+                    <tr class="border-bottom">
+                        <th style="padding-right: 2px;" scope="row"><strong>Home Address:</strong></th>
+                        <td>
+                            <strong>{{ $customer->homeAddress }}</strong>
+                        </td>
+                    </tr>
+                    <tr class="border-bottom">
+                        <th style="padding-right: 2px;" scope="row"><strong>Billing Address:</strong></th>
+                        <td>{{ $customer->billingAddress }}</td>
+                    </tr>
+                @else
+                    <tr class="border-bottom">
+                        <th style="padding-right: 2px;" scope="row"><strong>Address:</strong></th>
+                        <td>
+                            <strong>{{ $customer->homeAddress }}</strong>
+                        </td>
+                    </tr>
+                    <tr cla
+                @endif
                 <tr class="border-bottom">
-                    <th scope="row">Address:</th>
-                    <td>
-                        {{ $customer->homeAddress }}
-                        @if($customer->home_address_id != $customer->billing_address_id)
-                            (Billing: {{ $customer->billingAddress }})
-                        @endif
-                    </td>
-                </tr>
-                <tr class="border-bottom">
-                    <th scope="row">Passport Details:</th>
+                    <th style="padding-right: 2px;" scope="row"><strong>Passport Details:</strong></th>
                     <td>
                         {{ $customer->passport_first_name }} {{ $customer->passport_middle_names }} {{ $customer->passport_last_name }}, {{ $customer->passport_number }}, {{ $customer->passport_issue_date }} to {{ $customer->passport_expiry_date }}
                     </td>
                 </tr>
                 <tr class="border-bottom">
-                    <th scope="row">Emergency Contact:</th>
+                    <th style="padding-right: 2px;" scope="row"><strong>Emergency Contact:</strong></th>
                     <td>
-                        {{ $customer->emergency_contact_name }} ({{ $customer->emergency_contact_relationship }}) {{ $customer->emergency_contact_telephone }}
+                        {{ $customer->emergency_contact_name }} ({{ $customer->emergency_contact_relationship }}),  <a href="tel:{{ $customer->emergency_contact_telephone }}">{{ $customer->emergency_contact_telephone }}</a>
                     </td>
                 </tr>
+                @if(isset($customer->notes))
+                    <tr class="border-bottom">
+                        <th style="padding-right: 2px;" scope="row"><strong>Notes:</strong></th>
+                        <td>
+                            {{ $customer->notes }}
+                        </td>
+                    </tr>
+                @endif
             </table>
         </div>
     </div>
