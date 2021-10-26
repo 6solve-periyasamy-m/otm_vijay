5292f8fcea200fdabadc871b4dcae3f761ca7241
Celeste Gateley <celeste.gateley@gmail.com>
2021-10-26 22:36:08 +0100

    Added add customer button to order customer form

--- resources/views/partials/models/orders_customers/form.blade.php
+++ resources/views/partials/models/orders_customers/form.blade.php
@@ -25,6 +25,7 @@
     <div class="form-group">
         <label for="customer_id-input">Customer</label>
         <select style="width: 100%" name="customer_id"  class="form-control" id="customer_id-input"></select>
+        <a href="{{ route('customers.create') }}" target="_blank" class="btn btn-success d-inline">+</a>
     </div>
     <p></p>
     <div class="form-group">
