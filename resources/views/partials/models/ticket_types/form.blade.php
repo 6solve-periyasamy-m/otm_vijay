62819fd6dd2927c229d0134d3c9250ba87561a8c
Eddy <kelvineddyimache@gmail.com>
2021-10-27 15:17:04 +0100

    Ticket Type Add Form

--- resources/views/partials/models/ticket_types/form.blade.php
+++ resources/views/partials/models/ticket_types/form.blade.php
@@ -1,11 +1,17 @@
-<form action="{{ $action }}" method="post">
-    @csrf
-    <div class="form-group">
-        <label for="name-input">Name</label>
-        <input name="name" value="{{ $name ?? "" }}" class="form-control" id="name-input">
+<div class="card">
+    <div class="card-body">
+        <form action="{{ $action }}" method="post">
+            @csrf
+            <div class="row">
+                <div class="form-group col-12">
+                    <label for="name-input">Name</label>
+                    <input name="name" value="{{ $name ?? "" }}" class="form-control" id="name-input">
+                </div>
+                <p></p>
+                <div class="form-group col-12">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
     </div>
-    <p></p>
-    <div class="form-group">
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-</form>
+</div>
