5f73bc068cb7ecd75c3fdb68c154db84195ca77a
Eddy <kelvineddyimache@gmail.com>
2021-10-27 15:28:19 +0100

    Add Country Form

--- resources/views/partials/models/countries/form.blade.php
+++ resources/views/partials/models/countries/form.blade.php
@@ -1,21 +1,27 @@
-<form action="{{ $action }}" method="post">
-    @csrf
-    <div class="form-group">
-        <label for="name-input">Name</label>
-        <input name="name" value="{{ $name ?? "" }}" class="form-control" id="name-input">
+<div class="card">
+    <div class="card-body">
+        <form action="{{ $action }}" method="post">
+            @csrf
+            <div class="row">
+                <div class="form-group col-12">
+                    <label for="name-input">Name</label>
+                    <input name="name" value="{{ $name ?? "" }}" class="form-control" id="name-input">
+                </div>
+                <p></p>
+                <div class="form-group col-12">
+                    <label for="code-input">Code</label>
+                    <input name="code" value="{{ $code ?? "" }}" class="form-control" id="code-input">
+                </div>
+                <p></p>
+                <div class="form-group col-12">
+                    <label for="currency-input">Currency</label>
+                    <input name="currency" value="{{ $currency ?? "" }}" class="form-control" id="currency-input">
+                </div>
+                <p></p>
+                <div class="form-group col-12">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
     </div>
-    <p></p>
-    <div class="form-group">
-        <label for="code-input">Code</label>
-        <input name="code" value="{{ $code ?? "" }}" class="form-control" id="code-input">
-    </div>
-    <p></p>
-    <div class="form-group">
-        <label for="currency-input">Currency</label>
-        <input name="currency" value="{{ $currency ?? "" }}" class="form-control" id="currency-input">
-    </div>
-    <p></p>
-    <div class="form-group">
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-</form>
+</div>
