c947ec3ee6359aeddb7546a6ba5a1c6e57683f2e
Eddy <kelvineddyimache@gmail.com>
2021-10-29 16:51:50 +0100

    Customer View Page

--- resources/views/pages/models/customers/view.blade.php
+++ resources/views/pages/models/customers/view.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.main')
+@extends('layout.master')
 
 @section('title', 'View Customer')
 
@@ -9,95 +9,84 @@
 @endsection
 
 @section('content')
-    <div class="id-card w-100" style="border: 1px solid black; border-radius: 15px;">
-        <div class="d-inline align-top">
-            <img src="{{ asset('images/exampleavatar.jpg') }}" style="width: 225px; height: 225px; border-radius: 15px 0 0 15px">
+    <div class="otm-callout">
+        <div class="row">
+            <div class="col-xl-3">
+                <img src="{{ asset('images/exampleavatar.jpg') }}" class="img-thumbnail">
+            </div>
+            <div class="col-xl-9">
+                <div class="row">
+                    <div class="col-12">
+                        <p>Personal Details</p>
+                        <h6 class="fw-bold">{{ $customer->title }} {{ $customer->first_name }} {{ $customer->middle_names }} {{ $customer->last_name }} ({{ $customer->gender }})</h6>
+                    </div>
+                    <div class="col-12">
+                        <p>Email Address</p>
+                        <h6 class="fw-bold"><a href="mailto:{{ $customer->email_address }}">{{ $customer->email_address }}</a></h6>
+                    </div>
+                    <div class="col-12">
+                        <p>Phone Number</p>
+                        <h6 class="fw-bold"> 
+                            <a href="tel:{{ $customer->mobile_number }}">{{ $customer->mobile_number }}</a>
+                            @if(isset($customer->other_phone_number))
+                                (<a href="tel:{{ $customer->other_phone_number }}">{{ $customer->other_phone_number }}</a>)
+                            @endif
+                        </h6>
+                    </div>
+                    <div class="col-12">
+                        @if($customer->home_address_id != $customer->billing_address_id)
+                            <p>Home Address</p>
+                            <h6 class="fw-bold">{{ $customer->homeAddress }}</h6>
+                            <p>Billing Address</p>
+                            <h6 class="fw-bold">{{ $customer->billingAddress }}</h6>                    
+                        @else
+                            <p>Address</p>
+                            <h6 class="fw-bold">{{ $customer->homeAddress }}</h6>                    
+                        @endif                
+                    </div>
+                    <div class="col-12">
+                        <p>Passport Details</p>
+                        <h6 class="fw-bold">{{ $customer->passport_first_name }} {{ $customer->passport_middle_names }} {{ $customer->passport_last_name }}, {{ $customer->passport_number }}, {{ $customer->passport_issue_date }} to {{ $customer->passport_expiry_date }}</h6>
+                    </div>
+                    <div class="col-12">
+                        <p>Emergency Contact</p>
+                        <h6 class="fw-bold">{{ $customer->emergency_contact_name }} ({{ $customer->emergency_contact_relationship }}),  <a href="tel:{{ $customer->emergency_contact_telephone }}">{{ $customer->emergency_contact_telephone }}</a></h6>
+                    </div>
+                    <div class="col-12">
+                        <p>Notes</p>
+                        <h6 class="fw-bold">{{ $customer->notes }}</h6>
+                    </div>                    
+                    <div class="col-12">
+                        <a href="{{ route('customers.edit', ['customer' => $customer,]) }}" class="btn btn-success">
+                            <i class="icon-note"></i>
+                            Edit Customer
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="d-inline-flex align-top">
-            <table>
-                <tr class="border-bottom">
-                    <th style="padding-right: 2px;" scope="row"><strong>Personal Details:</strong></th>
-                    <td>
-                        {{ $customer->title }} {{ $customer->first_name }} {{ $customer->middle_names }} {{ $customer->last_name }} ({{ $customer->gender }})
-                    </td>
-                </tr>
-                <tr class="border-bottom">
-                    <th style="padding-right: 2px;" scope="row"><strong>Email Address:</strong></th>
-                    <td>
-                        <a href="mailto:{{ $customer->email_address }}">{{ $customer->email_address }}</a>,
-                    </td>
-                </tr>
-                <tr class="border-bottom">
-                    <th style="padding-right: 2px;" scope="row"><strong>Phone Number:</strong></th>
-                    <td>
-                        <a href="tel:{{ $customer->mobile_number }}">{{ $customer->mobile_number }}</a>
-                        @if(isset($customer->other_phone_number))
-                            (<a href="tel:{{ $customer->other_phone_number }}">{{ $customer->other_phone_number }}</a>)
-                        @endif
-                    </td>
-                </tr>
-
-                @if($customer->home_address_id != $customer->billing_address_id)
-                    <tr class="border-bottom">
-                        <th style="padding-right: 2px;" scope="row"><strong>Home Address:</strong></th>
-                        <td>
-                            <strong>{{ $customer->homeAddress }}</strong>
-                        </td>
+    </div>        
+    <hr style="border-bottom: 5px solid #cccccc; border-radius: 2px;"/>
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-striped order-table">
+                <thead>
+                    <tr>
+                        <th scope="col">Booking Reference</th>
+                        <th scope="col">Tour Name</th>
+                        <th scope="col">Ordered On</th>
+                        <th scope="col">Tour Cost</th>
                     </tr>
-                    <tr class="border-bottom">
-                        <th style="padding-right: 2px;" scope="row"><strong>Billing Address:</strong></th>
-                        <td>{{ $customer->billingAddress }}</td>
+                </thead>
+                @foreach($customer->orderCustomers as $orderCustomer)
+                    <tr>
+                        <th scope="row"><a href="{{ route('orders.view', ['order' => $orderCustomer->order,]) }}">{{ $orderCustomer->order->booking_reference }}</a></th>
+                        <td>{{ $orderCustomer->order->tour->title }}</td>
+                        <td>{{ $orderCustomer->order->ordered_on }}</td>
+                        <td>{{ $orderCustomer->tour_cost }}</td>
                     </tr>
-                @else
-                    <tr class="border-bottom">
-                        <th style="padding-right: 2px;" scope="row"><strong>Address:</strong></th>
-                        <td>
-                            <strong>{{ $customer->homeAddress }}</strong>
-                        </td>
-                    </tr>
-                @endif
-                <tr class="border-bottom">
-                    <th style="padding-right: 2px;" scope="row"><strong>Passport Details:</strong></th>
-                    <td>
-                        {{ $customer->passport_first_name }} {{ $customer->passport_middle_names }} {{ $customer->passport_last_name }}, {{ $customer->passport_number }}, {{ $customer->passport_issue_date }} to {{ $customer->passport_expiry_date }}
-                    </td>
-                </tr>
-                <tr class="border-bottom">
-                    <th style="padding-right: 2px;" scope="row"><strong>Emergency Contact:</strong></th>
-                    <td>
-                        {{ $customer->emergency_contact_name }} ({{ $customer->emergency_contact_relationship }}),  <a href="tel:{{ $customer->emergency_contact_telephone }}">{{ $customer->emergency_contact_telephone }}</a>
-                    </td>
-                </tr>
-                @if(isset($customer->notes))
-                    <tr class="border-bottom">
-                        <th style="padding-right: 2px;" scope="row"><strong>Notes:</strong></th>
-                        <td>
-                            {{ $customer->notes }}
-                        </td>
-                    </tr>
-
-                @endif
+                @endforeach
             </table>
         </div>
     </div>
-    <a href="{{ route('customers.edit', ['customer' => $customer,]) }}" class="btn btn-success">Edit</a>
-    <hr style="border-bottom: 5px solid #cccccc; border-radius: 2px;"/>
-    <table class="table table-striped order-table">
-        <thead>
-            <tr>
-                <th scope="col">Booking Reference</th>
-                <th scope="col">Tour Name</th>
-                <th scope="col">Ordered On</th>
-                <th scope="col">Tour Cost</th>
-            </tr>
-        </thead>
-        @foreach($customer->orderCustomers as $orderCustomer)
-            <tr>
-                <th scope="row"><a href="{{ route('orders.view', ['order' => $orderCustomer->order,]) }}">{{ $orderCustomer->order->booking_reference }}</a></th>
-                <td>{{ $orderCustomer->order->tour->title }}</td>
-                <td>{{ $orderCustomer->order->ordered_on }}</td>
-                <td>{{ $orderCustomer->tour_cost }}</td>
-            </tr>
-        @endforeach
-    </table>
 @endsection
