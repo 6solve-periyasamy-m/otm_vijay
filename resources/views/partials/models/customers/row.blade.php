2d97189639742f64e1eb2b1e42a7b8aa8459790e
Eddy <kelvineddyimache@gmail.com>
2021-10-29 15:22:52 +0100

    Customer List Page

--- resources/views/partials/models/customers/row.blade.php
+++ resources/views/partials/models/customers/row.blade.php
@@ -5,12 +5,12 @@
     <td>{{ $mobile_number }}</td>
     <td>{{ $passport_expiry_date }}</td>
     <td>
-        <a href="{{route('customers.edit', ['customer' => $customer,])}}">
-            <ion-icon name="create"></ion-icon>
+        <a href="{{route('customers.edit', ['customer' => $customer,])}}" class="btn btn-outline-success btn-sm mb-1">
+            <i class="icon-note"></i>
         </a>
-        <a href="#"
+        <a href="#" class="btn btn-outline-danger btn-sm mb-1"
            onclick="event.preventDefault();document.getElementById('customer-{{ $customer->id }}-delete').submit();">
-            <ion-icon name="trash"></ion-icon>
+            <i class="icon-trash"></i>
         </a>
         <form id="customer-{{ $customer->id }}-delete"
               action="{{ route('customers.delete', ['customer' => $customer,]) }}" method="POST"
