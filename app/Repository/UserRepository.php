94ed1f581c0dbb2ecf22399cbf47055c1a164eb3
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-08 13:43:48 +0000

    Added strict typing to UserRepository

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -2,17 +2,18 @@
 
 namespace App\Repository;
 
+use App\Models\ApiToken;
 use App\Models\User;
 
 interface UserRepositoryInterface
 {
-    public static function getLatestToken(User $user);
+    public static function getLatestToken(User $user) : ApiToken;
 }
 
 class UserRepository implements UserRepositoryInterface
 {
 
-    public static function getLatestToken(User $user)
+    public static function getLatestToken(User $user) : ApiToken
     {
         $token = $user->tokens()->latest()->first();
         if (!(isset($token) && !$token->hasExpired())) $token = $user->generateToken();
