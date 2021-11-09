d26b48c7fbfd8c33d112a55c3fc64e46d9a3e67a
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-09 12:11:48 +0000

    Added command with scheduled task for pulling in country data

---
+++ app/Repository/LocationsRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repository;
+
+use App\Models\Country;
+use App\Models\Currency;
+use Illuminate\Support\Facades\Log;
+
+interface LocationsRepositoryInterface
+{
+    public static function updateCountry($ccn3, $cca3, $commonName, $dialing_code, $currencies);
+}
+
+class LocationsRepository implements LocationsRepositoryInterface
+{
+    public static function updateCountry($ccn3, $cca3, $commonName, $dialing_code, $currencies)
+    {
+        $country = Country::where('numeric_code', $ccn3)->first();
+        if (!isset($country)) {
+            $country = Country::make();
+            $country->numeric_code = $ccn3;
+        }
+        $country->alpha_code = $cca3;
+        $country->name = $commonName;
+        $country->dialing_code = $dialing_code;
+        $country->save();
+        foreach ($currencies as $code => $data) {
+            self::processCurrencyForCountry($country, $code, $data['name'], $data['symbol'] ?? null);
+        }
+    }
+
+    private static function processCurrencyForCountry(Country $country, $code, $name, $symbol)
+    {
+        $currency = Currency::where('code', $code)->first();
+        if (isset($currency)) {
+            foreach ($currency->countries as $cCountry) { if ($cCountry->id == $country->id) return; }
+        } else {
+            $currency = Currency::create(['code' => $code, 'name' => $name, 'symbol' => $symbol, ]);
+        }
+        $currency->countries()->save($country);
+    }
+}
