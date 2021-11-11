5597487699959903ab4bf11b070c56f4c163d429
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-11 09:18:28 +0000

    Updated database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Tour;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call(EventsTableSeeder::class);
         Tour::factory()->createOne();
         $this->call(LocationTypesTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
+        Artisan::call('countries:update');
         $this->call(RegionsTableSeeder::class);
         $this->call(LocationsTableSeeder::class);
         $this->call(AirlinesTableSeeder::class);
