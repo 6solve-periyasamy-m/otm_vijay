9141e4cbe899e4c54febdce325a96d6397816fe3
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-13 14:13:56 +0000

    Migrated board type form to use form components

--- resources/views/pages/models/board_types/update.blade.php
+++ resources/views/pages/models/board_types/update.blade.php
@@ -1,9 +1,9 @@
-@extends('layout.main')
+@extends('layout.form', ['action' => route('board-types.update', ['boardType' => $boardType,]),])
 
 @section('title', 'Update Board Type')
 
 @section('content')
-    @include('partials.models.board_types.form', ['action' => route('board-types.update', ['boardType' => $boardType,]),
+    @include('partials.models.board_types.form', [
       'name' => $boardType->name,
     ])
 @endsection
