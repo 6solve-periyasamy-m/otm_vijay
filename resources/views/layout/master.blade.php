d589e8793fde444bc5c5b691e0ef1c1936ae9fdb
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-13 10:26:21 +0000

    Added header and footer script stacks

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -21,12 +21,18 @@
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     @yield('header-script')
+    @stack('header-stack') <!-- TODO: Rename to script once all sections are converted -->
+    <script type="text/javascript">
+        $(document).ready(function () {
+            @stack('head-ready')
+        });
+    </script>
 </head>
 <body>
 @include('partials.navbar')
 <div class="container-fluid">
     <div class='row flex-xl-nowrap'>
-        @include('partials.sidebar')    
+        @include('partials.sidebar')
         <div id="container" class='col-12 col-md-9 col-xl-10 py-md-3 px-md-4 otm-content'>
             @if ($errors->any())
                 @foreach ($errors->all() as $error)
@@ -59,5 +65,10 @@
     });
 </script>
 @yield('footer-script')
+<script type="text/javascript">
+    $(document).ready(function () {
+        @stack('footer-ready')
+    });
+</script>
 </body>
 </html>
