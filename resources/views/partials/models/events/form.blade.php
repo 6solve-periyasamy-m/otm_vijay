0a71f520f7e2c3a71e3cc1dc81148c2b06f32773
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-13 15:18:58 +0000

    Migrated event form to use form components

--- resources/views/partials/models/events/form.blade.php
+++ resources/views/partials/models/events/form.blade.php
@@ -1,38 +1,9 @@
-<div class="card">
-    <div class="card-body">
-        <form action="{{ $action }}" method="post">
-            @csrf
-            <div class="row">
-                <div class="form-group col-12">
-                    <label for="name-input">Event Title</label>
-                    <input name="name" value="{{ $name ?? "" }}" class="form-control" id="name-input">
-                </div>                
-                <div class="form-group col-12">
-                    <label for="description-input">Event Description</label>
-                    <input name="description" value="{{ $description ?? "" }}" class="form-control"
-                        id="description-input">
-                </div>                
-                <div class="form-group col-12">
-                    <label for="starts_at-input">Event Start Date</label>
-                    <input type="date" name="starts_at" value="{{ $starts_at ?? "" }}" class="form-control"
-                        id="starts_at-input">
-                </div>                
-                <div class="form-group col-12">
-                    <label for="ends_at-input">Event End Date</label>
-                    <input type="date" name="ends_at" value="{{ $ends_at ?? "" }}" class="form-control" id="ends_at-input">
-                </div>                
-                <div class="form-group col-12">
-                    <label for="booking_url-input">Booking Url</label>
-                    <input name="booking_url" value="{{ $booking_url ?? "" }}" class="form-control" id="booking_url-input">
-                </div>                
-                <div class="form-group col-12">
-                    <label for="notes-input">Notes</label>
-                    <input name="notes" value="{{ $notes ?? "" }}" class="form-control" id="notes-input">
-                </div>                
-                <div class="form-group col-12">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-            </div>
-        </form>
-    </div>
-</div>
+@include('partials.fields.text', ['name' => 'Name', 'field' => 'name', 'value' => $name ?? null,])
+@include('partials.fields.text', ['name' => 'Description', 'field' => 'description', 'value' => $description ?? null,])
+@include('partials.fields.date', ['name' => 'Start Date', 'field' => 'starts_at', 'value' => $starts_at ?? null,
+'onChange' => 'changeDate($(\'#starts_at-input\'), $(\'#ends_at-input\'))', 'width' => 6,])
+@include('partials.fields.date', ['name' => 'End Date', 'field' => 'ends_at', 'value' => $ends_at ?? null,
+'onChange' => 'removeAutoset($(\'#starts_at-input\'), $(\'#ends_at-input\'));', 'classes' => 'autoset', 'width' => 6,])
+@include('partials.fields.text', ['name' => 'Booking URL', 'field' => 'booking_url', 'value' => $booking_url ?? null,])
+@include('partials.fields.prefab.notes')
+@include('partials.fields.submit')
