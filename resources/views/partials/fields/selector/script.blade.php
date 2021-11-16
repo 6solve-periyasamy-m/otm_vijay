712a0e5cc97dc469fd06d57e0c6ef4b0a5d04a99
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-16 12:47:23 +0000

    Added API data to selector script

--- resources/views/partials/fields/selector/script.blade.php
+++ resources/views/partials/fields/selector/script.blade.php
@@ -5,13 +5,16 @@ let {{ $field }}Select = $('.{{ $field }}-input');
         data: function (params) {
             return {
                 filter: params.term,
+                __api_token: '{{ Auth::user()->getCurrentToken()->token }}',
                 {{ $additionalParams }}
             };
-        }
+        },
+        type: 'post',
 }
 });
 $.ajax({
     url: '{{ route('api.' . $route . '.selected', ['id' => old($field) ?? $id ?? 0, ]) }}',
+    type: 'post', data: { __api_token: '{{ Auth::user()->getCurrentToken()->token }}', } })
 }).then(function (data) {
     {{ $field }}Select.append(new Option(data.text, data.id, true, true)).trigger('change');
 
