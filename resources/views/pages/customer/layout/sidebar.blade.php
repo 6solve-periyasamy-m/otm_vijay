d2634e3afeb1526be0f13799ffc48d642d972bd4
Eddy <kelvineddyimache@gmail.com>
2021-11-16 07:26:14 +0000

    Customer Layout

---
+++ resources/views/pages/customer/layout/sidebar.blade.php
@@ -0,0 +1,58 @@
+<aside class="left-sidebar" data-sidebarbg="skin6">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+                <!-- User Profile-->
+                <li class="sidebar-item"> 
+                    <a class="sidebar-link waves-effect waves-dark sidbar-link" href="index.html" aria-expanded="false">
+                        <i class="me-2 icon-settings"></i>
+                        <span class="hide-menu">Dashboard</span>
+                    </a>
+                </li>
+                <li class="sidebar-item"> 
+                    <a class="sidebar-link has-arrow waves-effect waves-dark active" href="javascript:void(0)" aria-expanded="false">                        
+                        <i class="me-2 icon-user"></i>
+                        <span class="hide-menu">Your Detail</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse first-level in">
+                        <li class="sidebar-item">
+                            <a href="layout-inner-fixed-left-sidebar.html" class="sidebar-link active">
+                                <span class="hide-menu">
+                                Update
+                                </span>
+                            </a>
+                        </li>  
+                        <li class="sidebar-item">
+                            <a href="layout-inner-fixed-left-sidebar.html" class="sidebar-link">
+                                <span class="hide-menu">
+                                ATOL
+                                </span>
+                            </a>
+                        </li>                 
+                    </ul>
+                </li>                
+            </ul>
+
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+    <div class="sidebar-footer">
+        <div class="row">
+            <div class="col-6 link-wrap">
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="" data-original-title="Settings">
+                    <i class="icon-settings"></i>
+                </a>
+            </div>            
+            <div class="col-6 link-wrap">
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="" data-original-title="Logout">
+                    <i class="icon-power"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</aside>
