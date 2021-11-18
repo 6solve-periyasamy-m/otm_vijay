ba56bd0f392da72da50ace5a2cd352028df1547e
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-18 13:43:59 +0000

    Fixed selector missing _id tag for address

--- resources/views/partials/models/addresses/selector.blade.php
+++ resources/views/partials/models/addresses/selector.blade.php
@@ -1,3 +1,3 @@
 @include('partials.fields.selector.default',
-            ['name' => 'Address', 'field' => ($prefix ?? "") . 'address', 'value' => $value,
+            ['name' => 'Address', 'field' => ($prefix ?? "") . 'address_id', 'value' => $value,
              'route' => 'addresses', 'additionalParameters' => 'customers: ' . (isset($customers) && $customers ? 'true' : 'false')])
