812629f14c81f36bcc85dbaa11cfcc080cb41824
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-18 11:18:42 +0000

    Migrated address selector to use modular selector

--- resources/views/partials/models/addresses/selector.blade.php
+++ resources/views/partials/models/addresses/selector.blade.php
@@ -1,28 +1,3 @@
-@push('head-stack')
-<script type="text/javascript">
-    $(document).ready(function () {
-        let {{ $prefix ?? "" }}addressSelect = $('#{{ $prefix ?? "" }}address_id-input');
-        {{ $prefix ?? "" }}addressSelect.select2({
-            ajax: {
-                url: '{{ route('api.addresses.select') }}',
-                data: function (params) {
-                    return {filter: params.term, customers: {{ isset($customers) && $customers ? "true" : "false" }},};
-                }
-            }
-        });
-        $.ajax({url: '{{ route('api.addresses.selected', ['id' => $id ?? 0, ]) }}',})
-            .then(function (data) {
-                {{ $prefix ?? "" }}addressSelect.append(new Option(data.text, data.id, true, true)).trigger('change');
-
-                {{ $prefix ?? "" }}addressSelect.trigger({
-                    type: 'select2:select',
-                    params: {data: data,}
-                });
-            });
-    });
-</script>
-@endpush
-<div class="form-group col-12">
-    <label for="{{ $prefix ?? "" }}address_id-input">Address</label>
-    <select name="{{ $prefix ?? "" }}address_id" class="form-control" id="{{ $prefix ?? "" }}address_id-input"></select>
-</div>
+@include('partials.fields.selector.default',
+            ['name' => 'Address', 'field' => ($prefix ?? "") . 'address', 'value' => $id,
+             'route' => 'addresses', 'additionalParameters' => 'customers: ' . (isset($customers) && $customers ? 'true' : 'false')])
