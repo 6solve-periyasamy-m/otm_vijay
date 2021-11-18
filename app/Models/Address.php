78d918f3e73dda826200ba569d6e92f072a2b084
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-18 13:42:32 +0000

    Added address parent id as fillable in address

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -11,7 +11,7 @@ class Address extends Model
     use HasFactory;
     use SoftDeletes;
 
-    protected $fillable = ['name','location_type_id','address_line_1','address_line_2','address_line_3','town','region','country_id','postcode',];
+    protected $fillable = ['name','address_parent_id','location_type_id','address_line_1','address_line_2','address_line_3','town','region','country_id','postcode',];
 
     public function locationType()
     {
