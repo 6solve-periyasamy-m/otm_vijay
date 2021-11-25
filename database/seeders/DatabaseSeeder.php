23d4b4854629021c0a791eccfaa56e691dd7dd57
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-25 15:40:42 +0000

    Added UserSeeder to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             AddressParent::create(['id' => $key, 'name' => $value]);
         }
         Artisan::call('countries:update');
+        $this->call(UserSeeder::class);
 
         if (config('app.debug')) {
             $this->call(LocationTypesTableSeeder::class);
