748c85a3b80fe7460217197924f2a6951c264de2
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-26 13:15:55 +0000

    Added booking confirmation email

---
+++ app/Repository/MailRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repository;
+
+use App\Models\Address;
+use App\Models\Country;
+use App\Models\Currency;
+use App\Models\EmailTemplate;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+interface MailRepositoryInterface
+{
+    public static function getBookingConfirmationBody(Order $order);
+    public static function getEmailTemplate(string $name);
+}
+
+class MailRepository implements MailRepositoryInterface
+{
+    public static function getBookingConfirmationBody(Order $order)
+    {
+        $fillables = OrderRepository::getOrderShortCodes($order);
+        $template = self::getEmailTemplate('booking.confirmation')->body;
+        foreach ($fillables as $key => $value) {
+            $template = str_replace('['.$key.']', $value, $template);
+        }
+        return $template;
+    }
+
+    public static function getEmailTemplate(string $name)
+    {
+        $template = EmailTemplate::where('name', '=', $name)->first();
+        if (!isset($template)) {
+            $template = EmailTemplate::create(['name' => $name,]);
+        }
+        return $template;
+    }
+}
