04738336bc9963781d45b907a978d1f18053ecf7
Celeste Gateley <celeste.gateley@gmail.com>
2021-11-29 10:34:05 +0000

    Made OrderController store method raise OrderCreated event

--- app/Http/Controllers/Models/OrderController.php
+++ app/Http/Controllers/Models/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Models;
 
+use App\Events\OrderCreated;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\OrderCustomer;
@@ -43,6 +44,7 @@ class OrderController extends Controller
         $order->booking_reference = Order::generateBookingReference($order);
         $order->save();
         OrderRepository::addIncludedToCustomer($orderCustomer, $order);
+        event(new OrderCreated($order));
         return redirect()->route('orders.view', ['order' => $order,]);
     }
 
