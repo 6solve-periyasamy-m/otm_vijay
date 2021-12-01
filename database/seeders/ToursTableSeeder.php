c0c93250e994db1df482e52d75d238c5a4d1ca22
Nicholas Alexander <nalex@sfsw.net>
2021-12-01 21:26:02 +0000

    add tour to seeder

--- database/seeders/ToursTableSeeder.php
+++ database/seeders/ToursTableSeeder.php
@@ -42,6 +42,29 @@ class ToursTableSeeder extends Seeder
                 'updated_at' => '2021-11-22 13:31:25',
                 'deleted_at' => NULL,
             ),
+            1 => 
+            array (
+                'id' => 2,
+                'event_id' => 2,
+                'name' => 'The World Cup',
+                'description' => 'The Rugby World Cup 2022',
+                'notes' => NULL,
+                'base_price_per_person' => 4550.0,
+                'margin' => 25.0,
+                'single_occupancy_surcharge' => 130.0,
+                'deposit' => 1500.0,
+                'stock_control_active' => 1,
+                'stock' => 50,
+                'booking_form_url' => 'world-cup',
+                'tour_colour_id' => NULL,
+                'tour_merchandise_id' => NULL,
+                'is_active' => 1,
+                'date_from' => '2022-02-01',
+                'date_to' => '2022-02-23',
+                'created_at' => '2021-11-22 13:28:39',
+                'updated_at' => '2021-11-22 13:31:25',
+                'deleted_at' => NULL,
+            ),
         ));
         
         
