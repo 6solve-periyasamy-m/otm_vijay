4b584600c5058a63d5bd53a937095cef254c6f10
Celeste Gateley <celeste.gateley@gmail.com>
2021-12-06 12:22:24 +0000

    Added permission checking on activity views

--- resources/views/partials/models/activities/row.blade.php
+++ resources/views/partials/models/activities/row.blade.php
@@ -5,15 +5,27 @@
     <td>{{ $description }}</td>
     <td>{{ $notes }}</td>
     <td class="actions">
-        <a href="{{route('activities.edit', ['activity' => $activity,])}}" class="btn btn-sm btn-outline-success mb-1">
+        @can('edit', \App\Models\Activity::class)
+            <a href="{{route('activities.edit', ['activity' => $activity,])}}" class="btn btn-sm btn-outline-success mb-1">
+                <i class="icon-note"></i>
+            </a>
+        @else
+            <span class="btn btn-outline-dark btn-sm mb-1">
             <i class="icon-note"></i>
-        </a>
-        <a href="#" class="btn btn-sm btn-outline-danger mb-1"
-           onclick="event.preventDefault();document.getElementById('activity-{{ $activity->id }}-delete').submit();">
+        </span>
+        @endcan
+        @can('delete', \App\Models\Activity::class)
+            <a href="#" class="btn btn-sm btn-outline-danger mb-1"
+               onclick="event.preventDefault();document.getElementById('activity-{{ $activity->id }}-delete').submit();">
+                <i class="icon-trash"></i>
+            </a>
+            <form id="activity-{{ $activity->id }}-delete"
+                  action="{{ route('activities.delete', ['activity' => $activity,]) }}" method="POST"
+                  style="display: none;">{{ csrf_field() }}</form>
+        @else
+            <span class="btn btn-outline-dark btn-sm mb-1">
             <i class="icon-trash"></i>
-        </a>
-        <form id="activity-{{ $activity->id }}-delete"
-              action="{{ route('activities.delete', ['activity' => $activity,]) }}" method="POST"
-              style="display: none;">{{ csrf_field() }}</form>
+        </span>
+        @endcan
     </td>
 </tr>
