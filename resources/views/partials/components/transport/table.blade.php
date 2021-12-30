e977499fdb21ae309e684a5a1164b6fd91d7d0eb
Celeste Gateley <celeste.gateley@gmail.com>
2021-12-30 13:20:00 +0000

    Added stock to views

--- resources/views/partials/components/transport/table.blade.php
+++ resources/views/partials/components/transport/table.blade.php
@@ -46,9 +46,12 @@ $(document).ready(function() {
                     <input type="checkbox" disabled @if($transportInventory->arrival_time_confirmed == 1) checked @endif>
                 </td>
                 <td>
-                    <input type="checkbox" disabled @if($flightInventory->fit_selectable == 1) checked @endif>
+                    <input type="checkbox" disabled @if($transportInventory->fit_selectable == 1) checked @endif>
                 </td>
-                <td>{{ $transportInventory->stock }}</td>
+                <td>
+                    {{$transportInventory->stock - $transportInventory->getUsedStock()}}/{{ $transportInventory->stock }}<br/>
+                    ({{$transportInventory->getUsedStock()}} Sold)
+                </td>
                 <td>{{ StringFormatter::formatCurrency($transportInventory->purchase_price) }}</td>
                 <td>{{ StringFormatter::formatCurrency($transportInventory->sales_price) }}</td>
                 <td>{{ $transportInventory->notes }}</td>
