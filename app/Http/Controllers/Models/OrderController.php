22a178de156ee1c613f6e13cef1d34880198891a
Celeste Gateley <celeste.gateley@gmail.com>
2022-01-23 12:06:29 +0000

    Fixed missing implementation of order installment cloning

--- app/Http/Controllers/Models/OrderController.php
+++ app/Http/Controllers/Models/OrderController.php
@@ -50,6 +50,7 @@ class OrderController extends Controller
         $order->booking_reference = Order::generateBookingReference($order);
         $order->save();
         OrderRepository::addIncludedToCustomer($orderCustomer);
+        OrderRepository::cloneInstallments($order);
         event(new OrderCreatedEvent($order));
         event(new OrderCustomerCreatedEvent($orderCustomer, false));
         return redirect()->route('orders.view', ['order' => $order,]);
