229a53f43e431fd4a32fdea56665f61f0b2c008c
Celeste Gateley <celeste.gateley@gmail.com>
2022-01-24 14:44:16 +0000

    Fix for pulling incorrect request input for invoice_footer

--- app/Http/Controllers/Models/TourController.php
+++ app/Http/Controllers/Models/TourController.php
@@ -39,7 +39,7 @@ class TourController extends Controller
             'deposit' => $request->input('deposit'),
             'is_active' => $request->input('is_active') === 'on' ? 1 : 0,
             'notes' => $request->input('notes'),
-            'invoice_footer' => $request->input('deposit'),
+            'invoice_footer' => $request->input('invoice_footer'),
         ]);
         return redirect()->route('tours.view', ['tour' => $tour,]);
     }
@@ -73,7 +73,7 @@ class TourController extends Controller
             'tour_category_id' => $request->input('tour_category_id'),
             'is_active' => $request->input('is_active') === 'on' ? 1 : 0,
             'notes' => $request->input('notes'),
-            'invoice_footer' => $request->input('deposit'),
+            'invoice_footer' => $request->input('invoice_footer'),
         ]);
         return redirect()->route('tours.view', ['tour' => $tour,]);
     }
