1f86fd9ba2aa393a31601cd3de2891a1eeb99fdf
Celeste Gateley <celeste.gateley@gmail.com>
2022-01-24 14:13:59 +0000

    Enforced uniqueness for customer emails in validation

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -28,7 +28,7 @@ class Customer extends Model
             'last_name' => 'required',
             'date_of_birth' => 'required|date',
             'mobile_number' => 'required',
-            'email_address' => 'required|email',
+            'email_address' => 'required|email|unique:customers,email_address',
             'gender' => 'required',
             'emergency_contact_name' => 'required',
             'emergency_contact_relationship' => 'required',
