ef6322602e10826c61839c3b7a72224f2d2a1499
vagrant <vagrant@homestead>
2022-01-30 12:41:10 +0000

    fix query sequence

--- app/Repository/FlightsRepository.php
+++ app/Repository/FlightsRepository.php
@@ -51,8 +51,8 @@ class FlightsRepository implements FlightsRepositoryInterface
             'flight_inventory_tours.tour_component_type',
             'flights.available_after')
         ->join('airlines', 'airline_id', 'airlines.id')
-        ->join('travel_classes', 'flight_inventories.travel_class_id', 'travel_classes.id')
         ->join('flight_inventories', 'flight_inventories.flight_id', 'flights.id')
+        ->join('travel_classes', 'flight_inventories.travel_class_id', 'travel_classes.id')
         ->join('flight_inventory_tours', 'flight_inventory_tours.flight_inventory_id', 'flight_inventories.id')
         ->whereNull('flight_inventory_tours.deleted_at')
         ->whereNull('flight_inventories.deleted_at')
