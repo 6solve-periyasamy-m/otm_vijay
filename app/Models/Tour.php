59923c74dae71f09bbd9e830baeb9d875c487aaa
Celeste Gateley <celeste.gateley@gmail.com>
2022-01-30 11:59:19 +0000

    Fix for final payment not being fillable

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -12,7 +12,7 @@ class Tour extends Model
     use HasFactory;
     use SoftDeletes;
 
-    protected $fillable = ['event_id', 'name', 'description', 'date_from', 'date_to', 'base_price_per_person', 'margin', 'single_occupancy_surcharge', 'stock_control_active', 'stock', 'deposit', 'booking_form_url', 'tour_category_id', 'is_active', 'notes', 'invoice_footer'];
+    protected $fillable = ['event_id', 'name', 'description', 'date_from', 'date_to', 'base_price_per_person', 'margin', 'single_occupancy_surcharge', 'stock_control_active', 'stock', 'deposit', 'booking_form_url', 'tour_category_id', 'is_active', 'notes', 'invoice_footer', 'final_payment'];
     protected $casts = ['date_from' => 'date', 'date_to' => 'date', 'final_payment' => 'date'];
 
     public static function getValidationRules()
