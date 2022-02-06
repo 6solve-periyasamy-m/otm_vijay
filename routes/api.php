4a8df76751c0c61c5fca00b9e43869578a103751
Nicholas Alexander <work@sfsw.net>
2022-02-06 10:42:31 +0000

    mark deprecated function (commented until tested)

--- routes/api.php
+++ routes/api.php
@@ -68,7 +68,7 @@ Route::prefix('booking')->group(function () {
     Route::get('/airports', [AirlinesController::class, 'getAirports']);
 
     // Travellers
-    Route::get('/tourparty', [CustomerController::class, 'getTravellers']);
+    // DEPRECATE Route::get('/tourparty', [CustomerController::class, 'getTravellers']);
     Route::get('/customer/{token}', [CustomerController::class, 'getCustomerByToken']);
     Route::get('/travellers/{token}', [BookingCustomerController::class, 'loadTravellers']);
 
