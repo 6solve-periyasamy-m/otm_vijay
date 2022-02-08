4b1d90bb1ba7c35960a6885913d813a824552eea
Celeste Gateley <celeste.gateley@gmail.com>
2022-02-08 16:45:54 +0000

    Updated SettingsTableSeeder to include new email and subject templates

--- database/seeders/SettingsTableSeeder.php
+++ database/seeders/SettingsTableSeeder.php
@@ -91,6 +91,95 @@ class SettingsTableSeeder extends Seeder
             ),
             14 => 
             array (
+                'key' => 'email.additional-traveller-added.subject',
+                'value' => 'You have been added as an additional traveller on [TOUR_NAME]',
+            ),
+            15 => 
+            array (
+                'key' => 'email.additional-traveller-added.template',
+                'value' => '<p>Greetings [CUSTOMER_TITLE] [CUSTOMER_FIRST_NAME] [CUSTOMER_LAST_NAME],</p>
+
+<p>This email is to inform you that you have been added to [TOUR_NAME] by [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME]. You will not have to do anything on your end, and this email is a courtesy.</p>
+
+<p>If you believe this has been done in error, please don&#39;t hesitate to contact [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME], or, if you do not know this individual, please contact us at <a href="mailto://[SETTING_COMPANY_CONTACT_EMAIL]">[SETTING_COMPANY_CONTACT_EMAIL]</a>, and we will contact the lead booker to get this sorted!</p>
+
+<p>With thanks!</p>
+
+<p>[SETTING_COMPANY_NAME]</p>',
+            ),
+            16 => 
+            array (
+                'key' => 'email.additional-traveller-removed.subject',
+                'value' => 'You have been removed as an additional traveller on [TOUR_NAME]',
+            ),
+            17 => 
+            array (
+                'key' => 'email.additional-traveller-removed.template',
+                'value' => '<p>Greetings [CUSTOMER_TITLE] [CUSTOMER_FIRST_NAME] [CUSTOMER_LAST_NAME],</p>
+
+<p>This email is to inform you that you have been removed from the [TOUR_NAME] tour by [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME]. You will not have to do anything on your end, and this email is a courtesy.</p>
+
+<p>If you believe this has been done in error, please don&#39;t hesitate to contact [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME], or, if you do not know this individual, please contact us at <a href="mailto://[SETTING_COMPANY_CONTACT_EMAIL]">[SETTING_COMPANY_CONTACT_EMAIL]</a>, and we will contact the lead booker to get this sorted!</p>
+
+<p>With thanks!</p>
+
+<p>[SETTING_COMPANY_NAME]</p>',
+            ),
+            18 => 
+            array (
+                'key' => 'email.booking-confirmation.subject',
+                'value' => 'Booking confirmed for Order [BOOKING_REFERENCE] on Tour [TOUR_NAME]',
+            ),
+            19 => 
+            array (
+                'key' => 'email.booking-confirmation.template',
+                'value' => '<p>Greetings [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME],</p>
+
+<p>Congratulations on your booking for the tour [TOUR_NAME]. This order is referenced with the booking code [BOOKING_REFERENCE]. To find out more, or make payments, go to our <a href="[PORTAL_LINK]">portal</a>.</p>
+
+<p>Thank you, and enjoy your tour!</p>
+
+<p>[SETTING_COMPANY_NAME]</p>',
+            ),
+            20 => 
+            array (
+                'key' => 'email.order-cancelled.subject',
+                'value' => 'Order [BOOKING_REFERENCE]: Your order has been cancelled',
+            ),
+            21 => 
+            array (
+                'key' => 'email.order-cancelled.template',
+                'value' => '<p>Greetings [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME],</p>
+
+<p>This email is to let you know that your order [BOOKING_REFERENCE] to go on our [TOUR_NAME] tour has been cancelled. If you believe this to have been done in error, please contact us at <a href="mailto://[SETTING_COMPANY_EMAIL]">[SETTING_COMPANY_EMAIL]</a>.</p>
+
+<p>Hoping to see you again soon!</p>
+
+<p>[SETTING_COMPANY_NAME]</p>',
+            ),
+            22 => 
+            array (
+                'key' => 'email.order-changed.subject',
+                'value' => 'Order [BOOKING_REFERENCE]: A change has been made to your order',
+            ),
+            23 => 
+            array (
+                'key' => 'email.order-changed.template',
+                'value' => '<p>Greetings [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME],</p>
+
+<p>This email is to inform you that some part of your order has changed, and more details can be found on our <a href="[PORTAL_LINK]">portal</a>. If you require assistance, please do not hesitate to contact us at <a href="mailto://[SETTING_COMPANY_CONTACT_EMAIL]">[SETTING_COMPANY_CONTACT_EMAIL]</a>, and we will be happy to help you where we can!</p>
+
+<p>With thanks!</p>
+
+<p>[SETTING_COMPANY_NAME]</p>',
+            ),
+            24 => 
+            array (
+                'key' => 'email.payment-due.subject',
+                'value' => 'Order [BOOKING_REFERENCE]: Payment due on [DUE_PAYMENT_DATE]',
+            ),
+            25 => 
+            array (
                 'key' => 'email.payment-due.template',
                 'value' => '<p>Dear [TITLE] [FIRST_NAME] [LAST_NAME],</p>
 
@@ -102,7 +191,12 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            15 => 
+            26 => 
+            array (
+                'key' => 'email.payment-made.subject',
+                'value' => 'Order [BOOKING_REFERENCE]: Payment accepted',
+            ),
+            27 => 
             array (
                 'key' => 'email.payment-made.template',
                 'value' => '<p>Dear [TITLE] [FIRST_NAME] [LAST_NAME],</p>
@@ -115,7 +209,30 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            16 => 
+            28 => 
+            array (
+                'key' => 'email.payment-overdue.subject',
+                'value' => 'Order [BOOKING_REFERENCE]: Payment overdue since [DUE_PAYMENT_DATE]',
+            ),
+            29 => 
+            array (
+                'key' => 'email.payment-overdue.template',
+                'value' => '<p>Dear [TITLE] [FIRST_NAME] [LAST_NAME],</p>
+
+<p>You have a payment due on [DUE_PAYMENT_DATE], for [DUE_PAYMENT_AMOUNT]. This payment is now overdue, and if you do not pay soon, then your order will be camcelled.</p>
+
+<p>If you have any issues, please contact us and we can work towards a solution.</p>
+
+<p>With thanks!</p>
+
+<p>[SETTING_COMPANY_NAME]</p>',
+            ),
+            30 => 
+            array (
+                'key' => 'email.refund-given.subject',
+                'value' => 'Order [BOOKING_REFERENCE]: Refund given',
+            ),
+            31 => 
             array (
                 'key' => 'email.refund-given.template',
                 'value' => '<p>Dear [TITLE] [FIRST_NAME] [LAST_NAME],</p>
@@ -128,12 +245,12 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            17 =>
+            32 => 
             array (
                 'key' => 'system.format.date',
                 'value' => 'd/m/Y',
             ),
-            18 =>
+            33 => 
             array (
                 'key' => 'system.format.time',
                 'value' => 'H:i',
