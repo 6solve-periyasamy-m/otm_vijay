e3e1242d07fa93ff644a90e5094a23d5216fc24d
Celeste Gateley <celeste.gateley@gmail.com>
2022-02-08 10:54:23 +0000

    Implement string formatting for shortcodes

--- app/Repository/ShortCodeRepository.php
+++ app/Repository/ShortCodeRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Models\Order;
 use App\Models\Payment;
 use Faker\Factory as Faker;
+use StringFormatter as Formatter;
 
 class ShortCodeRepository
 {
@@ -20,19 +21,19 @@ class ShortCodeRepository
             'FIRST_NAME' => !isset($order) ? $faker->firstName : $customer->first_name,
             'MIDDLE_NAMES' => !isset($order) ? $faker->firstName : $customer->middle_names,
             'LAST_NAME' => !isset($order) ? $faker->lastName : $customer->last_name,
-            'PASSPORT_EXPIRY_DATE' => !isset($order) ? $faker->date : $customer->passport_expiry_date,
+            'PASSPORT_EXPIRY_DATE' => Formatter::formatDate(!isset($order) ? $faker->date : $customer->passport_expiry_date),
             'BOOKING_REFERENCE' => !isset($order) ? $faker->regexify('OTM[0-9]{12}[A-Z]{4}') : $order->booking_reference,
-            'ORDERED_ON' => !isset($order) ? $faker->date : $order->ordered_on,
-            'TOTAL_PAID' => !isset($order) ? $faker->numberBetween(100, 1000) : OrderRepository::getTotalPaid($order),
-            'DUE_PAYMENT_AMOUNT' => !isset($order) ? $faker->numberBetween(100, 1000) : $nextPayment['amount'],
-            'DUE_PAYMENT_DATE' => !isset($order) ? $faker->date : $nextPayment['due'],
+            'ORDERED_ON' => Formatter::formatDate(!isset($order) ? $faker->date : $order->ordered_on),
+            'TOTAL_PAID' => Formatter::formatCurrency(!isset($order) ? $faker->numberBetween(100, 1000) : OrderRepository::getTotalPaid($order)),
+            'DUE_PAYMENT_AMOUNT' => Formatter::formatCurrency(!isset($order) ? $faker->numberBetween(100, 1000) : $nextPayment['amount']),
+            'DUE_PAYMENT_DATE' => Formatter::formatDate(!isset($order) ? $faker->date : $nextPayment['due']),
             'TOUR_NAME' => !isset($order) ? implode(' ', $faker->words) : $tour->name,
             'TOUR_DESCRIPTION' => !isset($order) ? $faker->sentence : $tour->description,
-            'TOUR_START' => !isset($order) ? $faker->date : $tour->date_from,
-            'TOUR_END' => !isset($order) ? $faker->date : $tour->date_to,
-            'TOUR_BASE_PER_PERSON' => !isset($order) ? $faker->numberBetween(100, 1000) : $tour->base_price_per_person,
-            'TOUR_DEPOSIT' => !isset($order) ? $faker->numberBetween(100, 1000) : $tour->deposit,
-            'TOUR_SURCHARGE' => !isset($order) ? $faker->numberBetween(100, 1000) : $tour->single_occupancy_surcharge,
+            'TOUR_START' => Formatter::formatDate(!isset($order) ? $faker->date : $tour->date_from),
+            'TOUR_END' => Formatter::formatDate(!isset($order) ? $faker->date : $tour->date_to),
+            'TOUR_BASE_PER_PERSON' => Formatter::formatDate(!isset($order) ? $faker->numberBetween(100, 1000) : $tour->base_price_per_person),
+            'TOUR_DEPOSIT' => Formatter::formatDate(!isset($order) ? $faker->numberBetween(100, 1000) : $tour->deposit),
+            'TOUR_SURCHARGE' => Formatter::formatDate(!isset($order) ? $faker->numberBetween(100, 1000) : $tour->single_occupancy_surcharge),
             'LATEST_INVOICE' => !isset($order) ? $faker->url : route('orders.invoice.latest', ['order' => $order,]), // TODO: Link to customers invoices
             'PORTAL_LINK' => !isset($order) ? $faker->url : route('customer.portal', ['customer' => $customer,]),
             'ATOL_LINK' => !isset($order) ? $faker->url : route('customer.atol', ['customer' => $customer,]),
@@ -46,8 +47,8 @@ class ShortCodeRepository
     {
         $faker = Faker::create();
         return array_merge([
-            'PAYMENT_AMOUNT' => !isset($payment) ? $faker->numberBetween(100, 1000) : $payment->amount,
-            'PAYMENT_DATE' => !isset($payment) ? $faker->date : $payment->paid_on,
+            'PAYMENT_AMOUNT' => Formatter::formatCurrency(!isset($payment) ? $faker->numberBetween(100, 1000) : $payment->amount),
+            'PAYMENT_DATE' => Formatter::formatDate(!isset($payment) ? $faker->date : $payment->paid_on),
             'PAYMENT_METHOD' =>!isset($payment) ? 'Demo Payment Method' :  $payment->paymentMethod->name,
             'PAYMENT_TYPE' => !isset($payment) ? 'Demo Payment Type' : $payment->payment_type
         ], self::getOrderShortCodes(isset($payment) ? $payment->order : null));
