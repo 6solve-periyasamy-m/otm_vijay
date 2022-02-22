8c2e38f635d04b6702d6622fbb5a2ea44574ea83
Celeste Gateley <celeste.gateley@gmail.com>
2022-02-22 15:43:07 +0000

    Fix for customer report

--- app/Repository/BespokeReportRepository.php
+++ app/Repository/BespokeReportRepository.php
@@ -69,10 +69,7 @@ class BespokeReportRepository
 
     public static function processOrderComponents(array $used, array $available, bool $format = false): array
     {
-        $data = [];
-        foreach (OrderAccommodation::all() as $row) {
-            $data[] = self::processLowest($row, 'customer', 'orderCustomer', $used, $available, $format);
-        }
+        $data = self::processAccommodation($used, $available, $format);
         foreach (OrderActivity::all() as $row) {
             $data[] = self::processLowest($row, 'customer', 'orderCustomer', $used, $available, $format);
         }
@@ -173,5 +170,37 @@ class BespokeReportRepository
                 break;
         }
         return $data;
+    }
+
+    private static function processAccommodation(array $used, array $available, bool $format)
+    {
+        $rows = [];
+        foreach (OrderAccommodation::all() as $row) {
+            foreach ($row->group->orderCustomers as $objParent) {
+                $objGrandparent = $objParent->customer;
+                $data = [];
+                foreach ($available as $key => $info) {
+                    if (in_array($key, $used)) {
+                        $field = 'Not Set';
+                        if ($info->depth == 0) {
+                            $field = $objGrandparent->{$info->accessor};
+                        }
+                        if ($info->depth == 1) {
+                            $field = $objParent->{$info->accessor};
+                        }
+                        if ($info->depth == 2) {
+                            $field = $row->{$info->accessor};
+                        }
+                        if ($format) {
+                            $field = self::format($field, $info->format);
+                        }
+                        $data[] = $field;
+                    }
+                }
+                $rows[] = $data;
+            }
+        }
+
+        return $rows;
     }
 }
