1b5997860ae62fdc2d0ecf29dfbf4c4cbbca483d
vagrant <vagrant@homestead>
2022-02-22 20:44:00 +0000

    larger text and less text for activities and transports

--- resources/assets/js/components/BookingFormActivity.vue
+++ resources/assets/js/components/BookingFormActivity.vue
@@ -11,31 +11,33 @@
             </div>
             <div class="card-body" v-if="activated">
                 <div class="listing headings">
-                    <div class="column-name">
-                        Name
+                    <div class="column-starts-at">
+                        When
                     </div>
-                    <div class="column-description">
-                        Description 
+                    <div class="column-name">
+                        Details   
+                    </div>
+                    <div class="column-address">
+                        Address 
                     </div>
                     <div class="column-ticket-type">
                         Ticket type
                     </div>
-                    <div class="column-starts-at">
-                        When
-                    </div>
                 </div>
                 <div class="listing" v-for="activity in activities" :key="activity.activity_inventory_tour_id">
-                    <div class="column-name">
-                        {{ activity.name }}
+                    <div class="column-starts-at">
+                        {{startDate(activity)}}
                     </div>
-                    <div class="column-description">
-                        {{ activity.description }} 
+                    <div class="column-name">
+                        {{ activity.name }}<br>{{ activity.description !== activity.name ? activity.description :''}} 
+                    </div>
+                    <div class="column-address">
+                        {{ activity.address != undefined && activity.address.length ? activity.address : '-'}}
+                        <br>
+                        {{ activity.address_region != undefined && activity.address_region.length ? activity.address_region : ''}}
                     </div>
                     <div class="column-ticket-type">
                         {{activity.ticket_type_name}}
-                    </div>
-                    <div class="column-starts-at">
-                        {{startDate(activity)}}
                     </div>
                 </div>
             </div>
@@ -126,6 +128,7 @@ export default {
     .column-description {
         width: 24rem;
     }
+    .column-address, 
     .column-notes {
         width: 12rem;
     }
--- resources/assets/js/components/BookingFormTransport.vue
+++ resources/assets/js/components/BookingFormTransport.vue
@@ -9,49 +9,19 @@
                     <p>Form section for booking or confirming ground transport including options available to support activities</p>
                 </h5>
             </div>
-            <div  v-if="activated" class="card-body compress">
+            <div  v-if="activated" class="card-body">
                 <div class="transports">
                     <div class="transport">
                         <div class="listing headings">
-                            <div class="heading name">
-                                Name
-                            </div>
-                            <div class="heading description">
-                                Description
-                            </div>
-                            <div class="heading tour_component_type">
-                                Transport Type
-                            </div>
-                            <div class="heading is_domestic">
-                                Domestic/International
-                            </div>
-                            <div class="heading address_from">
-                                From
-                            </div>
-                            <div class="heading address_to">
-                                To
-                            </div>
+                            <div class="heading address_from"> When </div>
+                            <div class="heading name"> Name </div>
+                            <div class="heading tour_component_type"> Transport Type </div>
                         </div>
                         <div v-for="transport in transports">
                             <div class="listing">
-                                <div class="name">
-                                    {{transport.name}}
-                                </div>
-                                <div class="description">
-                                    {{transport.description}}
-                                </div>
-                                <div class="transport_type">
-                                    {{transport.transport_type_name}}
-                                </div>
-                                <div class="is_domestic">
-                                    {{transport.is_domestic ? 'Domestic' : 'International'}}
-                                </div>
-                                <div class="address_from">
-                                    {{transport.departure_address}}<br>Departure {{datetime(transport.departs_at)}}
-                                </div>
-                                <div class="address_to">
-                                    {{transport.arrival_address}}<br>Arrival {{datetime(transport.arrives_at)}}
-                                </div>
+                                <div class="address_from"> {{datetime(transport.departs_at)}} <br>{{transport.departure_address}} </div>
+                                <div class="name"> {{transport.name}} </div>
+                                <div class="transport_type"> {{ transport.transport_type_name != undefined && transport.transport_type_name.length ? transport.transport_type_name : '-' }} </div>
                             </div>
                         </div>
                     </div>
@@ -124,7 +94,7 @@ export default {
         margin: 1rem;
     }
     .listing.headings div {
-        text-align: center;
+        text-align: left;
     } 
     .listing .name {
         width: 12rem;
