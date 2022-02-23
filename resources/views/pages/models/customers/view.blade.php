43926dfb56d5c4f7c8158a15fe426290df340757
Celeste Gateley <celeste.gateley@gmail.com>
2022-02-23 11:49:55 +0000

    Adjusted information on customer details screen

--- resources/views/pages/models/customers/view.blade.php
+++ resources/views/pages/models/customers/view.blade.php
@@ -46,7 +46,7 @@
                     </div>
                     <div class="col-12">
                         <p>Passport Details</p>
-                        <h6 class="fw-bold">{{ $customer->passport_first_name }} {{ $customer->passport_middle_names }} {{ $customer->passport_last_name }}, {{ $customer->passport_number }}, {{ $customer->passport_issue_date }} to {{ $customer->passport_expiry_date }}</h6>
+                        <h6 class="fw-bold">{{ $customer->passport_first_name }} {{ $customer->passport_middle_names }} {{ $customer->passport_last_name }}, {{ $customer->passport_number }}, Expires {{ StringFormatter::formatDate($customer->passport_expiry_date) }}</h6>
                     </div>
                     <div class="col-12">
                         <p>Emergency Contact</p>
@@ -82,8 +82,8 @@
                     <tr>
                         <th scope="row"><a href="{{ route('orders.view', ['order' => $orderCustomer->order,]) }}">{{ $orderCustomer->order->booking_reference }}</a></th>
                         <td>{{ $orderCustomer->order->tour->name }}</td>
-                        <td>{{ $orderCustomer->order->ordered_on }}</td>
-                        <td>{{ $orderCustomer->tour_cost }}</td>
+                        <td>{{ StringFormatter::formatDateTime($orderCustomer->order->ordered_on) }}</td>
+                        <td>{{ StringFormatter::formatCurrency($orderCustomer->tour_cost) }}</td>
                     </tr>
                 @endforeach
             </table>
