371ef28ebb222d05cc4c6e4efe5e532367a46dae
Celeste Gateley <celeste.gateley@gmail.com>
2022-02-24 15:03:09 +0000

    Fix for too few arguments error

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -140,7 +140,7 @@ class BookingController extends Controller
         $bookingRepository->setStatusDepositCheckout($booking);
         Log::info('Booking: sending deposit request to StripeGateway:', [[['name' => "Deposit for Booking from $customer->full_name", 'quantity' => 1, 'cost' => $amount]], $booking->token,'Deposit']);
 
-        return StripeGateway::checkout([['name' => "Deposit for Booking from $customer->full_name", 'quantity' => 1, 'cost' => $amount]], $booking->token, 'Deposit');
+        return StripeGateway::checkout([['name' => "Deposit for Booking from $customer->full_name", 'quantity' => 1, 'cost' => $amount]], $booking->token, 'Deposit', $customer->id);
     }
 
     /**
