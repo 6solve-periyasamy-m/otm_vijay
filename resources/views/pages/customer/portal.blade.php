94807f9003dba0686cc8a3896fd4939d15a1b938
Celeste Gateley <celeste.gateley@gmail.com>
2022-02-26 14:21:35 +0000

    Updated the portal to now link directly, rather than using sub-menus

--- resources/views/pages/customer/portal.blade.php
+++ resources/views/pages/customer/portal.blade.php
@@ -1,93 +1,46 @@
 @extends('layout.customer')
 
-@section('title', '')
+@section('title', 'Customer Portal')
 
 @section('content')
 
 <div class="d-flex justify-content-center align-items-center h-75 mt-4">
-    <div class="donut-menu">        
-        <div class="menu-item">            
-        </div>
+    <div class="donut-menu">
         <div class="menu-item">
         </div>
         <div class="menu-item">
         </div>
         <div class="menu-item">
         </div>
-        <div class="donut-center">            
+        <div class="menu-item">
+        </div>
+        <div class="donut-center">
         </div>
     </div>
     <div class="click-menu">
-        <div class="menu-item">
+        <div class="menu-item" onclick="window.location = '{{ route('customer.edit') }}'">
             <div class="menu-text">
                 <span class="icon-user menu-icon"></span>
                 <span>Your Detail</span>
             </div>
-            <ul class="sub-menu">
-                <li class="menu-link hide"></li>
-                <li class="menu-link">
-                    <a href="{{ route('customer.edit') }}">Update Details</a>
-                </li>                
-                <li class="menu-link">
-                    <a href="{{ route('customer.atol') }}">View ATOL</a>
-                </li>                
-            </ul>
         </div>
-        <div class="menu-item">
+        <div class="menu-item" onclick="window.location = '{{ route('customer.finances') }}'">
             <div class="menu-text">
                 <span class="icon-credit-card menu-icon"></span>
                 <span>Your Finance</span>
             </div>
-            <ul class="sub-menu">
-                <li class="menu-link hide"></li>
-                <li class="menu-link">
-                    <a href="{{ route('customer.finances') }}">View Balance</a>
-                </li>
-                <li class="menu-link">
-                    <a href="{{ route('customer.finances') }}">Make Payments</a>
-                </li>
-                <li class="menu-link">
-                    <a href="{{ route('customer.finances') }}">View Invoice</a>
-                </li>               
-            </ul>
         </div>
         <div class="menu-item">
             <div class="menu-text">
                 <span class="icon-diamond menu-icon"></span>
                 <span>Extra</span>
             </div>
-            <ul class="sub-menu">
-                <li class="menu-link hide"></li>
-                <li class="menu-link">
-                    <a href="#">Add-ons</a>
-                </li>                
-                <li class="menu-link">
-                    <a href="#">Upgrades</a>
-                </li>                
-            </ul>
         </div>
         <div class="menu-item">
             <div class="menu-text">
                 <span class="icon-globe menu-icon"></span>
-                <span>Your Tour</span>
+                <span>Your Tours</span>
             </div>
-            <ul class="sub-menu">
-                <li class="menu-link">
-                    <a href="#"><font-awesome-icon icon="futbol" class="menu-icon" /></a>
-                </li>
-                <li class="menu-link">
-                    <a href="#"><font-awesome-icon icon="home" class="menu-icon" /></a>
-                </li> 
-                <li class="menu-link">
-                    <a href="#"><font-awesome-icon icon="plane" class="menu-icon" /></a>
-                </li> 
-                <li class="menu-link">
-                    <a href="#"><font-awesome-icon icon="train" class="menu-icon" /></a>
-                </li> 
-                <li class="menu-link">
-                    <a href="#"><font-awesome-icon icon="list-alt" class="menu-icon" /></a>
-                </li>                
-            </ul>
         </div>
     </div>
 </div>
