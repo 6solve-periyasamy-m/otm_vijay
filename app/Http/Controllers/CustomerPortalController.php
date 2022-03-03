e1dfdf968c5c5010f6d56dedde58ac7d64b8d94c
vagrant <vagrant@homestead>
2022-03-03 21:57:23 +0000

    fix merge conflict

--- app/Http/Controllers/CustomerPortalController.php
+++ app/Http/Controllers/CustomerPortalController.php
@@ -71,10 +71,6 @@ class CustomerPortalController extends Controller
         if ($amount > $order->remaining) {
             return back()->withErrors('Cannot pay more than you owe');
         }
-<<<<<<< HEAD
-        return StripeGateway::checkout([['name' => "Installment Payment ({$order->booking_reference})", 'quantity' => 1, 'cost' => $amount]], $order->booking_reference, 'Installment', $this->getCustomer()->id);
-=======
         return StripeGateway::checkout([['name' => "Installment Payment ({$order->booking_reference})", 'quantity' => 1, 'cost' => $amount]], $order, 'Installment', $this->getCustomer()->id);
->>>>>>> main
     }
 }
