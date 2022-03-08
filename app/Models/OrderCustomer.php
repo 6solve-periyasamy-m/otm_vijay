309494214690ba744be66f0a72f81e7b954b3164
Celeste Gateley <celeste.gateley@gmail.com>
2022-03-08 19:30:25 +0000

    Fix for cascading soft deletes with OrderAccommodation and OrderCustomer

--- app/Models/OrderCustomer.php
+++ app/Models/OrderCustomer.php
@@ -15,7 +15,7 @@ class OrderCustomer extends Model
     use SoftDeletes, CascadeSoftDeletes;
 
     protected $fillable = ['order_id', 'customer_id', 'tour_cost', 'single_occupancy_surcharge', 'travel_insurer', 'policy_number',];
-    protected $cascadeDeletes = ['orderAccommodation', 'orderActivities', 'orderFlights', 'orderTransports', 'adjustments'];
+    protected $cascadeDeletes = ['orderCustomerGroups', 'orderActivities', 'orderFlights', 'orderTransports', 'adjustments'];
     public $additional_attributes = ['booking_reference', 'ordered_on', 'lead_booker_name', 'is_lead_booker', 'customer_name'];
 
     public static function getValidationRules()
@@ -35,6 +35,11 @@ class OrderCustomer extends Model
     public function customer()
     {
         return $this->belongsTo(Customer::class);
+    }
+
+    public function orderCustomerGroups()
+    {
+        return $this->hasMany(OrderCustomerGroup::class, 'order_customer_id');
     }
 
     public function orderAccommodation()
