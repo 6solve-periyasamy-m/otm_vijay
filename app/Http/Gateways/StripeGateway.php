7a3fe70a694aa9bf3b2b855b57194411e24e7646
Celeste Gateley <celeste.gateley@gmail.com>
2022-03-10 22:05:13 +0000

    Implemented idempotency for payments and switched to listening for charge succeeded instead of checkout successful

--- app/Http/Gateways/StripeGateway.php
+++ app/Http/Gateways/StripeGateway.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Gateways;
 
+use App\Models\Customer;
 use App\Models\Order;
+use App\Models\PaymentIntention;
+use App\Repository\SettingsRepository;
 use Stripe\Checkout\Session;
+use Stripe\PaymentIntent;
 
 class StripeGateway extends Gateway
 {
     public static function checkout(array $items, Order $order, string $paymentType, int $customerId)
     {
         $lineItems = [];
+        $amount = 0;
         foreach ($items as $item) {
             $lineItems[] = [
                 'price_data' => [
@@ -21,14 +26,19 @@ class StripeGateway extends Gateway
                 ],
                 'quantity' => $item['quantity'],
             ];
+            $amount += ($item['cost'] * 100);
         }
+        $intention = PaymentIntention::build(Customer::find($customerId), $order->booking_reference, $paymentType);
         $session = Session::create([
             'line_items' => $lineItems,
             'mode' => 'payment',
+            'payment_intent_data' => [
+                'metadata' => [
+                    'intention_id' => $intention->id,
+                ],
+            ],
             'metadata' => [
-                'payment_type' => $paymentType,
-                'booking_reference' => $order->booking_reference,
-                'customer_id' => $customerId,
+                'intention_id' => $intention->id,
             ],
             'success_url' => route('payment.gateway.stripe.success'),
             'cancel_url' => route('payment.gateway.stripe.cancelled'),
