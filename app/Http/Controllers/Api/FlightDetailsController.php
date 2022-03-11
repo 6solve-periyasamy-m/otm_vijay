d39fc0c753c1305dcebb87b1b10c8ab2abe45f0e
Nicholas Alexander <work@sfsw.net>
2022-03-11 07:39:06 +0000

    try moving function

--- app/Http/Controllers/Api/FlightDetailsController.php
+++ app/Http/Controllers/Api/FlightDetailsController.php
@@ -84,61 +84,7 @@ class FlightDetailsController extends ApiController
      */
     public function bookFlights(Request $request)
     {
-        Log::info('bookFlights');
-        // validation
-        $validated = $request->validate([
-            'customer_id' => 'required',
-            'tour_id' => 'required',
-            'flight_type' => 'required',
-            'custom' => 'required',
-            'token' => 'required'
-        ]);
-        Log::info('bookFlights post Validation');
-        $customer_id = $request->customer_id;
-        $tour_id = $request->tour_id;
-        $flight_type = $request->flight_type;
-        $custom = $request->custom;
-        $booking_token = $request->token;
-        // token is posted: why look it up?  opportunity to catch a false post?
-        $token = $_COOKIE['OTM_booking_token'];
-        if ($token !== $booking_token) {
-            throw new \Exception('Booking token mismatch');
-        }
-
-        $this->logging == 'flights' && Log::debug('***** bookFlightDetails parameters:', [$customer_id, $tour_id, $order_id, $flight_type, $inventory_tour_id, $custom, $token]);
-        $tours = new Tour();
-        $rejection = 0;
-        $result = null;
-        // validate parameters are valid
-        if ($flight_inventory_tour_id) {
-            $flightInventoryTour = $this->getFlightInventoryTour($flight_inventory_tour_id);
-            if (!$flightInventoryTour) {
-                $rejection = 404;
-                $status = 'Invalid Flight Inventory Tour record';
-            }
-        } else {
-            throw new Exception('ERROR: bookFlightDetails has no inventory_tour_id to book');
-        }
-        $tour = $tours->find($tour_id);
-        $booking = Booking::where('token', $booking_token)->first();
-        if (empty($booking)) {
-            throw new Exception('booking could not be found for token '.$token);
-        }
-        $status = '';
-
-        if ($tour->id !== $booking->tour_id) {
-            throw new Exception('BookFlightDetails: booking and tour ids do not agree');
-        }
-        if ($customer_id !== $booking->customer_id) {
-            throw new Exception('BookFlightDetails: booking and customer IDs do not agree');
-        }
-        if (isset($flightInventoryTour)) {
-            $result = $this->storeOrUpdateFlightBooking($booking, $flight_type, $flightInventoryTour);
-        } else {
-            throw new Exception('ERROR: can not store a flight without a flightInventoryTour record');
-        }
-Log::info('returning a result from flightDetails', [$result]);
-        return response()->json(['success' => $result]);
+Log::debug('FlightDetailsController::bookFlights called');
     }
 
     /**
