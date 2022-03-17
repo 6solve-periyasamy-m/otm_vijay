2fabbcdcf653f2775e6f661879ccb4155d335425
Celeste Gateley <celeste.gateley@gmail.com>
2022-03-17 10:40:53 +0000

    Fix for component editing issues

--- resources/views/partials/models/transport_inventory_tours/form.blade.php
+++ resources/views/partials/models/transport_inventory_tours/form.blade.php
@@ -1,6 +1,6 @@
 @include('partials.fields.selector.default',
         ['name' => 'Transport Inventory', 'field' => 'transport_inventory_id', 'value' => $transport_inventory_id ?? 0,
          'route' => 'inventory.transport', ])
-@include('partials.fields.prefab.component_type')
-    @include('partials.fields.text', ['name' => 'Tour Sales Price', 'field' => 'tour_sales_price', 'value' => $tour_sales_price ?? null, ])
-    @include('partials.fields.submit')
+@include('partials.fields.prefab.component_type', ['classes' => 'accommodation-component-type-select', 'value' => $tour_component_type])
+@include('partials.fields.text', ['name' => 'Tour Sales Price', 'field' => 'tour_sales_price', 'value' => $tour_sales_price ?? null, ])
+@include('partials.fields.submit')
