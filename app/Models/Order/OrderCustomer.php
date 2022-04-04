da033bc03e0a12b1e00882fe9107cae63467b72d
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-04 13:38:16 +0100

    Organised code within OrderCustomer model

--- app/Models/Order/OrderCustomer.php
+++ app/Models/Order/OrderCustomer.php
@@ -12,10 +12,15 @@ use App\Models\OrderMerchandise;
 use App\Models\OrderTransport;
 use App\Repository\GroupRepository;
 use App\Repository\OrderRepository;
+use Carbon\Carbon;
 use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Collection;
 
 class OrderCustomer extends Model
 {
@@ -23,10 +28,9 @@ class OrderCustomer extends Model
     use SoftDeletes, CascadeSoftDeletes;
 
     protected $fillable = ['order_id', 'customer_id', 'tour_cost', 'single_occupancy_surcharge', 'travel_insurer', 'policy_number',];
-    protected $cascadeDeletes = ['orderCustomerGroups', 'orderActivities', 'orderFlights', 'orderTransports', 'adjustments'];
-    public $additional_attributes = ['booking_reference', 'ordered_on', 'lead_booker_name', 'is_lead_booker', 'customer_name'];
+    protected array $cascadeDeletes = ['orderCustomerGroups', 'orderActivities', 'orderFlights', 'orderTransports', 'adjustments'];
 
-    public static function getValidationRules()
+    public static function getValidationRules(): array
     {
         return [
             'customer_id' => 'exists:customers,id',
@@ -35,49 +39,54 @@ class OrderCustomer extends Model
         ];
     }
 
-    public function order()
+    public function order(): BelongsTo
     {
         return $this->belongsTo(Order::class);
     }
 
-    public function customer()
+    public function customer(): BelongsTo
     {
         return $this->belongsTo(Customer::class);
     }
 
-    public function orderCustomerGroups()
+    public function orderCustomerGroups(): HasMany
     {
         return $this->hasMany(OrderCustomerGroup::class, 'order_customer_id');
     }
 
-    public function orderAccommodation()
-    {
-        return GroupRepository::getOrderCustomerAccommodation($this);
-    }
-
-    public function orderActivities()
+    public function orderActivities(): HasMany
     {
         return $this->hasMany(OrderActivity::class, 'order_customer_id');
     }
 
-    public function orderFlights()
+    public function orderFlights(): HasMany
     {
         return $this->hasMany(OrderFlight::class, 'order_customer_id');
     }
 
-    public function orderTransports()
+    public function orderTransports(): HasMany
     {
         return $this->hasMany(OrderTransport::class, 'order_customer_id');
     }
 
-    public function adjustments()
+    public function adjustments(): HasMany
     {
         return $this->hasMany(OrderCustomerAdjustment::class, 'order_customer_id');
     }
 
-    public function orderMerchandise()
+    public function orderMerchandise(): HasMany
     {
         return $this->hasMany(OrderMerchandise::class, 'order_customer_id');
+    }
+
+    public function groups(): BelongsToMany
+    {
+        return $this->belongsToMany(Group::class, OrderCustomerGroup::class)->using(OrderCustomerGroup::class);
+    }
+
+    public function orderAccommodation(): Collection
+    {
+        return GroupRepository::getOrderCustomerAccommodation($this);
     }
 
     public function isCancelled(): bool
@@ -85,34 +94,29 @@ class OrderCustomer extends Model
         return $this->order->cancelled;
     }
 
-    public function getBookingReferenceAttribute()
+    public function getBookingReferenceAttribute(): string
     {
         return $this->order->booking_reference;
     }
 
-    public function getOrderedOnAttribute()
+    public function getOrderedOnAttribute(): Carbon
     {
         return $this->order->ordered_on;
     }
 
-    public function getLeadBookerNameAttribute()
+    public function getLeadBookerNameAttribute(): string
     {
         return "{$this->order->leadBooker->customer->first_name} {$this->order->leadBooker->customer->last_name}";
     }
 
-    public function getCustomerNameAttribute()
+    public function getCustomerNameAttribute(): string
     {
         return "{$this->customer->first_name} {$this->customer->last_name}";
     }
 
-    public function getIsLeadBookerAttribute()
+    public function getIsLeadBookerAttribute(): bool
     {
         return OrderRepository::isLeadBooker($this->order, $this->customer);
-    }
-
-    public function groups()
-    {
-        return $this->belongsToMany(Group::class, OrderCustomerGroup::class)->using(OrderCustomerGroup::class);
     }
 
     public function getPrimaryGroupAttribute(): ?Group
