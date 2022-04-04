da03f6dd8ca7fa4916c05a04f952bed80abd0ba3
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-04 12:06:41 +0100

    Remove old order imports

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,9 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Collection;
 
 class Order extends Model
 {
