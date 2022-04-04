7188436697ba4275027d717060aabf1feb8472d7
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-04 15:37:41 +0100

    Added PHPDoc to payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,12 +3,52 @@
 namespace App\Models;
 
 use App\Models\Order\Order;
+use Database\Factories\PaymentFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Carbon;
 use Illuminate\Validation\Rule;
 
+/**
+ * App\Models\Payment
+ *
+ * @property int $id
+ * @property int $order_id
+ * @property int $payment_method_id
+ * @property int|null $customer_id
+ * @property string $amount
+ * @property Carbon $paid_on Date when payment was made
+ * @property string $payment_type Payment Type. Should be Deposit/Installment/Refund
+ * @property Carbon|null $deleted_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Customer|null $customer Customer who made the payment
+ * @property-read Order $order Which order the payment is for
+ * @property-read PaymentMethod $paymentMethod Which payment method was used
+ * @method static PaymentFactory factory(...$parameters)
+ * @method static Builder|Payment newModelQuery()
+ * @method static Builder|Payment newQuery()
+ * @method static QueryBuilder|Payment onlyTrashed()
+ * @method static Builder|Payment query()
+ * @method static Builder|Payment whereAmount($value)
+ * @method static Builder|Payment whereCreatedAt($value)
+ * @method static Builder|Payment whereCustomerId($value)
+ * @method static Builder|Payment whereDeletedAt($value)
+ * @method static Builder|Payment whereId($value)
+ * @method static Builder|Payment whereOrderId($value)
+ * @method static Builder|Payment wherePaidOn($value)
+ * @method static Builder|Payment wherePaymentMethodId($value)
+ * @method static Builder|Payment wherePaymentType($value)
+ * @method static Builder|Payment whereUpdatedAt($value)
+ * @method static QueryBuilder|Payment withTrashed()
+ * @method static QueryBuilder|Payment withoutTrashed()
+ * @mixin Eloquent
+ */
 class Payment extends Model
 {
     use HasFactory, SoftDeletes;
