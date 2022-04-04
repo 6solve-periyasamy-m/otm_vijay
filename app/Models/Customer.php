94bd69a947ec30f8e74677d3cf87ebd52a6c315c
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-04 14:54:18 +0100

    Added PHPDoc to Customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,18 +6,133 @@ use App\Models\Order\Order;
 use App\Models\Order\OrderCustomer;
 use App\Notifications\CustomerResetPassword;
 use App\Repository\CustomerAuthenticationRepository;
+use Database\Factories\CustomerFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 use Illuminate\Validation\Rule;
 use Laravel\Cashier\Billable;
+use Laravel\Cashier\Subscription;
 
 
+/**
+ * App\Models\Customer
+ *
+ * @property int $id
+ * @property string|null $email_address
+ * @property string|null $password
+ * @property string|null $email_verified_at
+ * @property string|null $remember_token Remember token for login
+ * @property string|null $login_token Login token for booking form.
+ * @property string|null $gender
+ * @property string|null $title
+ * @property string $first_name
+ * @property string|null $middle_names
+ * @property string $last_name
+ * @property Carbon $date_of_birth
+ * @property string $mobile_number
+ * @property string|null $other_phone_number
+ * @property int $home_address_id
+ * @property int $billing_address_id
+ * @property string|null $emergency_contact_name
+ * @property string|null $emergency_contact_relationship
+ * @property string|null $emergency_contact_telephone
+ * @property string|null $passport_first_name
+ * @property string|null $passport_middle_name
+ * @property string|null $passport_last_name
+ * @property string|null $passport_number
+ * @property Carbon|null $passport_issue_date
+ * @property Carbon|null $passport_expiry_date
+ * @property string|null $passport_country_of_issue
+ * @property string|null $loyalty_number
+ * @property string $profile_picture Asset link to profile picture
+ * @property int|null $t_shirt_size_id
+ * @property int|null $hat_size_id
+ * @property string|null $notes Internal notes related to the customer
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
+ * @property string|null $stripe_id Stripe customer ID
+ * @property string|null $pm_type Stripe payment method type
+ * @property string|null $pm_last_four Stripe payment method last four
+ * @property string|null $trial_ends_at Stripe trial end date
+ * @property-read Address $billingAddress Billing address. Should be a unique entry in the database
+ * @property-read string $customer_full_name (Deprecated) Full name of the customer
+ * @property-read string $full_name Full name of customer
+ * @property-read HatSize|null $hatSize Customer hat size
+ * @property-read Address $homeAddress Home address. Should be a unique entry in the database
+ * @property-read Collection|Order[] $leadingOrders Orders where they are the lead booker
+ * @property-read int|null $leading_orders_count Amount of orders where they are the lead booker
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications System notifications for customer
+ * @property-read int|null $notifications_count Amount of system notifications for customer
+ * @property-read Collection|OrderCustomer[] $orderCustomers OrderCustomers for this customer
+ * @property-read int|null $order_customers_count Amount of OrderCustomers for this customer
+ * @property-read Collection|Order[] $orders Orders for this customer
+ * @property-read int|null $orders_count Amount of orders for this customer
+ * @property-read Collection|Subscription[] $subscriptions Stripe Subscriptions
+ * @property-read int|null $subscriptions_count Amount of Stripe Subscriptions
+ * @property-read TShirtSize|null $tShirtSize Customer t-shirt size
+ * @property-read Collection|CustomerApiToken[] $tokens Customer API tokens
+ * @property-read int|null $tokens_count Amount of Customer API tokens
+ * @method static CustomerFactory factory(...$parameters)
+ * @method static Builder|Customer newModelQuery()
+ * @method static Builder|Customer newQuery()
+ * @method static QueryBuilder|Customer onlyTrashed()
+ * @method static Builder|Customer query()
+ * @method static Builder|Customer whereBillingAddressId($value)
+ * @method static Builder|Customer whereCreatedAt($value)
+ * @method static Builder|Customer whereDateOfBirth($value)
+ * @method static Builder|Customer whereDeletedAt($value)
+ * @method static Builder|Customer whereEmailAddress($value)
+ * @method static Builder|Customer whereEmailVerifiedAt($value)
+ * @method static Builder|Customer whereEmergencyContactName($value)
+ * @method static Builder|Customer whereEmergencyContactRelationship($value)
+ * @method static Builder|Customer whereEmergencyContactTelephone($value)
+ * @method static Builder|Customer whereFirstName($value)
+ * @method static Builder|Customer whereGender($value)
+ * @method static Builder|Customer whereHatSizeId($value)
+ * @method static Builder|Customer whereHomeAddressId($value)
+ * @method static Builder|Customer whereId($value)
+ * @method static Builder|Customer whereLastName($value)
+ * @method static Builder|Customer whereLoginToken($value)
+ * @method static Builder|Customer whereLoyaltyNumber($value)
+ * @method static Builder|Customer whereMiddleNames($value)
+ * @method static Builder|Customer whereMobileNumber($value)
+ * @method static Builder|Customer whereNotes($value)
+ * @method static Builder|Customer whereOtherPhoneNumber($value)
+ * @method static Builder|Customer wherePassportCountryOfIssue($value)
+ * @method static Builder|Customer wherePassportExpiryDate($value)
+ * @method static Builder|Customer wherePassportFirstName($value)
+ * @method static Builder|Customer wherePassportIssueDate($value)
+ * @method static Builder|Customer wherePassportLastName($value)
+ * @method static Builder|Customer wherePassportMiddleName($value)
+ * @method static Builder|Customer wherePassportNumber($value)
+ * @method static Builder|Customer wherePassword($value)
+ * @method static Builder|Customer wherePmLastFour($value)
+ * @method static Builder|Customer wherePmType($value)
+ * @method static Builder|Customer whereProfilePicture($value)
+ * @method static Builder|Customer whereRememberToken($value)
+ * @method static Builder|Customer whereStripeId($value)
+ * @method static Builder|Customer whereTShirtSizeId($value)
+ * @method static Builder|Customer whereTitle($value)
+ * @method static Builder|Customer whereTrialEndsAt($value)
+ * @method static Builder|Customer whereUpdatedAt($value)
+ * @method static QueryBuilder|Customer withTrashed()
+ * @method static QueryBuilder|Customer withoutTrashed()
+ * @mixin Eloquent
+ */
 class Customer extends Authenticatable
 {
     use SoftDeletes;
