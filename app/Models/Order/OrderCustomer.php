479dcefc4f6c6799696631874d7dcff96c9fbbe6
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-04 13:55:07 +0100

    Added phpdoc to OrderCustomer model

--- app/Models/Order/OrderCustomer.php
+++ app/Models/Order/OrderCustomer.php
@@ -14,14 +14,74 @@ use App\Repository\GroupRepository;
 use App\Repository\OrderRepository;
 use Carbon\Carbon;
 use Dyrynda\Database\Support\CascadeSoftDeletes;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection as SupportCollection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Carbon as SupportCarbon;
 use Illuminate\Support\Collection;
 
+/**
+ * App\Models\Order\OrderCustomer
+ *
+ * @property int $id
+ * @property int|null $order_id
+ * @property int|null $customer_id
+ * @property string|null $tour_cost Cost for included components for the customer
+ * @property string|null $single_occupancy_surcharge Single occupancy surcharge for the customer
+ * @property string|null $travel_insurer Travel insurer for the customer
+ * @property string|null $policy_number Policy number for the order
+ * @property SupportCarbon|null $created_at
+ * @property SupportCarbon|null $updated_at
+ * @property SupportCarbon|null $deleted_at
+ * @property-read SupportCollection|OrderCustomerAdjustment[] $adjustments Customer specific price adjustments
+ * @property-read int|null $adjustments_count Amount of customer specific adjustments
+ * @property-read Customer|null $customer The customer details
+ * @property-read string $booking_reference The booking reference of the order
+ * @property-read string $customer_name The full name of the customer
+ * @property-read bool $has_occupancy Whether the customer has occupancy set correctly
+ * @property-read bool $has_surcharge Whether the customer should be charged for single occupancy
+ * @property-read bool $is_lead_booker Whether the customer is the lead booker
+ * @property-read string $lead_booker_name The full name of the lead booker
+ * @property-read Carbon $ordered_on When the order was placed
+ * @property-read Group|null $primary_group The primary group of the customer
+ * @property-read SupportCollection|Group[] $groups All groups the customer is in
+ * @property-read int|null $groups_count How many groups the customer is in
+ * @property-read Order|null $order The order details
+ * @property-read SupportCollection|OrderActivity[] $orderActivities What activities the customer is taking part in
+ * @property-read int|null $order_activities_count How many activities the customer is taking part in
+ * @property-read SupportCollection|OrderCustomerGroup[] $orderCustomerGroups Pivot list for OrderCustomerGroup
+ * @property-read int|null $order_customer_groups_count Amount of pivots for customer
+ * @property-read SupportCollection|OrderFlight[] $orderFlights What flights the customer is taking
+ * @property-read int|null $order_flights_count How many flights the customer is taking
+ * @property-read SupportCollection|OrderMerchandise[] $orderMerchandise What extras the customer has ordered
+ * @property-read int|null $order_merchandise_count How many extras the customer has ordered
+ * @property-read SupportCollection|OrderTransport[] $orderTransports What transport the customer is taking
+ * @property-read int|null $order_transports_count How many transport the customer is taking
+ * @method static Builder|OrderCustomer newModelQuery()
+ * @method static Builder|OrderCustomer newQuery()
+ * @method static QueryBuilder|OrderCustomer onlyTrashed()
+ * @method static Builder|OrderCustomer query()
+ * @method static Builder|OrderCustomer whereCreatedAt($value)
+ * @method static Builder|OrderCustomer whereCustomerId($value)
+ * @method static Builder|OrderCustomer whereDeletedAt($value)
+ * @method static Builder|OrderCustomer whereId($value)
+ * @method static Builder|OrderCustomer whereOrderId($value)
+ * @method static Builder|OrderCustomer wherePolicyNumber($value)
+ * @method static Builder|OrderCustomer whereSingleOccupancySurcharge($value)
+ * @method static Builder|OrderCustomer whereTourCost($value)
+ * @method static Builder|OrderCustomer whereTravelInsurer($value)
+ * @method static Builder|OrderCustomer whereUpdatedAt($value)
+ * @method static QueryBuilder|OrderCustomer withTrashed()
+ * @method static QueryBuilder|OrderCustomer withoutTrashed()
+ * @mixin Eloquent
+ */
 class OrderCustomer extends Model
 {
     use HasFactory;
