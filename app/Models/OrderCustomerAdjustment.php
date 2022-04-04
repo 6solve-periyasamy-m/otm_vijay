34e44b40c9175fb0af0319bd4ad1ff912aa084e9
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-04 15:27:02 +0100

    Added PHPDoc comments for OrderCustomerAdjustment

--- app/Models/OrderCustomerAdjustment.php
+++ app/Models/OrderCustomerAdjustment.php
@@ -3,11 +3,44 @@
 namespace App\Models;
 
 use App\Models\Order\OrderCustomer;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\OrderCustomerAdjustment
+ *
+ * @property int $id
+ * @property int $order_customer_id
+ * @property string $amount
+ * @property string $reason
+ * @property Carbon $date Date the adjustment was made. This is separate from created_at/updated_at, as it may be done retrospectively.
+ * @property Carbon|null $deleted_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read bool $cancelled Is the order cancelled?
+ * @property-read OrderCustomer $orderCustomer Order Customer the adjustment is for.
+ * @method static Builder|OrderCustomerAdjustment newModelQuery()
+ * @method static Builder|OrderCustomerAdjustment newQuery()
+ * @method static QueryBuilder|OrderCustomerAdjustment onlyTrashed()
+ * @method static Builder|OrderCustomerAdjustment query()
+ * @method static Builder|OrderCustomerAdjustment whereAmount($value)
+ * @method static Builder|OrderCustomerAdjustment whereCreatedAt($value)
+ * @method static Builder|OrderCustomerAdjustment whereDate($value)
+ * @method static Builder|OrderCustomerAdjustment whereDeletedAt($value)
+ * @method static Builder|OrderCustomerAdjustment whereId($value)
+ * @method static Builder|OrderCustomerAdjustment whereOrderCustomerId($value)
+ * @method static Builder|OrderCustomerAdjustment whereReason($value)
+ * @method static Builder|OrderCustomerAdjustment whereUpdatedAt($value)
+ * @method static QueryBuilder|OrderCustomerAdjustment withTrashed()
+ * @method static QueryBuilder|OrderCustomerAdjustment withoutTrashed()
+ * @mixin Eloquent
+ */
 class OrderCustomerAdjustment extends Model
 {
     use HasFactory, SoftDeletes;
