13b3e2c93d602bfb46a43abec68c5bee460d0213
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-07 22:39:39 +0100

    Performed code cleanup on Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,19 +5,17 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Event extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
-
-    public $additional_attributes = ['event_details'];
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['name', 'description', 'starts_at', 'ends_at', 'booking_url', 'notes',];
     protected $casts = ['starts_at' => 'date', 'ends_at' => 'date'];
 
-    public static function getValidationRules()
+    public static function getValidationRules(): array
     {
         return [
             'name' => 'required',
@@ -26,12 +24,12 @@ class Event extends Model
         ];
     }
 
-    function getEventDetailsAttribute()
+    function getEventDetailsAttribute(): string
     {
         return $this->name . ' - ' . Carbon::parse($this->starts_at)->format('d/m/Y') . ' : ' . Carbon::parse($this->ends_at)->format('d/m/Y');
     }
 
-    public function tours()
+    public function tours(): HasMany
     {
         return $this->hasMany(Tour::class, 'event_id');
     }
