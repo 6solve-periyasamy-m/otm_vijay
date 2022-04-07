f141aaaf1d1b271ea0699cad53dff3ce9a4a24bc
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-07 18:16:40 +0100

    Added phpdoc comments to customer models

--- app/Models/Customer/Group.php
+++ app/Models/Customer/Group.php
@@ -6,13 +6,48 @@ use App\Models\Accommodation\RoomType;
 use App\Models\Order\Component\OrderAccommodation;
 use App\Models\Order\OrderCustomer;
 use Dyrynda\Database\Support\CascadeSoftDeletes;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Customer\Group
+ *
+ * @property int $id
+ * @property string $name
+ * @property int|null $room_type_id
+ * @property Carbon|null $deleted_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection|OrderCustomer[] $orderCustomers
+ * @property-read int|null $order_customers_count
+ * @property-read Collection|OrderCustomerGroup[] $pivot
+ * @property-read int|null $pivot_count
+ * @property-read RoomType|null $roomType
+ * @property-read Collection|OrderAccommodation[] $rooms
+ * @property-read int|null $rooms_count
+ * @method static Builder|Group newModelQuery()
+ * @method static Builder|Group newQuery()
+ * @method static QueryBuilder|Group onlyTrashed()
+ * @method static Builder|Group query()
+ * @method static Builder|Group whereCreatedAt($value)
+ * @method static Builder|Group whereDeletedAt($value)
+ * @method static Builder|Group whereId($value)
+ * @method static Builder|Group whereName($value)
+ * @method static Builder|Group whereRoomTypeId($value)
+ * @method static Builder|Group whereUpdatedAt($value)
+ * @method static QueryBuilder|Group withTrashed()
+ * @method static QueryBuilder|Group withoutTrashed()
+ * @mixin Eloquent
+ */
 class Group extends Model
 {
     use HasFactory, SoftDeletes, CascadeSoftDeletes;
--- app/Models/Customer/OrderCustomerGroup.php
+++ app/Models/Customer/OrderCustomerGroup.php
@@ -3,11 +3,36 @@
 namespace App\Models\Customer;
 
 use App\Models\Order\OrderCustomer;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Customer\OrderCustomerGroup
+ *
+ * @property int $id
+ * @property int $order_customer_id
+ * @property int $group_id
+ * @property Carbon|null $deleted_at
+ * @property-read Group $group
+ * @property-read OrderCustomer $orderCustomer
+ * @method static Builder|OrderCustomerGroup newModelQuery()
+ * @method static Builder|OrderCustomerGroup newQuery()
+ * @method static QueryBuilder|OrderCustomerGroup onlyTrashed()
+ * @method static Builder|OrderCustomerGroup query()
+ * @method static Builder|OrderCustomerGroup whereDeletedAt($value)
+ * @method static Builder|OrderCustomerGroup whereGroupId($value)
+ * @method static Builder|OrderCustomerGroup whereId($value)
+ * @method static Builder|OrderCustomerGroup whereOrderCustomerId($value)
+ * @method static QueryBuilder|OrderCustomerGroup withTrashed()
+ * @method static QueryBuilder|OrderCustomerGroup withoutTrashed()
+ * @mixin Eloquent
+ */
 class OrderCustomerGroup extends Pivot
 {
     use HasFactory, SoftDeletes;
