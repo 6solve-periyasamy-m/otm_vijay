512011668b5368f69d73c93d7f8c48c6edc8268c
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-07 11:40:11 +0100

    Performed code cleanup on OrderTransport

--- app/Models/Order/Component/OrderTransport.php
+++ app/Models/Order/Component/OrderTransport.php
@@ -3,79 +3,110 @@
 namespace App\Models\Order\Component;
 
 use App\Models\Order\OrderCustomer;
+use App\Models\Transport;
+use App\Models\TransportInventory;
 use App\Models\TransportInventoryTour;
-use App\Repository\TransportComponentRepository;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Order\Component\OrderTransport
+ *
+ * @property int $id
+ * @property int|null $order_customer_id
+ * @property int|null $transport_inventory_tour_id
+ * @property float $cost
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
+ * @property-read bool $cancelled
+ * @property-read string $details
+ * @property-read string $tour_component_type
+ * @property-read float $tour_sales_price
+ * @property-read OrderCustomer|null $orderCustomer
+ * @property-read TransportInventoryTour|null $transportInventoryTour
+ * @property-read TransportInventoryTour|null $tourComponent
+ * @method static Builder|OrderTransport newModelQuery()
+ * @method static Builder|OrderTransport newQuery()
+ * @method static QueryBuilder|OrderTransport onlyTrashed()
+ * @method static Builder|OrderTransport query()
+ * @method static Builder|OrderTransport whereCost($value)
+ * @method static Builder|OrderTransport whereCreatedAt($value)
+ * @method static Builder|OrderTransport whereDeletedAt($value)
+ * @method static Builder|OrderTransport whereId($value)
+ * @method static Builder|OrderTransport whereOrderCustomerId($value)
+ * @method static Builder|OrderTransport whereTransportInventoryTourId($value)
+ * @method static Builder|OrderTransport whereUpdatedAt($value)
+ * @method static QueryBuilder|OrderTransport withTrashed()
+ * @method static QueryBuilder|OrderTransport withoutTrashed()
+ * @mixin Eloquent
+ */
 class OrderTransport extends Model
 {
     use HasFactory;
     use SoftDeletes;
 
     protected $fillable = ['order_customer_id', 'transport_inventory_tour_id','cost'];
-    public $additional_attributes = ['details','tour_component_type','tour_sales_price'];
 
-    public static function findByOrderCustomer($orderCustomerId)
+    public static function findByOrderCustomer($orderCustomerId): Collection|array
     {
-        $orderTransports = OrderTransport::where('order_customer_id', $orderCustomerId)->get();
-
-        return $orderTransports;
+        return OrderTransport::where('order_customer_id', $orderCustomerId)->get();
     }
 
-    // TODO: Deprecate
-    public function orderCustomers()
+    public function orderCustomer(): BelongsTo
     {
         return $this->belongsTo(OrderCustomer::class, 'order_customer_id');
     }
 
-    public function transport()
-    {
-        return TransportComponentRepository::getComponentFromOrderComponent($this->id);
-    }
-
-    public function transportInventory()
-    {
-        return TransportComponentRepository::getInventoryFromOrderComponent($this->id);
-    }
-
-    public function transportInventoryTour()
+    public function tourComponent(): BelongsTo
     {
         return $this->belongsTo(TransportInventoryTour::class, 'transport_inventory_tour_id');
     }
 
+    public function transportInventoryTour(): BelongsTo
+    {
+        return $this->belongsTo(TransportInventoryTour::class, 'transport_inventory_tour_id');
+    }
+
+    public function transportInventory(): TransportInventory
+    {
+        return $this->transportInventoryTour->transportInventory;
+    }
+
+    public function transport(): Transport
+    {
+        return $this->transportInventoryTour->transportInventory->transport;
+    }
+
     public function getCancelledAttribute(): bool
     {
-        return $this->orderCustomers->order->cancelled;
+        return $this->orderCustomer->order->cancelled;
     }
 
-    public function tourComponent()
-    {
-        return $this->transportInventoryTour();
-    }
-
-    public function getDetailsAttribute()
+    public function getDetailsAttribute(): string
     {
         return "{$this->tourComponent->inventory}";
     }
 
-    public function getTourComponentTypeAttribute()
+    public function getTourComponentTypeAttribute(): string
     {
         return $this->tourComponent->tour_component_type;
     }
 
-    public function getTourSalesPriceAttribute()
+    public function getTourSalesPriceAttribute(): float
     {
         return $this->tourComponent->tour_sales_price;
     }
 
-    public function orderCustomer()
+    public function swap(TransportInventoryTour $swap)
     {
-        return $this->orderCustomers();
-    }
-
-    public function swap(TransportInventoryTour $swap) {
         $this->transport_inventory_tour_id = $swap->id;
         $this->cost = $swap->tour_sales_price;
         $this->save();
