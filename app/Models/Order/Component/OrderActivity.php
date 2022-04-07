cccb0d84b7b17e240b4dd73b88895ff9cc678dca
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-07 11:18:21 +0100

    Performed code cleanup on OrderActivity

--- app/Models/Order/Component/OrderActivity.php
+++ app/Models/Order/Component/OrderActivity.php
@@ -2,81 +2,111 @@
 
 namespace App\Models\Order\Component;
 
+use App\Models\Activity;
+use App\Models\ActivityInventory;
 use App\Models\ActivityInventoryTour;
 use App\Models\Order\OrderCustomer;
-use App\Repository\ActivityComponentRepository;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Order\Component\OrderActivity
+ *
+ * @property int $id
+ * @property int $order_customer_id
+ * @property int $activity_inventory_tour_id
+ * @property float $cost
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
+ * @property-read ActivityInventoryTour $activityInventoryTour
+ * @property-read ActivityInventoryTour $tourComponent
+ * @property-read bool $cancelled Is the order cancelled?
+ * @property-read string $details
+ * @property-read string $tour_component_type
+ * @property-read float $tour_sales_price
+ * @property-read OrderCustomer $orderCustomer
+ * @method static Builder|OrderActivity newModelQuery()
+ * @method static Builder|OrderActivity newQuery()
+ * @method static QueryBuilder|OrderActivity onlyTrashed()
+ * @method static Builder|OrderActivity query()
+ * @method static Builder|OrderActivity whereActivityInventoryTourId($value)
+ * @method static Builder|OrderActivity whereCost($value)
+ * @method static Builder|OrderActivity whereCreatedAt($value)
+ * @method static Builder|OrderActivity whereDeletedAt($value)
+ * @method static Builder|OrderActivity whereId($value)
+ * @method static Builder|OrderActivity whereOrderCustomerId($value)
+ * @method static Builder|OrderActivity whereUpdatedAt($value)
+ * @method static QueryBuilder|OrderActivity withTrashed()
+ * @method static QueryBuilder|OrderActivity withoutTrashed()
+ * @mixin Eloquent
+ */
 class OrderActivity extends Model
 {
     use HasFactory;
     use SoftDeletes;
 
     protected $fillable = ['order_customer_id', 'activity_inventory_tour_id','cost'];
-    public $additional_attributes = ['details','tour_component_type','tour_sales_price'];
 
-    public static function findByOrderCustomer($orderCustomerId)
+    public static function findByOrderCustomer($orderCustomerId): Collection|array
     {
-        $orderActivities = OrderActivity::where('order_customer_id', $orderCustomerId)->get();
-
-
-        return $orderActivities;
+        return OrderActivity::where('order_customer_id', $orderCustomerId)->get();
     }
 
-    // TODO: Deprecate
-    public function orderCustomers()
+    public function orderCustomer(): BelongsTo
     {
         return $this->belongsTo(OrderCustomer::class, 'order_customer_id');
     }
 
-    public function activity()
-    {
-        return ActivityComponentRepository::getComponentFromOrderComponent($this->id);
-    }
-
-    public function activityInventory()
-    {
-        return ActivityComponentRepository::getInventoryFromOrderComponent($this->id);
-    }
-
-    public function activityInventoryTour()
+    public function tourComponent(): BelongsTo
     {
         return $this->belongsTo(ActivityInventoryTour::class, 'activity_inventory_tour_id');
     }
 
+    public function activityInventoryTour(): BelongsTo
+    {
+        return $this->belongsTo(ActivityInventoryTour::class, 'activity_inventory_tour_id');
+    }
+
+    public function activityInventory(): ActivityInventory
+    {
+        return $this->activityInventoryTour->activityInventory;
+    }
+
+    public function activity(): Activity
+    {
+        return $this->activityInventoryTour->activityInventory->activity;
+    }
+
     public function getCancelledAttribute(): bool
     {
-        return $this->orderCustomers->order->cancelled;
+        return $this->orderCustomer->order->cancelled;
     }
 
-    public function tourComponent()
-    {
-        return $this->activityInventoryTour();
-    }
-
-    public function getDetailsAttribute()
+    public function getDetailsAttribute(): string
     {
         return "{$this->tourComponent->inventory}";
     }
 
-    public function getTourComponentTypeAttribute()
+    public function getTourComponentTypeAttribute(): string
     {
         return $this->tourComponent->tour_component_type;
     }
 
-    public function getTourSalesPriceAttribute()
+    public function getTourSalesPriceAttribute(): float
     {
         return $this->tourComponent->tour_sales_price;
     }
 
-    public function orderCustomer()
+    public function swap(ActivityInventoryTour $swap)
     {
-        return $this->orderCustomers();
-    }
-
-    public function swap(ActivityInventoryTour $swap) {
         $this->activity_inventory_tour_id = $swap->id;
         $this->cost = $swap->tour_sales_price;
         $this->save();
