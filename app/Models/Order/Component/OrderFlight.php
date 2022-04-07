5f15873e2b9aae9a2ec1cdc9d8efecdbbecef3fb
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-07 11:27:06 +0100

    Performed code cleanup on OrderFlight

--- app/Models/Order/Component/OrderFlight.php
+++ app/Models/Order/Component/OrderFlight.php
@@ -4,55 +4,98 @@ namespace App\Models\Order\Component;
 
 use App\Models\Airport;
 use App\Models\Flight;
+use App\Models\FlightInventory;
 use App\Models\FlightInventoryTour;
 use App\Models\Order\OrderCustomer;
-use App\Repository\FlightComponentRepository;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Order\Component\OrderFlight
+ *
+ * @property int $id
+ * @property int|null $order_customer_id
+ * @property int|null $flight_inventory_tour_id
+ * @property float $cost
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
+ * @property-read Airport|null $arrivalAirport
+ * @property-read Airport|null $departureAirport
+ * @property-read FlightInventoryTour|null $flightInventoryTour
+ * @property-read FlightInventoryTour|null $tourComponent
+ * @property-read string $atol_string
+ * @property-read bool $cancelled
+ * @property-read string $details
+ * @property-read string $tour_component_type
+ * @property-read float $tour_sales_price
+ * @property-read OrderCustomer|null $orderCustomer
+ * @method static Builder|OrderFlight newModelQuery()
+ * @method static Builder|OrderFlight newQuery()
+ * @method static QueryBuilder|OrderFlight onlyTrashed()
+ * @method static Builder|OrderFlight query()
+ * @method static Builder|OrderFlight whereCost($value)
+ * @method static Builder|OrderFlight whereCreatedAt($value)
+ * @method static Builder|OrderFlight whereDeletedAt($value)
+ * @method static Builder|OrderFlight whereFlightInventoryTourId($value)
+ * @method static Builder|OrderFlight whereId($value)
+ * @method static Builder|OrderFlight whereOrderCustomerId($value)
+ * @method static Builder|OrderFlight whereUpdatedAt($value)
+ * @method static QueryBuilder|OrderFlight withTrashed()
+ * @method static QueryBuilder|OrderFlight withoutTrashed()
+ * @mixin Eloquent
+ */
 class OrderFlight extends Model
 {
     use HasFactory;
     use SoftDeletes;
 
     protected $fillable = ['order_customer_id', 'flight_inventory_tour_id','cost'];
-    public $additional_attributes = ['details','tour_component_type','tour_sales_price'];
 
-    public static function findByOrderCustomer($orderCustomerId)
+    public static function findByOrderCustomer($orderCustomerId): Collection|array
     {
-        $orderFlights = OrderFlight::where('order_customer_id', $orderCustomerId)->with('arrivalAirport')->with('departureAirport')->get();
-
-        return $orderFlights;
+        return OrderFlight::where('order_customer_id', $orderCustomerId)->with('arrivalAirport')->with('departureAirport')->get();
     }
 
-    // TODO: Deprecate
-    public function orderCustomers()
+    public function orderCustomer(): BelongsTo
     {
         return $this->belongsTo(OrderCustomer::class, 'order_customer_id');
     }
 
-    public function flight()
-    {
-        return FlightComponentRepository::getComponentFromOrderComponent($this->id);
-    }
-
-    public function flightInventory()
-    {
-        return FlightComponentRepository::getInventoryFromOrderComponent($this->id);
-    }
-
-    public function flightInventoryTour()
+    public function tourComponent(): BelongsTo
     {
         return $this->belongsTo(FlightInventoryTour::class, 'flight_inventory_tour_id');
     }
 
-    public function departureAirport()
+    public function flightInventoryTour(): BelongsTo
+    {
+        return $this->belongsTo(FlightInventoryTour::class, 'flight_inventory_tour_id');
+    }
+
+    public function flightInventory(): FlightInventory
+    {
+        return $this->flightInventoryTour->flightInventory;
+    }
+
+    public function flight(): Flight
+    {
+        return $this->flightInventoryTour->flightInventory->flight;
+    }
+
+    public function departureAirport(): HasOneThrough
     {
         return $this->hasOneThrough(Airport::class, Flight::class, 'departure_airport_id', 'id');
     }
 
-    public function arrivalAirport()
+    public function arrivalAirport(): HasOneThrough
     {
         return $this->hasOneThrough(Airport::class, Flight::class, 'arrival_airport_id', 'id');
     }
@@ -62,39 +105,30 @@ class OrderFlight extends Model
         return $this->orderCustomers->order->cancelled;
     }
 
-    public function tourComponent()
-    {
-        return $this->flightInventoryTour();
-    }
-
-    public function getDetailsAttribute()
+    public function getDetailsAttribute(): string
     {
         return "{$this->tourComponent->inventory} - {$this->tourComponent->flight_type}";
     }
 
-    public function getTourComponentTypeAttribute()
+    public function getTourComponentTypeAttribute(): string
     {
         return $this->tourComponent->tour_component_type;
     }
 
-    public function getTourSalesPriceAttribute()
+    public function getTourSalesPriceAttribute(): float
     {
         return $this->tourComponent->tour_sales_price;
-    }
-
-    public function orderCustomer()
-    {
-        return $this->orderCustomers();
-    }
-
-    public function swap(FlightInventoryTour $swap) {
-        $this->flight_inventory_tour_id = $swap->id;
-        $this->cost = $swap->tour_sales_price;
-        $this->save();
     }
 
     public function getAtolStringAttribute(): string
     {
         return $this->tourComponent->atol_string;
+    }
+
+    public function swap(FlightInventoryTour $swap)
+    {
+        $this->flight_inventory_tour_id = $swap->id;
+        $this->cost = $swap->tour_sales_price;
+        $this->save();
     }
 }
