031132033d5c32000f431eeb2b5b0e572a560d10
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-07 22:09:12 +0100

    Added PHPDoc comments to remaining system models

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Report
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property string $parent
+ * @property array $fields
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
+ * @method static Builder|Report newModelQuery()
+ * @method static Builder|Report newQuery()
+ * @method static QueryBuilder|Report onlyTrashed()
+ * @method static Builder|Report query()
+ * @method static Builder|Report whereCreatedAt($value)
+ * @method static Builder|Report whereDeletedAt($value)
+ * @method static Builder|Report whereDescription($value)
+ * @method static Builder|Report whereFields($value)
+ * @method static Builder|Report whereId($value)
+ * @method static Builder|Report whereName($value)
+ * @method static Builder|Report whereParent($value)
+ * @method static Builder|Report whereUpdatedAt($value)
+ * @method static QueryBuilder|Report withTrashed()
+ * @method static QueryBuilder|Report withoutTrashed()
+ * @mixin Eloquent
+ */
 class Report extends Model
 {
     use HasFactory;
--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,8 +2,22 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Setting
+ *
+ * @property string $key
+ * @property string $value
+ * @method static Builder|Setting newModelQuery()
+ * @method static Builder|Setting newQuery()
+ * @method static Builder|Setting query()
+ * @method static Builder|Setting whereKey($value)
+ * @method static Builder|Setting whereValue($value)
+ * @mixin Eloquent
+ */
 class Setting extends Model
 {
     public $incrementing = false;
