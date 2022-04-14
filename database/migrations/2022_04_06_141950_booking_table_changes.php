b5b298b46081db663539c41e9833fe76dc11e919
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-14 13:40:39 +0100

    Fix for migrations failing due to pre-existing fields. All active versions have the changes, so this fix will make no difference to live systems, only fresh deployments

--- database/migrations/2022_04_06_141950_booking_table_changes.php
+++ database/migrations/2022_04_06_141950_booking_table_changes.php
@@ -18,8 +18,6 @@ class BookingTableChanges extends Migration
         });
         Schema::table('booking_accommodations', function (Blueprint $table) {
             $table->dropConstrainedForeignId('accommodation_inventory_tour_id');
-            $table->foreignId('room_type_id')->constrained();
-            $table->integer('group_id')->nullable();
         });
     }
 
@@ -35,8 +33,6 @@ class BookingTableChanges extends Migration
         });
         Schema::table('booking_accommodations', function (Blueprint $table) {
             $table->foreignId('accommodation_inventory_tour_id')->constrained();
-            $table->dropConstrainedForeignId('room_type_id');
-            $table->dropColumn('group_id');
         });
     }
 }
