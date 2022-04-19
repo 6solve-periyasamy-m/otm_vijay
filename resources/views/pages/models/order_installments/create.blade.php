01b6fdfbf4724c107665f0c9a6b94004232a57e3
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-19 11:36:37 +0100

    Added ability to create/resync order installments

--- resources/views/pages/models/order_installments/create.blade.php
+++ resources/views/pages/models/order_installments/create.blade.php
@@ -1,7 +1,9 @@
-@extends('layout.form', ['action' => route('order-installments.store', ['tour' => $tour,]),])
+@extends('layout.form', ['action' => route('order-installments.store', ['order' => $order, ]),])
 
 @section('title', 'Create Order Installment')
 
 @section('form-body')
-  @include('partials.models.payment_installments.form')
+    @include('partials.fields.date', ['name' => 'Due On', 'field' => 'due_on', 'value' => $due_on ?? null])
+    @include('partials.fields.text', ['name' => 'Amount', 'field' => 'amount', 'value' => $amount ?? null])
+    @include('partials.fields.submit')
 @endsection
