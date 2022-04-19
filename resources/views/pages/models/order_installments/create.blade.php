8a13ca62c9cf29723af06e7a84f159d3e9bbab53
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-19 12:37:56 +0100

    Removed values from order installment create form

--- resources/views/pages/models/order_installments/create.blade.php
+++ resources/views/pages/models/order_installments/create.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Create Order Installment')
 
 @section('form-body')
-    @include('partials.fields.date', ['name' => 'Due On', 'field' => 'due_on', 'value' => $due_on ?? null])
-    @include('partials.fields.text', ['name' => 'Amount', 'field' => 'amount', 'value' => $amount ?? null])
+    @include('partials.fields.date', ['name' => 'Due On', 'field' => 'due_on',])
+    @include('partials.fields.text', ['name' => 'Amount', 'field' => 'amount',])
     @include('partials.fields.submit')
 @endsection
