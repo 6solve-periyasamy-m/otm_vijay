f7754512914b156d179dc6692d30b8db688de488
Celeste Gateley <celeste.gateley@gmail.com>
2022-04-25 14:31:05 +0100

    Added notification if the page width is too low

--- resources/views/pages/customer/booking/layout.blade.php
+++ resources/views/pages/customer/booking/layout.blade.php
@@ -10,5 +10,10 @@
     <hr class="splitter">
     <h2 class="col-md-12 mb-0">{{ $tour->name }} - {{ StringFormatter::formatCurrency($tour->base_price_per_person) }} per person</h2>
     <hr class="splitter">
+    <script type="text/javascript">
+        if ($(window).width() < 960) {
+            alert('Please switch to landscape mode for the best experience with this form');
+        }
+    </script>
     @yield('booking-body')
 @endsection
