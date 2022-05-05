e883f52250e383754f27ece69998000007a156ee
Celeste Gateley <celeste.gateley@gmail.com>
2022-05-05 12:16:38 +0100

    Changed accommodation group names

--- database/seeders/AccommodationGroupTableSeeder.php
+++ database/seeders/AccommodationGroupTableSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use DB;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class AccommodationGroupTableSeeder extends Seeder
 {
@@ -17,111 +17,259 @@ class AccommodationGroupTableSeeder extends Seeder
       
       DB::table('accommodation_groups')->delete();
 
-      DB::table('accommodation_groups')->insert(
-      array (
-        array(
-            'id' => 1, 'name' => 'Group 1',
-            ),array(
-            'id' => 2, 'name' => 'Group 2',
-            ),array(
-            'id' => 3, 'name' => 'Group 3',
-            ),array(
-            'id' => 4, 'name' => 'Group 4',
-            ),array(
-            'id' => 5, 'name' => 'Group 5',
-            ),array(
-            'id' => 6, 'name' => 'Group 6',
-            ),array(
-            'id' => 7, 'name' => 'Group 7',
-            ),array(
-            'id' => 8, 'name' => 'Group 8',
-            ),array(
-            'id' => 9, 'name' => 'Group 9',
-            ),array(
-            'id' => 10, 'name' => 'Group 10',
-            ),array(
-            'id' => 11, 'name' => 'Group 11',
-            ),array(
-            'id' => 12, 'name' => 'Group 12',
-            ),array(
-            'id' => 13, 'name' => 'Group 13',
-            ),array(
-            'id' => 14, 'name' => 'Group 14',
-            ),array(
-            'id' => 15, 'name' => 'Group 15',
-            ),array(
-            'id' => 16, 'name' => 'Group 16',
-            ),array(
-            'id' => 17, 'name' => 'Group 17',
-            ),array(
-            'id' => 18, 'name' => 'Group 18',
-            ),array(
-            'id' => 19, 'name' => 'Group 19',
-            ),array(
-            'id' => 20, 'name' => 'Group 20',
-            ),array (
-            'id' => 21, 'name' => 'Single',
-            ),array(
-            'id' => 22, 'name' => 'Couple 1', 
-            ),array(
-            'id' => 23, 'name' => 'Couple 2', 
-            ),array(
-            'id' => 24, 'name' => 'Couple 3', 
-            ),array(
-            'id' => 25, 'name' => 'Couple 4', 
-            ),array(
-            'id' => 26, 'name' => 'Couple 5', 
-            ),array(
-            'id' => 27, 'name' => 'Couple 6', 
-            ),array(
-            'id' => 28, 'name' => 'Couple 7', 
-            ),array(
-            'id' => 29, 'name' => 'Couple 8', 
-            ),array(
-            'id' => 30, 'name' => 'Couple 9',
-            ),array(
-            'id' => 31, 'name' => 'Couple 10',
-            ),array(
-            'id' => 32, 'name' => 'Friends 1',
-            ),array(
-            'id' => 33, 'name' => 'Friends 2',
-            ),array(
-            'id' => 34, 'name' => 'Friends 3',
-            ),array(
-            'id' => 35, 'name' => 'Friends 4',
-            ),array(
-            'id' => 36, 'name' => 'Friends 5',
-            ),array(
-            'id' => 37, 'name' => 'Friends 6',
-            ),array(
-            'id' => 38, 'name' => 'Friends 7',
-            ),array(
-            'id' => 39, 'name' => 'Friends 8',
-            ),array(
-            'id' => 40, 'name' => 'Friends 9',
-            ),array(
-            'id' => 41, 'name' => 'Family 1',
-            ),array(
-            'id' => 42, 'name' => 'Family 2',
-            ),array(
-            'id' => 43, 'name' => 'Family 3',
-            ),array(
-            'id' => 44, 'name' => 'Family 4',
-            ),array(
-            'id' => 45, 'name' => 'Family 5',
-            ),array(
-            'id' => 46, 'name' => 'Family 6',
-            ),array(
-            'id' => 47, 'name' => 'Family 7',
-            ),array(
-            'id' => 48, 'name' => 'Family 8',
-            ),array(
-            'id' => 49, 'name' => 'Family 9',
-            ),array(
-            'id' => 50, 'name' => 'Family 10',
-            )
-            )
-          );
+      DB::table('accommodation_groups')->insert(array (
+            0 =>
+                array (
+                    'id' => 1,
+                    'name' => 'Room 1',
+                ),
+            1 =>
+                array (
+                    'id' => 2,
+                    'name' => 'Room 2',
+                ),
+            2 =>
+                array (
+                    'id' => 3,
+                    'name' => 'Room 3',
+                ),
+            3 =>
+                array (
+                    'id' => 4,
+                    'name' => 'Room 4',
+                ),
+            4 =>
+                array (
+                    'id' => 5,
+                    'name' => 'Room 5',
+                ),
+            5 =>
+                array (
+                    'id' => 6,
+                    'name' => 'Room 6',
+                ),
+            6 =>
+                array (
+                    'id' => 7,
+                    'name' => 'Room 7',
+                ),
+            7 =>
+                array (
+                    'id' => 8,
+                    'name' => 'Room 8',
+                ),
+            8 =>
+                array (
+                    'id' => 9,
+                    'name' => 'Room 9',
+                ),
+            9 =>
+                array (
+                    'id' => 10,
+                    'name' => 'Room 10',
+                ),
+            10 =>
+                array (
+                    'id' => 11,
+                    'name' => 'Room 11',
+                ),
+            11 =>
+                array (
+                    'id' => 12,
+                    'name' => 'Room 12',
+                ),
+            12 =>
+                array (
+                    'id' => 13,
+                    'name' => 'Room 13',
+                ),
+            13 =>
+                array (
+                    'id' => 14,
+                    'name' => 'Room 14',
+                ),
+            14 =>
+                array (
+                    'id' => 15,
+                    'name' => 'Room 15',
+                ),
+            15 =>
+                array (
+                    'id' => 16,
+                    'name' => 'Room 16',
+                ),
+            16 =>
+                array (
+                    'id' => 17,
+                    'name' => 'Room 17',
+                ),
+            17 =>
+                array (
+                    'id' => 18,
+                    'name' => 'Room 18',
+                ),
+            18 =>
+                array (
+                    'id' => 19,
+                    'name' => 'Room 19',
+                ),
+            19 =>
+                array (
+                    'id' => 20,
+                    'name' => 'Room 20',
+                ),
+            20 =>
+                array (
+                    'id' => 21,
+                    'name' => 'Room 21',
+                ),
+            21 =>
+                array (
+                    'id' => 22,
+                    'name' => 'Room 22',
+                ),
+            22 =>
+                array (
+                    'id' => 23,
+                    'name' => 'Room 23',
+                ),
+            23 =>
+                array (
+                    'id' => 24,
+                    'name' => 'Room 24',
+                ),
+            24 =>
+                array (
+                    'id' => 25,
+                    'name' => 'Room 25',
+                ),
+            25 =>
+                array (
+                    'id' => 26,
+                    'name' => 'Room 26',
+                ),
+            26 =>
+                array (
+                    'id' => 27,
+                    'name' => 'Room 27',
+                ),
+            27 =>
+                array (
+                    'id' => 28,
+                    'name' => 'Room 28',
+                ),
+            28 =>
+                array (
+                    'id' => 29,
+                    'name' => 'Room 29',
+                ),
+            29 =>
+                array (
+                    'id' => 30,
+                    'name' => 'Room 30',
+                ),
+            30 =>
+                array (
+                    'id' => 31,
+                    'name' => 'Room 31',
+                ),
+            31 =>
+                array (
+                    'id' => 32,
+                    'name' => 'Room 32',
+                ),
+            32 =>
+                array (
+                    'id' => 33,
+                    'name' => 'Room 33',
+                ),
+            33 =>
+                array (
+                    'id' => 34,
+                    'name' => 'Room 34',
+                ),
+            34 =>
+                array (
+                    'id' => 35,
+                    'name' => 'Room 35',
+                ),
+            35 =>
+                array (
+                    'id' => 36,
+                    'name' => 'Room 36',
+                ),
+            36 =>
+                array (
+                    'id' => 37,
+                    'name' => 'Room 37',
+                ),
+            37 =>
+                array (
+                    'id' => 38,
+                    'name' => 'Room 38',
+                ),
+            38 =>
+                array (
+                    'id' => 39,
+                    'name' => 'Room 39',
+                ),
+            39 =>
+                array (
+                    'id' => 40,
+                    'name' => 'Room 40',
+                ),
+            40 =>
+                array (
+                    'id' => 41,
+                    'name' => 'Room 41',
+                ),
+            41 =>
+                array (
+                    'id' => 42,
+                    'name' => 'Room 42',
+                ),
+            42 =>
+                array (
+                    'id' => 43,
+                    'name' => 'Room 43',
+                ),
+            43 =>
+                array (
+                    'id' => 44,
+                    'name' => 'Room 44',
+                ),
+            44 =>
+                array (
+                    'id' => 45,
+                    'name' => 'Room 45',
+                ),
+            45 =>
+                array (
+                    'id' => 46,
+                    'name' => 'Room 46',
+                ),
+            46 =>
+                array (
+                    'id' => 47,
+                    'name' => 'Room 47',
+                ),
+            47 =>
+                array (
+                    'id' => 48,
+                    'name' => 'Room 48',
+                ),
+            48 =>
+                array (
+                    'id' => 49,
+                    'name' => 'Room 49',
+                ),
+            49 =>
+                array (
+                    'id' => 50,
+                    'name' => 'Room 50',
+                ),
+        ));
+
+
     }
 }
-
