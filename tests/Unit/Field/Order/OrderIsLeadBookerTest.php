6aaf5306247719f8208d182930e9ed207b62416c
Celeste Gateley <celeste.gateley@gmail.com>
2022-05-13 14:09:16 +0100

    Implemented testing for IsLeadBooker

---
+++ tests/Unit/Field/Order/OrderIsLeadBookerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Field\Order;
+
+use App\Models\Customer\Customer;
+use App\Repository\StaticOrderRepository;
+use Tests\DatabaseTestCase;
+use Tests\Traits\TestsOrder;
+
+class OrderIsLeadBookerTest extends DatabaseTestCase
+{
+    use TestsOrder;
+
+    public function testIsLeadBookerWithLead()
+    {
+        $order = $this->generateOrder();
+        $this->assertTrue(StaticOrderRepository::isLeadBooker($order, $order->leadBooker->customer));
+    }
+
+    public function testIsLeadBookerWithAdditional()
+    {
+        $order = $this->generateOrder();
+        $orderCustomer = $this->generateOrderCustomer(false, $order);
+        $this->assertFalse(StaticOrderRepository::isLeadBooker($order, $orderCustomer->customer));
+    }
+
+    public function testLeadIsLeadBookerWithAdditional()
+    {
+        $order = $this->generateOrder();
+        $this->generateOrderCustomer(false, $order);
+        $this->assertTrue(StaticOrderRepository::isLeadBooker($order, $order->leadBooker->customer));
+    }
+
+    public function testWithNotCustomer()
+    {
+        $customer = Customer::factory()->create();
+        $order = $this->generateOrder();
+        $this->assertFalse(StaticOrderRepository::isLeadBooker($order, $customer));
+    }
+
+}
