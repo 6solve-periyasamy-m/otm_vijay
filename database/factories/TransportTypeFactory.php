f828bb6c6260abdf6a2f50e9bd652c1592c6ffc0
Celeste Gateley <celeste.gateley@gmail.com>
2022-05-17 14:57:45 +0100

    Implemented TransportType factory

---
+++ database/factories/TransportTypeFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Transport\TransportType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TransportTypeFactory extends Factory
+{
+    protected $model = TransportType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+        ];
+    }
+}
