8174edab271b6e88e9f7a968769bc71afe377b88
Celeste Gateley <celeste.gateley@gmail.com>
2022-05-24 15:43:33 +0100

    Implemented test for ManualAdjustmentController::destroy

--- tests/Feature/Route/Admin/Order/OrderManualAdjustmentRouteTest.php
+++ tests/Feature/Route/Admin/Order/OrderManualAdjustmentRouteTest.php
@@ -31,4 +31,15 @@ class OrderManualAdjustmentRouteTest extends AuthenticatedRouteTestCase
         $orderInstallment = $this->generateManualAdjustment(100);
         $this->performAllForRoute($this->class, 'create', 'manual-adjustments.create', ['order' => $orderInstallment->order, 'manualAdjustment' => $orderInstallment,]);
     }
+
+    /**
+     * @covers \App\Http\Controllers\Models\ManualAdjustmentController::destroy
+     * @return void
+     */
+    public function testOrderCustomerAdjustmentDelete(): void
+    {
+        $orderInstallment = $this->generateManualAdjustment(100);
+        $this->performAllForDeleteRoute($this->class, 'delete', 'manual-adjustments.delete',
+            ['order' => $orderInstallment->order, 'manualAdjustment' => $orderInstallment], $orderInstallment);
+    }
 }
