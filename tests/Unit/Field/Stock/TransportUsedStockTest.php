f416fd73d0922316a6d17adc9afcbb3b7e056625
Celeste Gateley <celeste.gateley@gmail.com>
2022-05-26 11:13:22 +0100

    Implemented test for Transport used stock

---
+++ tests/Unit/Field/Stock/TransportUsedStockTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Field\Stock;
+
+use Tests\DatabaseTestCase;
+use Tests\Traits\TestsOrder;
+
+/**
+ * @covers \App\Models\Transport\TransportInventory::getUsedStockAttribute
+ * @covers \App\Repository\StockRepository::getTransportStock
+ */
+class TransportUsedStockTest extends DatabaseTestCase
+{
+    use TestsOrder;
+
+    public function testZeroIfUnused()
+    {
+        $inventory = $this->generateTransportInventory();
+        $this->assertEquals(0, $inventory->used_stock);
+    }
+
+    public function testZeroIfCancelled()
+    {
+        $inventory = $this->generateTransportInventory();
+        $tourInventory = $this->generateTransportInventoryTour(null, 'Included', 100, $inventory);
+        $order = $this->generateOrder(true, false);
+        $tourInventory->addToOrder($order->leadBooker);
+        $order->cancelled = true;
+        $order->save();
+        $this->assertEquals(0, $inventory->used_stock);
+    }
+
+    public function testZeroIfDeleted()
+    {
+        $inventory = $this->generateTransportInventory();
+        $tourInventory = $this->generateTransportInventoryTour(null, 'Included', 100, $inventory);
+        $order = $this->generateOrder(true, false);
+        $orderInventory = $tourInventory->addToOrder($order->leadBooker);
+        $orderInventory->delete();
+        $this->assertEquals(0, $inventory->used_stock);
+    }
+
+    public function testWithOneOrderCustomer()
+    {
+        $inventory = $this->generateTransportInventory();
+        $tourInventory = $this->generateTransportInventoryTour(null, 'Included', 100, $inventory);
+        $order = $this->generateOrder(true, false);
+        $tourInventory->addToOrder($order->leadBooker);
+        $this->assertEquals(1, $inventory->used_stock);
+    }
+
+    public function testWithMultipleCustomersOnSingleOrder()
+    {
+        $inventory = $this->generateTransportInventory();
+        $tourInventory = $this->generateTransportInventoryTour(null, 'Included', 100, $inventory);
+        $order = $this->generateOrder(true, false);
+        $tourInventory->addToOrder($order->leadBooker);
+        for ($i = 0; $i < 5; $i++) {
+            $orderCustomer = $this->generateOrderCustomer(false, $order);
+            $tourInventory->addToOrder($orderCustomer);
+        }
+        $this->assertEquals(6, $inventory->used_stock);
+    }
+
+    public function testWithMultipleOrders()
+    {
+        $inventory = $this->generateTransportInventory();
+        $tourInventory = $this->generateTransportInventoryTour(null, 'Included', 100, $inventory);
+        for ($i = 0; $i < 5; $i++) {
+            $order = $this->generateOrder(true, false);
+            $tourInventory->addToOrder($order->leadBooker);
+            for ($ix = 0; $ix < 5; $ix++) {
+                $orderCustomer = $this->generateOrderCustomer(false, $order);
+                $tourInventory->addToOrder($orderCustomer);
+            }
+        }
+        $this->assertEquals(30, $inventory->used_stock);
+    }
+}
