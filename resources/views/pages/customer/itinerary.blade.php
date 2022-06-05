96aafe66dbce2eb2bcc6dca72736e455a4d879f0
benjamin-Keller <zhero240@gmail.com>
2022-06-05 17:28:45 +0200

    Cleanup

--- resources/views/pages/customer/itinerary.blade.php
+++ resources/views/pages/customer/itinerary.blade.php
@@ -14,7 +14,7 @@
 <div class="row payment-balance">
     <div class="col-12">
         <form class="form-horizontal mx-2">
-            <div class="form-group d-flex align-items-center flex-wrap">
+            <div class="form-group order-select-wrapper">
                 <p class="mb-0 heading">Select Order</p>
                 <select class="form-select order-select" onchange="onOrderChange(this);" id="booking_reference">
                     @foreach($orders as $selector)
