b8238b950ce314ddb35613944d3724b9990f104d
benjamin-Keller <zhero240@gmail.com>
2022-06-05 17:25:59 +0200

    Updates components

--- resources/views/partials/fields/selector/upgrade-purchase.blade.php
+++ resources/views/partials/fields/selector/upgrade-purchase.blade.php
@@ -3,15 +3,17 @@
 @endpush
 <div class="form-group col-12 {{ isset($width) ? 'col-xl-' . $width : '' }} {{ $divClasses ?? "" }}">
     @isset($name)<label for="{{ $field }}-input">{{ $name }}</label>@endisset
-    <div class="d-flex">
+    <div class="d-flex flex-wrap justify-content-between">
         <select class="form-control {{ $field }}-input" id="{{ $field }}-input" name="{{ $field }}">
             @foreach($options as $key => $value)
                 <option value="{{ $key }}" @if($selected == $key) selected @endif>{{ $value }}</option>
             @endforeach
         </select>
+        <span class="p-2">
         @if(!(\App\Repository\SettingsRepository::getBoolean('payment.required', true)))
         <a href="{{ $createRoute }}" target="{{ $target ?? '_blank' }}" class="btn btn-success d-inline ms-1" onclick="{{$onclick ?? ''}}">+</a>
         @endif
         <a href="{{ $purchaseRoute }}" target="{{ $targetPurchase ?? '_blank' }}" class="btn btn-primary d-inline ms-1" onclick="{{$onclickPurchase ?? ''}}">$</a>
+        </span>
     </div>
 </div>
