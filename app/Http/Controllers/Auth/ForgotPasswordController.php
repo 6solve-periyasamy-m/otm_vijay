609bffa17a65467bf7fdc825b37664c77c118025
Celeste Gateley <celeste.gateley@gmail.com>
2022-06-14 19:58:44 +0100

    Fix for the admin password reset form not functioning properly

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -22,6 +22,6 @@ class ForgotPasswordController extends Controller
 
     public function showLinkRequestForm()
     {
-        return view('pages.auth.passwords.reset');
+        return view('pages.auth.passwords.email');
     }
 }
