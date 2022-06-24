6a5aae11e3ecd5fe6ec772244f5bb10b1dac3046
Celeste Gateley <celeste.gateley@gmail.com>
2022-06-24 11:11:32 +0100

    Fixed booking form bugs

--- app/Http/Controllers/Customer/CustomerBookingController.php
+++ app/Http/Controllers/Customer/CustomerBookingController.php
@@ -25,6 +25,7 @@ class CustomerBookingController extends Controller
     private function getTour(string $bookingUrl, int $size = 1): ?Tour
     {
         $tour = Tour::where('booking_form_url', $bookingUrl)->where('is_active', true)->first();
+        if (!isset($tour)) return null;
         if ($tour->stock_control_active &&
             $tour->stock - $tour->getUsedStock() < $size) {
             return null;
@@ -179,6 +180,6 @@ class CustomerBookingController extends Controller
         $min = max($dueToday, 0.3);
         $max = min($dueToday, 999999.99);
         $request->validate(['amount' => 'required|numeric|min:' . $min . '|max:' . $max]);
-        return StripeGateway::checkout([['name' => "Deposit for Booking from {$booking->customer->full_name}", 'quantity' => 1, 'cost' => $request->amount]], $booking->token, 'Deposit', $booking->customer->id);
+        return StripeGateway::checkout([['name' => "Deposit for Booking from {$booking->leadTraveller->full_name}", 'quantity' => 1, 'cost' => $request->amount]], $booking->token, 'Deposit', $booking->customer->id);
     }
 }
