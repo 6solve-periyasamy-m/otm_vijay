9b39ddcdda874cf5bba2b4bc670c6b6e5dd1e29f
Celeste Gateley <celeste.gateley@gmail.com>
2022-06-27 09:38:18 +0100

    Implemented view component for admin input, with floating labels

---
+++ resources/views/components/admin/input.blade.php
@@ -0,0 +1,11 @@
+<div class="form-group form-floating col-12 col-xl-{{ $attributes->get('width', 12) }} {{ $divClasses ?? "" }}" style="padding-left: 5px;">
+    <input type="{{ $attributes->get('type', 'text') }}"
+           class="form-control {{ $attributes->get('name') }}-input"
+           id="{{ $attributes->get('name') }}"
+           name="{{ $attributes->get('name') }}"
+           placeholder="placeholder"
+           value="{{ old( $attributes->get('name')) ?? $attributes->get('value', '') }}"
+           {{ $attributes->has('autocomplete') ? "autocomplete=\"{$attributes->get('autocomplete')}\"" : '' }}
+           {{ $attributes->has('required') ? 'required' :  ''}} @if($attributes->has('onchange')) onchange="{{ $onChange }}" @endif>
+    <label for="{{ $attributes->get('name') }}" style="padding-top: 10px;">{{ $slot }}</label>
+</div>
