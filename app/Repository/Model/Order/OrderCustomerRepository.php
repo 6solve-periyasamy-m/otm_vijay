2cd303f4a459cabcd39f665c789fc5d7209bf681
Celeste Gateley <celeste.gateley@gmail.com>
2022-07-19 11:23:46 +0100

    Disabled purchasing merchandise through components page

--- app/Repository/Model/Order/OrderCustomerRepository.php
+++ app/Repository/Model/Order/OrderCustomerRepository.php
@@ -27,13 +27,13 @@ class OrderCustomerRepository extends ModelRepository
         $order = $this->orderCustomer->order;
         $owned = $this->orderCustomer->repository->getOwnedIds();
         $data = [];
-        foreach ($order->tour->merchandise as $tourComponent) {
-            $owns = in_array($tourComponent->id, $owned['extras']);
-            if (!$tourComponent->is_bookable) continue;
-            if ($tourComponent->available_stock <= 0 && !$owned) continue;
-            $data[] = ['id' => $tourComponent->id, 'name' => $tourComponent->name, 'component' => 'merchandise', 'type' => $tourComponent->tour_component_type,
-                'cost' => $tourComponent->tour_sales_price, 'date' => now()->unix(), 'owned' => $owns,];
-        }
+//        foreach ($order->tour->merchandise as $tourComponent) {
+//            $owns = in_array($tourComponent->id, $owned['extras']);
+//            if (!$tourComponent->is_bookable) continue;
+//            if ($tourComponent->available_stock <= 0 && !$owned) continue;
+//            $data[] = ['id' => $tourComponent->id, 'name' => $tourComponent->name, 'component' => 'merchandise', 'type' => $tourComponent->tour_component_type,
+//                'cost' => $tourComponent->tour_sales_price, 'date' => now()->unix(), 'owned' => $owns,];
+//        }
         foreach ($order->tour->accommodationInventoryTours as $tourComponent) {
             if (!$tourComponent->is_bookable) continue;
             if ($tourComponent->tour_component_type == 'Add-on') {
