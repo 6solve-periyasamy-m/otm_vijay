e103acb8918c73707a1ed8e9a1a195fb2e463e47
Celeste Gateley <celeste.gateley@gmail.com>
2022-07-21 10:55:32 +0100

    Fix for incorrect data in merchandise

--- resources/views/partials/reports/tables/merchandise.blade.php
+++ resources/views/partials/reports/tables/merchandise.blade.php
@@ -19,7 +19,7 @@
             <td>{{ $row->variant }}</td>
             <td>{{ $row->size }}</td>
             <td>{{ $row->tour }}</td>
-            <td>{{ $row->ordered_on }}</td>
+            <td>{{ $row->customer }}</td>
             <td>@if(!$row->has_address)(Lead Address)@endif {{ $row->address }}</td>
             <td>{{ $row->cost }}</td>
             <td>{{ f_datetime($row->ordered_on) }}</td>
