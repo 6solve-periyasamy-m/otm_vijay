3a8de96ad543ff35f318fb0b05a88a4dfbb83b08
Celeste Gateley <celeste.gateley@gmail.com>
2022-07-25 11:40:08 +0100

    Fix for route type on store-basic quote

--- routes/web.php
+++ routes/web.php
@@ -254,7 +254,7 @@ Route::middleware('auth:web')->prefix('admin')->group(function () {
         Route::get('/', [QuoteController::class, 'index'])->name('all')->middleware('bouncer:Quote\Quote,read');
         Route::post('/create', [QuoteController::class, 'storeBespoke'])->name('store-bespoke')->middleware('bouncer:Quote\Quote,create');
         Route::get('/create/{tour?}', [QuoteController::class, 'create'])->name('create')->middleware('bouncer:Quote\Quote,create');
-        Route::get('/create/{tour}', [QuoteController::class, 'storeBasic'])->name('store-basic')->middleware('bouncer:Quote\Quote,create');
+        Route::post('/create/{tour}', [QuoteController::class, 'storeBasic'])->name('store-basic')->middleware('bouncer:Quote\Quote,create');
         Route::prefix('{quote}')->group(function () {
             Route::get('/', [QuoteController::class, 'view'])->name('view')->middleware('bouncer:Quote\Quote,read');
             Route::get('/update', [QuoteController::class, 'edit'])->name('edit')->middleware('bouncer:Quote\Quote,update');
