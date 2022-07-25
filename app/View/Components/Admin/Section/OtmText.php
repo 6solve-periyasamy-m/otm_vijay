fc3b49915febad60b691cec3fcd0b99bde98a353
Celeste Gateley <celeste.gateley@gmail.com>
2022-07-25 13:43:53 +0100

    Added otm-text and otm-card for building headers

---
+++ app/View/Components/Admin/Section/OtmText.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\View\Components\Admin\Section;
+
+use Illuminate\View\Component;
+
+class OtmText extends Component
+{
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.admin.section.otm-text');
+    }
+}
---
+++ resources/views/components/admin/section/otm-card.blade.php
@@ -0,0 +1,5 @@
+<div class="col-xxl-3 col-xl-4 col-md-6 col-sm-12">
+    <div class="otm-card {{ $attributes->has('row') ? 'row' : '' }}">
+        {{ $slot }}
+    </div>
+</div>
