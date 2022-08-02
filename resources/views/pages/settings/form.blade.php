6b874a1af1fb3e4b62494817ce236c62d3486a6d
Celeste Gateley <celeste.gateley@gmail.com>
2022-08-02 17:35:17 +0100

    Added setting for quote prefix

--- resources/views/pages/settings/form.blade.php
+++ resources/views/pages/settings/form.blade.php
@@ -16,9 +16,10 @@
     @include('partials.fields.text', ['name' => 'Company Address Country', 'field' => 'country', 'value' => setting('company.address.country', ''), 'width' => 6])
     @include('partials.fields.text', ['name' => 'Company Address Postcode', 'field' => 'postcode', 'value' => setting('company.address.postcode', ''), 'width' => 6])
     <hr class="splitter"/>
-    @include('partials.fields.text', ['name' => 'Booking Reference Prefix', 'field' => 'booking_prefix', 'value' => setting('booking.prefix', ''), 'width' => 4])
-    @include('partials.fields.text', ['name' => 'ATOL Issuer', 'field' => 'atol_issuer', 'value' => setting('atol.issuer', ''), 'width' => 4])
-    @include('partials.fields.text', ['name' => 'ATOL Number', 'field' => 'atol_number', 'value' => setting('atol.number', ''), 'width' => 4])
+    @include('partials.fields.text', ['name' => 'Booking Reference Prefix', 'field' => 'booking_prefix', 'value' => setting('quote.prefix', 'OTMQ'), 'width' => 3])
+    @include('partials.fields.text', ['name' => 'Booking Reference Prefix', 'field' => 'booking_prefix', 'value' => setting('booking.prefix', ''), 'width' => 3])
+    @include('partials.fields.text', ['name' => 'ATOL Issuer', 'field' => 'atol_issuer', 'value' => setting('atol.issuer', ''), 'width' => 3])
+    @include('partials.fields.text', ['name' => 'ATOL Number', 'field' => 'atol_number', 'value' => setting('atol.number', ''), 'width' => 3])
     <hr class="splitter"/>
     @include('partials.fields.file', ['name' => 'Company Logo', 'field' => 'company_logo', 'width' => 6])
     @include('partials.fields.file', ['name' => 'ATOL Stamp', 'field' => 'atol_stamp', 'width' => 6])
