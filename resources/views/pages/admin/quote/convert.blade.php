61287f72a09856d753ec719c57995c9cad5547f7
Celeste Gateley <celeste.gateley@gmail.com>
2022-08-02 12:57:01 +0100

    Made convert form start at 1 instead of 0

--- resources/views/pages/admin/quote/convert.blade.php
+++ resources/views/pages/admin/quote/convert.blade.php
@@ -5,14 +5,14 @@
 @section('title', 'Conversion of Quote ' . $quote->reference)
 
 @section('form-body')
-    @for($x = 0; $x < $travelling; $x++)
+    @for($x = 1; $x <= $travelling; $x++)
         <x-admin.input.selector.add name="travelling[{{ $x }}]" route="customers" width="6">
             <x-slot:create>{{ route('customers.create') }}</x-slot:create>
             Non-paying Traveller {{ $x }}
         </x-admin.input.selector.add>
     @endfor
     <hr class="splitter">
-    @for($x = 0; $x < $paying; $x++)
+    @for($x = 1; $x <= $paying; $x++)
         <x-admin.input.selector.add name="paying[{{ $x }}]" route="customers" width="6">
             <x-slot:create>{{ route('customers.create') }}</x-slot:create>
             Paying Traveller {{ $x }}
