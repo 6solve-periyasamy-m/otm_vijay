37b82ab5606cec2029fe8da176f3fd8105a98799
Celeste Gateley <celeste.gateley@gmail.com>
2022-09-05 19:24:30 +0100

    Added expected revenue table to home screen

--- resources/views/components/loading-spinner.blade.php
+++ resources/views/components/loading-spinner.blade.php
@@ -1,4 +1,4 @@
-<div class="w-100 d-flex justify-content-center align-content-center loader-replace" style="padding: 10rem 0">
+<div class="w-100 justify-content-center align-content-center loader-replace" style="padding: 10rem 0; display: flex;">
     <div class="lds-spinner">
         <div></div>
         <div></div>
