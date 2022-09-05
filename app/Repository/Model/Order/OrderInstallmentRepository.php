02c3492f4d9b0314e31989bfbe941eb7abc1a27d
Celeste Gateley <celeste.gateley@gmail.com>
2022-09-05 13:40:22 +0100

    Implemented method for evaluating how much of an installment has been paid

--- app/Repository/Model/Order/OrderInstallmentRepository.php
+++ app/Repository/Model/Order/OrderInstallmentRepository.php
@@ -14,16 +14,35 @@ class OrderInstallmentRepository extends ModelRepository
         $this->installment = $installment;
     }
 
-    public function isInstallmentPaid(): bool
+    public function getOldAmountPaid(): float
     {
         $order = $this->installment->order;
         $paid = sigfig(($order->total_adjustments * -1) + $order->paid - $order->calculated_deposit);
         foreach ($order->installments as $orderInstallment) {
             $paid = sigfig($paid - $orderInstallment->calculated_amount);
-            if ($paid < 0) return false;
-            if ($orderInstallment->id == $this->installment->id) return true;
+            if ($paid < 0 && $orderInstallment->id !== $this->installment->id) return 0;
+            if ($orderInstallment->id == $this->installment->id) return $paid < 0 ? 0 : $this->installment->amount - $paid;
         }
-        return $paid >= 0;
+        return $paid;
+    }
+
+    public function getAmountPaid(): float
+    {
+        $order = $this->installment->order;
+        $paid = sigfig(($order->total_adjustments * -1) + $order->paid - $order->calculated_deposit);
+        foreach ($order->installments as $installment) {
+            if ($installment->id === $this->installment->id) {
+                return min(max($paid, 0), $installment->calculated_amount);
+            }
+            $paid -= $installment->calculated_amount;
+            if ($paid <= 0) return 0;
+        }
+        dd($paid);
+    }
+
+    public function isInstallmentPaid(): bool
+    {
+        return $this->getAmountPaid() == $this->installment->calculated_amount;
     }
 
     public function get(): OrderInstallment
---
+++ tests/Unit/Field/Order/OrderInstallmentAmountPaidTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Field\Order;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\DatabaseTestCase;
+use Tests\Traits\TestsOrder;
+
+/**
+ * @covers \App\Models\Order\OrderInstallment::getPaidAttribute
+ * @covers \App\Repository\Model\Order\OrderInstallmentRepository::isInstallmentPaid
+ */
+class OrderInstallmentAmountPaidTest extends DatabaseTestCase
+{
+    use TestsOrder;
+    use RefreshDatabase;
+
+    public function testInstallmentPaidSinglePayment()
+    {
+        $installment = $this->generateOrderInstallment(now(), 100);
+        $this->generatePayment($installment->order, 100);
+        $this->assertEquals(100, $installment->repository->getAmountPaid());
+    }
+
+    public function testInstallmentPaidMultiplePayments()
+    {
+        $installment = $this->generateOrderInstallment(now(), 100);
+        $this->generatePayment($installment->order, 50);
+        $this->generatePayment($installment->order, 50);
+        $this->assertEquals(100, $installment->repository->getAmountPaid());
+    }
+
+    public function testInstallmentPaidWithDeposit()
+    {
+        $order = $this->generateOrder(true, true, 300, 50, 100);
+        $installment = $this->generateOrderInstallment(now(), 100, $order);
+        $this->generatePayment($order, 200);
+        $this->assertEquals(100, $installment->repository->getAmountPaid());
+    }
+
+    public function testInstallmentNotPaid()
+    {
+        $installment = $this->generateOrderInstallment(now(), 100);
+        $this->assertEquals(0, $installment->repository->getAmountPaid());
+    }
+
+    public function testInstallmentNotPaidWithDeposit()
+    {
+        $order = $this->generateOrder(true, true, 300, 50, 100);
+        $installment = $this->generateOrderInstallment(now(), 100, $order);
+        $this->assertEquals(0, $installment->repository->getAmountPaid());
+    }
+
+    public function testInstallmentPartiallyPaidWithoutDeposit()
+    {
+        $installment = $this->generateOrderInstallment(now(), 100);
+        $this->generatePayment($installment->order, 50);
+        $this->assertEquals(50, $installment->repository->getAmountPaid());
+    }
+
+    public function testInstallmentPartiallyPaidWithDeposit()
+    {
+        $order = $this->generateOrder(true, true, 300, 50, 50);
+        $installment = $this->generateOrderInstallment(now(), 100, $order);
+        $this->generatePayment($installment->order, 100);
+        $this->assertEquals(50, $installment->repository->getAmountPaid());
+    }
+}
