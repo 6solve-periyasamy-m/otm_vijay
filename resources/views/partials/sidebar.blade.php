1dd185db4d4fbcb053f8f06f63ad950a511ce88e
Celeste Gateley <celeste.gateley@gmail.com>
2022-10-14 14:06:48 +0100

    Shrunk sidebar links to allow more to be seen

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="col-12 col-md-3 col-xl-2 p-0  otm-sidebar d-block py-3">
+<div class="otm-sidebar d-flex flex-column flex-shrink-0 p-0 pt-3 col-12 col-md-3 col-xl-2 p-0">
     <ul class="nav flex-column mb-auto">
         @foreach(\App\View\Components\Admin\SidebarLink::getSidebarLinks() as $sidebarLink)
             {{ $sidebarLink->render() }}
