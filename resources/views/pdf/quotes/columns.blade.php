2e21aff9902c10c27062f4b98abb60a1dd383baf
Celeste Gateley <celeste.gateley@gmail.com>
2022-10-28 14:51:11 +0100

    Adjusted styling on quote document

--- resources/views/pdf/quotes/columns.blade.php
+++ resources/views/pdf/quotes/columns.blade.php
@@ -122,6 +122,7 @@ $paying = $sent->paid;
                 <div class="pageborder"></div>
             @endif
             <!-- Order Section -->
+            @if($quote->repository->hasComponents())
             <div class="section">
                 <h2 class="section-title header-title">Breakdown</h2>
                 <table class="order-table center">
@@ -133,6 +134,7 @@ $paying = $sent->paid;
                         </tr>
                     </thead>
                     <tbody>
+                        @if(sizeof($quote->activities))
                         <tr>
                             <td colspan="3" class="metadata center-text pagebreak">Accommodation</td>
                         </tr>
@@ -143,6 +145,8 @@ $paying = $sent->paid;
                                 <td class="quantity">{{ $paying + $travelling }}</td>
                             </tr>
                         @endforeach
+                        @endif
+                        @if(sizeof($quote->activities))
                         <tr>
                             <td colspan="3" class="metadata center-text pagebreak">Activities</td>
                         </tr>
@@ -153,6 +157,8 @@ $paying = $sent->paid;
                                 <td class="quantity">{{ $paying + $travelling }}</td>
                             </tr>
                         @endforeach
+                        @endif
+                        @if(sizeof($quote->flights))
                         <tr>
                             <td colspan="3" class="metadata center-text pagebreak">Flights</td>
                         </tr>
@@ -163,6 +169,8 @@ $paying = $sent->paid;
                                 <td class="quantity">{{ $paying + $travelling }}</td>
                             </tr>
                         @endforeach
+                        @endif
+                        @if(sizeof($quote->transport))
                         <tr>
                             <td colspan="3" class="metadata center-text pagebreak">Transport</td>
                         </tr>
@@ -173,15 +181,18 @@ $paying = $sent->paid;
                                 <td class="quantity">{{ $paying + $travelling }}</td>
                             </tr>
                         @endforeach
+                        @endif
                         <tr>
                             <td colspan="3" class="metadata right-text">Total: {{ f_currency($quote->repository->getTotalCost($paying)) }}</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
+            @endif
             <div class="pagebreak"></div>
             <div class="pageborder"></div>
             <!-- Installments Section -->
+            @if($quote->deposit > 0 || sizeof($quote->installments))
             <div class="section pagebreak-inside">
                 <h2 class="section-title header-title">Installments</h2>
                 <table class="order-table center">
@@ -215,7 +226,8 @@ $paying = $sent->paid;
             </div>
             <div class="pagebreak"></div>
             <div class="pageborder"></div>
-            <!-- Installments Section -->
+            @endif
+            <!-- Price Per Traveller Section -->
             <div class="section pagebreak-inside">
                 <h2 class="section-title header-title">Price per Traveller Matrix</h2>
                 <table class="order-table center">
@@ -238,32 +250,30 @@ $paying = $sent->paid;
                     </tbody>
                 </table>
             </div>
+            <!-- Terms Section -->
+            @if(!empty($quote->terms))
+            <div class="section pagebreak-inside">
+                <h2 class="section-title header-title">Terms and Conditions</h2>
+                <div class="notes">
+                    <div style="margin-top: 0">{!! $quote->terms !!}</div>
+                </div>
+            </div>
+            @endif
+            <!-- Footer Section -->
+            @if(!empty($quote->footer))
+            <div class="section pagebreak-inside">
+                <h2 class="section-title header-title">Additional Information</h2>
+                <div class="notes">
+                    <div style="margin-top: 0">{!! $quote->footer !!}</div>
+                </div>
+            </div>
+            @endif
             <!-- Notes Section -->
+            @if(!empty($quote->external_notes))
             <div class="section pagebreak-inside">
                 <h2 class="section-title header-title">Notes</h2>
                 <div class="notes">
-                    <div style="margin-top: 0">{!! $quote->invoice_footer !!}</div>
-                </div>
-            </div>
-            @if((isset($extended) && $extended) || $quote->extended ?? false)
-            <div class="section pagebreak-inside">
-                <h2 class="section-title header-title">Itinerary</h2>
-                <div class="cards">
-                    @foreach($quote->repository->getItinerary() as $repository)
-                    <div class="card">
-                        <div class="item">
-                            <img src="{{ $repository->getItineraryAsset() }}" />
-                        </div>
-                        <div class="item">
-                            <div style="display:inline-block">
-                                <h4>{{ $repository->getItineraryTitle() }}</h4>
-                                {{ $repository->getItineraryDescription() }}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pagebreak"></div>
-                    <div class="pageborder"></div>
-                    @endforeach
+                    <div style="margin-top: 0">{!! $quote->external_notes !!}</div>
                 </div>
             </div>
             @endif
