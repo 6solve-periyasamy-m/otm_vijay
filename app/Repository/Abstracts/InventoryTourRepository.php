0a9f84f5e0670efb951cb5adabbb2eb19ce5967d
Celeste Gateley <celeste.gateley@gmail.com>
2022-11-06 18:22:57 +0000

    Fix for duplicated code for upgrade key maps

--- app/Repository/Abstracts/InventoryTourRepository.php
+++ app/Repository/Abstracts/InventoryTourRepository.php
@@ -41,6 +41,30 @@ abstract class InventoryTourRepository extends InventoryContainerRepository impl
 
     public abstract function getUsedOnOrderCount(): int;
 
+    public function getUpgradeKeyMap(int $required = 0, bool $stock = false, bool $downgrade = true): array
+    {
+        $component = $this->get();
+        $parent = $this->getUpgradeParent();
+        if ($component->id == $parent->id) {
+            $upgrades = $component->upgrades;
+            $cost = 0;
+        } else {
+            $upgrades = $parent->upgrades;
+            $cost = $component->tour_sales_price;
+        }
+        $data = [];
+        if ($downgrade && ($required <= 0 || $parent->repository->getAvailableStock() >= $required)) {
+            $data[0] = 'Included - ' . f_currency(0) . ($stock ? ' - Stock ' . $parent->repository->getAvailableStock() . '/' . $parent->repository->getTotalStock() : '');
+        }
+        foreach ($upgrades as $upgrade) {
+            if ($required > 0 && $upgrade->upgrade->repository->getAvailableStock() < $required) continue;
+            if (!$downgrade && $upgrade->upgrade->tour_sales_price < $cost) continue;
+            $data[$upgrade->id] = $upgrade->description . ' - ' . f_currency($upgrade->upgrade->tour_sales_price)
+                . ($stock ? ' - Stock ' . $upgrade->upgrade->repository->getAvailableStock() . '/' . $upgrade->upgrade->repository->getTotalStock() : '');
+        }
+        return $data;
+    }
+
     public static function getComponent(string $type, int $id): ?InventoryTourRepository
     {
         return match ($type) {
