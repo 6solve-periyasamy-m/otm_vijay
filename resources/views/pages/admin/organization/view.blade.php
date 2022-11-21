e6d36f27f8f553d67daa6c32d0e69b633e3c1853
Celeste Gateley <celeste.gateley@gmail.com>
2022-11-21 20:59:17 +0000

    Added lead name to organization pages

--- resources/views/pages/admin/organization/view.blade.php
+++ resources/views/pages/admin/organization/view.blade.php
@@ -89,6 +89,7 @@
                         <tr>
                             <th scope="col">Booking Reference</th>
                             <th scope="col">Tour Name</th>
+                            <th scope="col">Lead</th>
                             <th scope="col">Ordered On</th>
                             <th scope="col">Tour Cost</th>
                             <th scope="col">Order Status</th>
@@ -98,6 +99,7 @@
                             <tr>
                                 <th scope="row"><a href="{{ route('orders.view', ['order' => $order,]) }}">{{ $order->booking_reference }}</a></th>
                                 <td>{{ $order->tour->name }}</td>
+                                <td>{{ $order->leadBooker->customer_name }}</td>
                                 <td>{{ f_datetime($order->ordered_on) }}</td>
                                 <td>{{ f_currency($order->total) }}</td>
                                 <td><h6 class="badge badge-{{ $order->status->color() }} fw-bold">{{ $order->status->description() }}</h6></td>
@@ -115,6 +117,7 @@
                         <tr>
                             <th scope="col">Quote Reference</th>
                             <th scope="col">Name</th>
+                            <th scope="col">Lead</th>
                             <th scope="col">Expiry Date</th>
                             <th scope="col">Quote Status</th>
                         </tr>
@@ -123,6 +126,7 @@
                             <tr>
                                 <th scope="row"><a href="{{ route('quotes.view', ['quote' => $quote,]) }}">{{ $quote->ref }}</a></th>
                                 <td>{{ $quote->name }}</td>
+                                <td>{{ $quote->leadTraveller->name }}</td>
                                 <td>{{ f_date($quote->expires) }}</td>
                                 <td>{{ $quote->status->badge() }}</td>
                             </tr>
