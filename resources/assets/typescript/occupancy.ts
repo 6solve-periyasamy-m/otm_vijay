0c44c04336f0e5b5f193e95a0a752f5e5448b5bb
Celeste Gateley <celeste.gateley@gmail.com>
2022-11-21 15:15:03 +0000

    Implemented basic page functionality for occupancy

--- resources/assets/typescript/occupancy.ts
+++ resources/assets/typescript/occupancy.ts
@@ -34,12 +34,20 @@ class Group {
         this.customers = customers;
         this.room = room;
     }
+
+    public addCustomer(customer: Customer) {
+        this.customers.push(customer);
+    }
+
+    public removeCustomer(customer: Customer) {
+        this.customers = removeElement<Customer>(this.customers, customer, (obj1, obj2) => { return obj1.id == obj2.id; })
+    }
 }
 
 class RoomingData {
-    groups: Group[];
-    customers: Customer[];
-    rooms: Room[];
+    private groups: Group[];
+    private readonly customers: Customer[];
+    private readonly rooms: Room[];
 
     constructor(rooms: Room[], customers: Customer[], groups: Group[]) {
         this.groups = groups;
@@ -47,17 +55,72 @@ class RoomingData {
         this.rooms = rooms;
     }
 
-    public getRooms(): Room[]
-    {
+    public getRooms(): Room[] {
         return this.rooms;
     }
 
-    public getRoom(id: number): Room|null
-    {
+    public getRoom(id: number): Room | null {
         for (const room of this.rooms) {
-            if (room.id === id) return room;
+            if (((room.id as unknown) as number) == id) return room;
         }
         return null;
+    }
+
+    public getGroups(): Group[] {
+        return this.groups;
+    }
+
+    public getOrphanedCustomers(): Customer[] {
+        let owned = [];
+        for (const group of this.groups) {
+            for (const customer of group.customers) {
+                owned.push(customer.id);
+            }
+        }
+        let unowned: Customer[] = [];
+        for (const customer of this.customers) {
+            if (owned.includes(customer.id)) continue;
+            unowned.push(customer);
+        }
+        return unowned;
+    }
+
+    public getGroup(id: number): Group | null {
+        for (const group of this.groups) {
+            if (group.id == id) return group;
+        }
+        return null;
+    }
+
+    public removeFromGroup(groupId: number, customerId: number) {
+        let group = this.getGroup(groupId);
+        let customer = this.getCustomer(customerId);
+        if (group !== null && customer !== null) {
+            group.removeCustomer(customer);
+        }
+    }
+
+    public addToGroup(groupId: number, customerId: number) {
+        let group = this.getGroup(groupId);
+        let customer = this.getCustomer(customerId);
+        if (group !== null && customer !== null) {
+            group.addCustomer(customer);
+        }
+    }
+
+    public getCustomers(): Customer[] {
+        return this.customers;
+    }
+
+    public getCustomer(id: number): Customer | null {
+        for (const customer of this.customers) {
+            if (customer.id == id) return customer;
+        }
+        return null;
+    }
+
+    public reset(): void {
+        this.groups = [];
     }
 }
 
@@ -83,25 +146,44 @@ interface RemoteRoomingData {
     groups: RemoteGroup[];
 }
 
-async function generateRoomingManager(url: string, parameters: Object  = {}): Promise<RoomingData> {
-    let data :RemoteRoomingData = await $.post({
+interface EquivalenceCallback<T> {
+    (obj1: T, obj2: T): boolean;
+}
+
+function removeElement<T>(array: T[], item: T, callback: EquivalenceCallback<T>|null): T[] {
+    for (let i = 0; i < array.length; i++) {
+        let equal = false;
+        if (callback !== null) {
+            equal = callback(array[i], item);
+        } else {
+            equal = array[i] == item;
+        }
+        if (equal) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
+}
+
+async function generateRoomingManager(url: string, parameters: Object = {}): Promise<RoomingData> {
+    let data: RemoteRoomingData = await $.post({
         url: url,
         dataType: "json",
         data: parameters,
     });
     let customers: Customer[] = [];
     for (const id of Object.keys(data.customers)) {
-        let nId: number = ((id as unknown) as number);
+        let nId: number = parseInt(id);
         customers.push(new Customer(nId, data.customers[nId].name, data.customers[nId].avatar));
     }
     let rooms = [];
     for (const id of Object.keys(data.rooms)) {
-        let nId: number = ((id as unknown) as number);
+        let nId: number = parseInt(id);
         rooms.push(new Room(nId, data.rooms[nId].name, data.rooms[nId].size));
     }
     let groups = [];
     for (const id of Object.keys(data.groups)) {
-        let nId: number = ((id as unknown) as number);
+        let nId: number = parseInt(id);
         let groupCustomers: Customer[] = [];
         for (const [key, customer] of Object.entries(customers)) {
             if (data.groups[nId].customers.includes(customer.id)) groupCustomers.push(customer);
--- resources/views/pages/occupancy/manager.blade.php
+++ resources/views/pages/occupancy/manager.blade.php
@@ -12,7 +12,7 @@
 @section('content')
     <div class="card">
         <div class="card-body col-12">
-            <div class="customers section-box drop-shadow col-12"></div>
+            <div class="customers section-box drop-shadow col-12 droppable"></div>
             <div class="col-12" style="margin: 10px;">
                 <select class="room-types"></select>
                 <a href="#" class="btn btn-danger round" onclick="reset()">Reset</a>
@@ -20,7 +20,7 @@
                 <a href="#" class="btn btn-success round" onclick="submit()">Save</a>
                 <a href="{{ route('orders.view', ['order' => $order,]) }}" class="btn btn-info round">Return to Order</a>
             </div>
-            <div class="manager section-box drop-shadow droppable"></div>
+            <div class="manager section-box drop-shadow"></div>
         </div>
     </div>
 @endsection
@@ -28,7 +28,7 @@
 {{-- Templates --}}
 @push('footer-stack')
 <script type="text/template" data-template="bed">
-    <div class="bed">${content}</div>
+    <div class="bed droppable single">${content}</div>
 </script>
 
 <script type="text/template" data-template="room-option">
@@ -36,7 +36,7 @@
 </script>
 
 <script type="text/template" data-template="customer">
-    <div class="customer" customer="${id}">
+    <div class="customer draggable" customer="${id}">
         <div class="customer-container">
             <div class="customer-section">
                 <img src="${avatar}" class="image">
@@ -49,7 +49,7 @@
 </script>
 
 <script type="text/template" data-template="room">
-    <div class="room drop-shadow" typeid="${id}">
+    <div class="room drop-shadow" roomid="${id}">
         <div class="details">
             <div class="group-input">
                 <input name="name" class="name-input" type="text" value="${name}"/>
@@ -63,7 +63,7 @@
 </script>
 
 <script type="text/template" data-template="room-locked">
-    <div class="room drop-shadow locked" typeid="${id}">
+    <div class="room drop-shadow locked" roomid="${id}">
         <div class="details">
             <div class="group-input">
                 <input name="name" class="name-input" type="text" value="${name}"/>
@@ -80,26 +80,104 @@
 {{-- JavaScript --}}
 @push('footer-stack')
 <script type="text/javascript">
+    const selector = $('.room-types');
+    const customers = $('.customers');
+    const rooms = $('.manager');
     data = null;
     function initialize() {
         let parameters = { __api_token: '{{ \Auth::user()->getCurrentToken()->token }}', }
 
         occupancy.generate('{{ route('api.orders.rooming.get', ['order' => $order,]) }}', parameters).then((rooming) => {
             data = rooming;
-            let selector = $('.room-types');
+            loadData();
+            setupDragDrop();
+        });
+    }
+
+    function renderCustomer(customer) {
+        return render(template('customer'), {id: customer.id, name: customer.name, avatar: customer.avatar, })
+    }
+
+    function renderGroup(group) {
+        let room = data.getRoom(group.room);
+        let beds = "";
+        let counter = room.size;
+        for (const customer of group.customers) {
+            beds += renderBed(customer);
+            counter--;
+        }
+        for (;counter > 0; counter--) {
+            beds += renderBed();
+        }
+        return render(template('room'), {beds: beds, id: group.id, name: group.name})
+    }
+
+    function renderBed(customer = null) {
+        let content = "";
+        if (customer !== null) {
+            content = renderCustomer(customer);
+        }
+        return render(template('bed'), {content: content,})
+    }
+
+    function loadData() {
+        if (data !== null) {
             for (const room of data.getRooms()) {
                 selector.append(render(template('room-option'), {id: room.id, name: room.name, size: room.size,}))
             }
-        });
+            for (const customer of data.getOrphanedCustomers()) {
+                customers.append(renderCustomer(customer));
+            }
+            for (const group of data.getGroups()) {
+                rooms.append(renderGroup(group));
+            }
+        }
     }
-    function initiateUI() {
-        $('.customer:not(.locked)').draggable({ revert: 'invalid', });
-        $('.customers:not(.locked)').droppable({
+
+    function setupDragDrop() {
+        $('.draggable:not(.locked)').draggable({ revert: 'invalid', });
+        $('.droppable:not(.locked, .single)').droppable({
             drop: function(e, ui) {
-                $(e.target).append($(ui.draggable).detach().css({'top':'','left':''}));
+                processDropEvent($(ui.draggable), e.target)
             }
         })
+        $('.droppable.single:not(.locked)').droppable({
+            accept: function (element) {
+                return !$(this).is(':parent');
+            },
+            drop: function(e, ui) {
+                processDropEvent($(ui.draggable), e.target)
+            }
+        });
     }
+
+    function processDropEvent(element, target) {
+        element = $(element);
+        target = $(target);
+        let old = element.closest('.room');
+        $(target).append(element.detach().css({'top':'','left':''}));
+        let customer = element.attr('customer');
+        if (data !== null) {
+            if (old !== null) {
+                data.removeFromGroup(old.attr('roomid'), customer);
+            }
+            if (target.hasClass('bed')) {
+                let room = $(target).closest('.room');
+
+                data.addToGroup(room.attr('roomid'), customer)
+            }
+            console.log(data.getGroups());
+        }
+    }
+
+    function reset() {
+        customers.empty();
+        rooms.empty();
+        data.reset();
+        loadData();
+        setupDragDrop()
+    }
+
     $(document).ready(() => {
        initialize();
     });
