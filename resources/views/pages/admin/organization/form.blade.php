692b68f7f62505bfe057e04b8386850c86464c22
Celeste Gateley <celeste.gateley@gmail.com>
2022-11-22 15:30:58 +0000

    Fix for organization form prefilling with wrong information

--- resources/views/pages/admin/organization/form.blade.php
+++ resources/views/pages/admin/organization/form.blade.php
@@ -11,8 +11,8 @@ $organization = $organization ?? null;
 
 @section('form-body')
 <x-admin.input name="name" width="4" value="{{ $organization?->name }}">Organization Name</x-admin.input>
-<x-admin.input name="phone" width="4" value="{{ $organization?->name }}">Contact Telephone</x-admin.input>
-<x-admin.input name="email" width="4" value="{{ $organization?->name }}">Contact Email</x-admin.input>
+<x-admin.input name="phone" width="4" value="{{ $organization?->contact_number }}">Contact Telephone</x-admin.input>
+<x-admin.input name="email" width="4" value="{{ $organization?->contact_email }}">Contact Email</x-admin.input>
 <x-admin.input.text-area name="internal_notes" width="6" value="{{ $organization?->internal_notes }}">Internal Notes</x-admin.input.text-area>
 <x-admin.input.text-area name="external_notes" width="6" value="{{ $organization?->external_notes }}">External Notes</x-admin.input.text-area>
 <input type="submit" class="btn btn-primary text-white" name="Submit">
