8b96f863737d8f370af4cc4bbaf87df50d63d107
Celeste Gateley <celeste.gateley@gmail.com>
2023-02-07 15:24:34 +0000

    Revert "Tour Options Menu"

--- resources/views/components/admin/popup-button.blade.php
+++
@@ -1,8 +0,0 @@
-<a {{ $attributes->class(['popup-grid-item']) }}>
-    <div class="icon">
-        <i class="icon-{{ $icon }}"></i>
-    </div>
-    <div class="text">
-        {{ $slot }}
-    </div>
-</a>
