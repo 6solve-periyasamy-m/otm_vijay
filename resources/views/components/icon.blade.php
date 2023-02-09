223f1aeb861301518248700e92dd6654c2fac7ea
Celeste Gateley <celeste.gateley@gmail.com>
2023-02-09 14:20:12 +0000

    Caught missing icon references

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -1,1 +1,5 @@
-<i class="icon-{{$icon ?? $attributes->get('icon', 'redo')}}">{{$slot??""}}</i>
+@php
+    $attributes = $attributes ?? new \Illuminate\View\ComponentAttributeBag();
+    $icon = $icon ?? $attributes?->get('icon', 'redo') ?? 'redo';
+@endphp
+<i {{ $attributes?->class("icon-{$icon}") }}>{{$slot??""}}</i>
