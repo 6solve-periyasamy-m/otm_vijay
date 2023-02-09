40dec67279b0e3cbedfd16ea9f70e14b052ba396
Celeste Gateley <celeste.gateley@gmail.com>
2023-02-09 12:41:38 +0000

    Fix for odd spacing and styling on tour add component page

---
+++ resources/views/partials/components/add-bar.blade.php
@@ -0,0 +1,14 @@
+<div class="d-inline-flex mb-3 col-12">
+    <div class="col-xl-10">
+        <select class="form-select {{$component}}-component-type-select">
+            <option value="Included" selected>Included</option>
+            <option value="Add-on">Add-on</option>
+        </select>
+    </div>
+    <div class="col-xl-2">
+        <a href="{{$href}}" class="btn btn-primary ms-3 text-white">
+            <i class="icon-plus"></i>
+            <span>Add Selected Rows</span>
+        </a>
+    </div>
+</div>
