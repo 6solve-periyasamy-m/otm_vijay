e1ab8f3973002ebb375f2243a76405ad591b97f0
Celeste Gateley <celeste.gateley@gmail.com>
2023-03-02 16:50:24 +0000

    Added shortcode and column for remaining on next for installment

--- resources/views/partials/reports/tables/reminders.blade.php
+++ resources/views/partials/reports/tables/reminders.blade.php
@@ -7,7 +7,8 @@
             <th scope="col">Phone Number</th>
             <th scope="col">Days Until</th>
             <th scope="col">Due On</th>
-            <th scope="col">Amount</th>
+            <th scope="col">Total</th>
+            <th scope="col">Remaining</th>
             <th scope="col">Reminded?</th>
         </tr>
     </thead>
@@ -21,6 +22,7 @@
                 <td>{{ $row->days }}</td>
                 <td>{{ f_date($row->next->due_on) }}</td>
                 <td>{{ f_currency($row->next->calculated_amount) }}</td>
+                <td>{{ f_currency($row->next->remaining) }}</td>
                 <td>{{ f_bool($row->reminded) }}</td>
             </tr>
         @endforeach
