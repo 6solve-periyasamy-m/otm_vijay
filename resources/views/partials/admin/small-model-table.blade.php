c5f4c54ef3c03dc3312423ccadeb6bb818bd1795
Celeste Gateley <celeste.gateley@gmail.com>
2023-03-02 11:54:23 +0000

    Made attribute create/edit open in a new tab

--- resources/views/partials/admin/small-model-table.blade.php
+++ resources/views/partials/admin/small-model-table.blade.php
@@ -7,7 +7,7 @@
         <div class="card-title d-flex justify-content-between">
             <h4 class="fw-bold">{{ $repository::getName() }}</h4>
             @if($repository::getCreateUrl() !== null)
-                <a class="btn btn-primary" href="{{ $repository::getCreateUrl() }}">
+                <a class="btn btn-primary" href="{{ $repository::getCreateUrl() }}" target="_blank">
                     {{ Icon::create() }}
                     <span>Create New</span>
                 </a>
@@ -28,7 +28,7 @@
                     <td>{{ $model->repository->getRelatedCount() }}</td>
                     <td class="actions">
                         @if($model->repository->getEditUrl() !== null)
-                            <a href="{{$model->repository->getEditUrl()}}" class="btn btn-sm btn-outline-success mb-1">
+                            <a href="{{$model->repository->getEditUrl()}}" target="_blank" class="btn btn-sm btn-outline-success mb-1">
                                 {{ Icon::edit() }}
                             </a>
                         @else
