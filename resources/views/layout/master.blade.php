b613dffb2007b670d77a14a0f992e2166ca39ce3
Celeste Gateley <celeste.gateley@gmail.com>
2023-03-10 14:31:53 +0000

    Fix for alpine loading too early

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -98,5 +98,7 @@
 @stack('footer-stack')
 @livewireScripts
 @livewire('livewire-ui-modal')
+<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+<script defer src="https://unpkg.com/@alpinejs/focus@3.x.x/dist/cdn.min.js"></script>
 </body>
 </html>
