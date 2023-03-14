4c7cc591f02f026c481d9e35eb8a1358306f996a
Celeste Gateley <celeste.gateley@gmail.com>
2023-03-14 12:29:22 +0000

    Prevent editing brands from clearing the system brand

--- resources/views/livewire/admin/system/brand/list.blade.php
+++ resources/views/livewire/admin/system/brand/list.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-xl-4">
-        <livewire:admin.system.brand.card :brand="$system" />
+        @include('livewire.admin.system.brand.card', ['brand' => $system,])
     </div>
     @foreach($this->brands as $brand)
         <div class="col-xl-4">
