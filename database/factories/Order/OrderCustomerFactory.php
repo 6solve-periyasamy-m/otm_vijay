681393439a93ec02112d7299e064c4382606f3d8
Celeste Gateley <celeste.gateley@gmail.com>
2023-03-30 12:41:53 +0100

    Fix for test suite accidentally assigning two order customers the same customer id

--- database/factories/Order/OrderCustomerFactory.php
+++ database/factories/Order/OrderCustomerFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories\Order;
 
+use App\Models\Customer\Customer;
 use App\Models\Order\OrderCustomer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -21,8 +22,9 @@ class OrderCustomerFactory extends Factory
      */
     public function definition()
     {
+        $customer = Customer::factory()->create();
         return [
-            'customer_id' => $this->faker->numberBetween(1, 5), // Hack method. We know that 5 will be generated before this
+            'customer_id' => $customer->id, // Hack method. We know that 5 will be generated before this
             'tour_cost' => $this->faker->numberBetween(250, 750),
             'single_occupancy_surcharge' => $this->faker->numberBetween(100, 250),
             'travel_insurer' => $this->faker->company,
