45b90c817d4319d7be463ffb3ff64a8fbafd3093
Celeste Gateley <celeste.gateley@gmail.com>
2023-04-04 17:14:04 +0100

    Hid select bar if no upgrades are available

--- resources/views/livewire/customer/booking/component-card.blade.php
+++ resources/views/livewire/customer/booking/component-card.blade.php
@@ -9,29 +9,33 @@
                 {{ f_datetime($this->component->start) }} to {{ f_datetime($this->component->end) }}
             </div>
         </div>
-        <div class="component-text row mx-2">
-            @if($this->component->image !== null)
-                <div class="col-xl-2 col-lg-3 col-md-4 col-6 border-right">
-                    <img src="{{ asset($this->component->image) }}" class="booking-image"/>
-                </div>
-                <div class="col-xl-10 col-lg-9 col-md-8 col-6">
+        <div class="component-text row">
+            <div class="mx-2">
+                @if($this->component->image !== null)
+                    <div class="col-xl-2 col-lg-3 col-md-4 col-6 border-right">
+                        <img src="{{ asset($this->component->image) }}" class="booking-image"/>
+                    </div>
+                    <div class="col-xl-10 col-lg-9 col-md-8 col-6">
+                        {{ $this->component->description }}
+                    </div>
+                @else
                     {{ $this->component->description }}
-                </div>
-            @else
-                {{ $this->component->description }}
-            @endif
+                @endif
+            </div>
         </div>
         <div class="row">
             <div class="col-xl-8 col-lg-8 my-auto">
-                <select class="booking-upgrade" wire:model="upgrade">
-                    <option selected>Please Select an Upgrade</option>
-                    @foreach($this->component->getAvailableUpgrades() as $upgrade)
-                        @php
-                            $owned = $this->component->equals($upgrade);
-                        @endphp
-                        <option value="{{ json_encode($upgrade->toLivewire()) }}" @if($owned) disabled @endif>{{ $upgrade->name }} @if($owned)(Current)@endif</option>
-                    @endforeach
-                </select>
+                @if(sizeof($this->component->getAvailableUpgrades()) > 1)
+                    <select class="booking-upgrade" wire:model="upgrade">
+                        <option selected>Please Select an Upgrade</option>
+                        @foreach($this->component->getAvailableUpgrades() as $upgrade)
+                            @php
+                                $owned = $this->component->equals($upgrade);
+                            @endphp
+                            <option value="{{ json_encode($upgrade->toLivewire()) }}" @if($owned) disabled @endif>{{ $upgrade->name }} @if($owned)(Current)@endif</option>
+                        @endforeach
+                    </select>
+                @endif
             </div>
             <div class="col-6 col-lg-2 col-xl-2 row border-right mx-auto">
                 <div class="col-12 border-bottom text-center buy-header">
