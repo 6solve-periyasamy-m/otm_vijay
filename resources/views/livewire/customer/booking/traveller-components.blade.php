75942b737be1d173ec7bbcf5bb419d80fdd493c9
Celeste Gateley <celeste.gateley@gmail.com>
2023-04-11 10:50:18 +0100

    Implemented accordions on new booking traveller components

--- resources/views/livewire/customer/booking/traveller-components.blade.php
+++ resources/views/livewire/customer/booking/traveller-components.blade.php
@@ -1,14 +1,12 @@
 <div>
     @foreach($this->traveller->repository->getSummaryComponents() as $day => $components)
-        <div class="card">
-          <div class="card-body">
-             <div class="card-title">
-                 {{ \Carbon\Carbon::createFromTimestamp($day)->format('l jS F Y') }}
-             </div>
-          </div>
-        </div>
-        @foreach($components as $component)
-            <livewire:customer.booking.component-card :component="$component"/>
-        @endforeach
+        <x-customer.accordion id="day-{{$day}}" nobg nocontainer>
+            <x-slot:header class="card-body">
+                <h2 class="col-md-12 mb-0">{{ \Carbon\Carbon::createFromTimestamp($day)->format('l jS F Y') }}</h2>
+            </x-slot:header>
+            @foreach($components as $component)
+                <livewire:customer.booking.component-card :component="$component"/>
+            @endforeach
+        </x-customer.accordion>
     @endforeach
 </div>
