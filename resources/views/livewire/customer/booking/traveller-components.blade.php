895f584f395aebea49749afc2bc3d39929561db0
Celeste Gateley <celeste.gateley@gmail.com>
2023-04-11 10:56:27 +0100

    Fix for arrow being too far right

--- resources/views/livewire/customer/booking/traveller-components.blade.php
+++ resources/views/livewire/customer/booking/traveller-components.blade.php
@@ -2,7 +2,7 @@
     @foreach($this->traveller->repository->getSummaryComponents() as $day => $components)
         <x-customer.accordion id="day-{{$day}}" nobg nocontainer>
             <x-slot:header class="card-body">
-                <h2 class="col-md-12 mb-0">{{ \Carbon\Carbon::createFromTimestamp($day)->format('l jS F Y') }}</h2>
+                <h2 class="mb-0">{{ \Carbon\Carbon::createFromTimestamp($day)->format('l jS F Y') }}</h2>
             </x-slot:header>
             @foreach($components as $component)
                 <livewire:customer.booking.component-card :component="$component"/>
