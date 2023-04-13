ffc19c76dba8012e3aa90c8845e3864eed5bb186
Celeste Gateley <celeste.gateley@gmail.com>
2023-04-13 15:33:26 +0100

    Adjusted padding on summary buttons

--- resources/views/partials/customer/booking/customer.blade.php
+++ resources/views/partials/customer/booking/customer.blade.php
@@ -39,10 +39,10 @@
             @else
             <div class="d-flex flex-between">
                 <div class="w-100 text-center p-1">
-                    <button class="btn btn-warning text-dark" style="width: 4rem;" wire:click="$emit('openModal', 'customer.booking.form.additional-traveller', {{ json_encode(['booking' => $traveller->booking->id, 'traveller' => $traveller->id]) }})">Edit</button>
+                    <button class="btn btn-warning text-dark px-auto" style="width: 4rem;" wire:click="$emit('openModal', 'customer.booking.form.additional-traveller', {{ json_encode(['booking' => $traveller->booking->id, 'traveller' => $traveller->id]) }})">Edit</button>
                 </div>
                 <div class="text-center p-1 border-left w-100">
-                    <button class="btn btn-danger text-dark" style="width: 4rem;" wire:click="$emit('openModal', 'customer.booking.form.additional-traveller', {{ json_encode(['booking' => $traveller->booking->id, 'traveller' => $traveller->id]) }})">Delete</button>
+                    <button class="btn btn-danger text-dark px-auto" style="width: 4rem;" wire:click="$emit('openModal', 'customer.booking.form.additional-traveller', {{ json_encode(['booking' => $traveller->booking->id, 'traveller' => $traveller->id]) }})">Delete</button>
                 </div>
             </div>
             @endif
