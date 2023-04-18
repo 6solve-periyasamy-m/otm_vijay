9066b9b684946fd4e07a4dcebcec1962dcf55bea
Celeste Gateley <celeste.gateley@gmail.com>
2023-04-18 09:58:04 +0100

    Added * to required input fields

--- resources/views/components/customer/input.blade.php
+++ resources/views/components/customer/input.blade.php
@@ -1,5 +1,5 @@
 <div class="form-floating form-group col-12 col-xl-{{ $attributes->get('width') ?? $width ?? 12 }}">
     <input {{ $attributes->except('width')->merge(['class' => "form-control float-padding {$attributes->get('name')}-input", 'id' => $attributes->get('name'), 'placeholder' => 'placeholder',]) }} {{ $disable ? 'disabled' : '' }}/>
-    <label for="{{ $attributes->get('id') }}">{{ $slot }}</label>
+    <label for="{{ $attributes->get('id') }}">{{ $slot }} @if($attributes->has('required')) <span class="fw-bold">*</span> @endif</label>
     @error($attributes->get('wire:model') ?? $attributes->get('name'))<span class="text-danger">{{ $message }}</span>@enderror
 </div>
