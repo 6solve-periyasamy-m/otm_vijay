6b26c054faa99bd936b1188a601941c840411ef7
Celeste Gateley <celeste.gateley@gmail.com>
2023-05-05 17:17:52 +0100

    Deleted completely unused views

--- resources/views/pages/booking/event/form.blade.php
+++
@@ -1,6 +0,0 @@
-@extends ('layout.main')
-@section('content')
-<div class="container-fluid" id="app">
-    <booking-form :event="{{$event}}"></booking-form>
-</div>
-@endsection
--- resources/views/pages/models/tour_categories/view.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.main')
-
-@section('title', 'View Tour Category')
-
-@section('content')
-    Name: {{ $tourCategory->name }}<br/>
-@endsection
