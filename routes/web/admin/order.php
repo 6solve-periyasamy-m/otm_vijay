4d59386b15de5c17a4c8f1755a7c9ce550c2a756
Celeste Gateley <celeste.gateley@gmail.com>
2023-05-09 09:56:44 +0100

    Fix for method does not exist on order view

--- routes/web/admin/order.php
+++ routes/web/admin/order.php
@@ -16,7 +16,7 @@ Route::post('/create', [OrderController::class, 'store'])->name('orders.store')-
 Route::get('reminders/authorize/{days}', [SettingsController::class, 'authorizeReminders'])->name('orders.reminders.authorize')->middleware('bouncer:Order\Order,update');
 Route::get('reminders/{max?}/{min?}', [OrderController::class, 'reminders'])->name('orders.reminders')->middleware('bouncer:Order\Order,read');
 Route::prefix('{order}')->group(function () {
-    Route::get('/', [OrderController::class, 'show'])->name("orders.view")->middleware('bouncer:Order\Order,read');
+    Route::get('/', [OrderController::class, 'view'])->name("orders.view")->middleware('bouncer:Order\Order,read');
     Route::get('/update/', [OrderController::class, 'edit'])->name('orders.edit')->middleware('bouncer:Order\Order,update');
     Route::post('/update/', [OrderController::class, 'update'])->name('orders.update')->middleware('bouncer:Order\Order,update');
     Route::post('/delete/', [OrderController::class, 'destroy'])->name('orders.delete')->middleware('bouncer:Order\Order,delete');
