fb6944cd45f1fa8549cb5bba50bda908665db060
Celeste Gateley <celeste.gateley@gmail.com>
2023-06-22 15:01:43 +0100

    Switched to using tailwind preflight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   corePlugins: {
-    preflight: false,
+    preflight: true,
   },
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./vendor/wire-elements/modal/resources/**/*.blade.php",
+    "./vendor/mediconesystems/livewire-datatables/resources/**/*.blade.php",
   ],
   theme: {
     extend: {},
