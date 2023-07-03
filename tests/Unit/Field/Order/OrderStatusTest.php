eec44f9b66e97ac61283a35fe9f782ddd15ff7f5
Celeste Gateley <celeste.gateley@gmail.com>
2023-07-03 13:31:21 +0100

    Fix for OrderStatusTest failing due to default data

--- tests/Unit/Field/Order/OrderStatusTest.php
+++ tests/Unit/Field/Order/OrderStatusTest.php
@@ -19,6 +19,8 @@ class OrderStatusTest extends DatabaseTestCase
     public function testBalanceOutstandingNoInstallments()
     {
         $order = $this->generateOrder();
+        $order->tour->update(['final_payment' => now()->addDays(2)]);
+        $order->tour->save();
         $this->assertEquals(OrderStatus::BALANCE_OUTSTANDING, $order->status);
     }
 
