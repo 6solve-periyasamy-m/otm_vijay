b318db26ed27cde8776db42adfa2dfc64118ae7b
celeste <celeste.gateley@gmail.com>
2023-08-15 11:17:56 +0100

    Removed AddressParent table

---
+++ app/Models/Helper/AddressParent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Helper;
+
+enum AddressParent: string
+{
+    case CUSTOMER = 'customer';
+    case ACCOMMODATION = 'accommodation';
+    case ACTIVITY = 'activity';
+    case AIRPORT = 'airport';
+    case TRANSPORT = 'transport';
+    case OTHER = 'other';
+}
