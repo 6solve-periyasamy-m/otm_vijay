0d657ed8548b52ddc82a97f072f90e0309b6bdbe
Celeste Gateley <celeste.gateley@gmail.com>
2023-08-29 13:37:13 +0100

    Added sorting/searching to associates table

--- app/Http/Livewire/Admin/Supplier/Associate/Table.php
+++ app/Http/Livewire/Admin/Supplier/Associate/Table.php
@@ -17,15 +17,25 @@ class Table extends LivewireDatatable
     {
         return [
             Column::name('name')
-                ->label(__('supplier.associate.table.name')),
+                ->label(__('supplier.associate.table.name'))
+                ->searchable()
+                ->sortable(),
             Column::name('job_title')
-                ->label(__('supplier.associate.table.job_title')),
+                ->label(__('supplier.associate.table.job_title'))
+                ->searchable()
+                ->sortable(),
             Column::callback('email', function ($email) { return "<a href=\"mailto:{$email}\">{$email}</a>"; })
-                ->label(__('supplier.associate.table.email')),
+                ->label(__('supplier.associate.table.email'))
+                ->searchable()
+                ->sortable(),
             Column::callback('primary_phone', function ($tel) { return "<a href=\"tel:{$tel}\">{$tel}</a>"; })
-                ->label(__('supplier.associate.table.primary_phone')),
+                ->label(__('supplier.associate.table.primary_phone'))
+                ->searchable()
+                ->sortable(),
             Column::callback('alternative_phone', function ($tel) { return "<a href=\"tel:{$tel}\">{$tel}</a>"; })
-                ->label(__('supplier.associate.table.alternative_phone')),
+                ->label(__('supplier.associate.table.alternative_phone'))
+                ->searchable()
+                ->sortable(),
         ];
     }
 }
