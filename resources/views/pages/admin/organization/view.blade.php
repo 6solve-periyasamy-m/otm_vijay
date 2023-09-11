ae20de83e163fdd7ffa30f69bdf7014cc029be75
Celeste Gateley <celeste.gateley@gmail.com>
2023-09-11 12:47:01 +0100

    Migrated organization details to livewire

--- resources/views/pages/admin/organization/view.blade.php
+++ resources/views/pages/admin/organization/view.blade.php
@@ -7,48 +7,7 @@
 @endphp
 
 @section('content')
-    <div class="otm-callout">
-        <div class="row">
-            <div class="col-xl-12">
-                <div class="row">
-                    <div class="col-3">
-                        <p>Organization Name</p>
-                        <h6 class="fw-bold">{{ $organization->name }}</h6>
-                    </div>
-                    <div class="col-3">
-                        <p>Email Address</p>
-                        <h6 class="fw-bold">
-                            @if(isset($organization->contact_email))
-                                <a href="mailto:{{ $organization->contact_email }}">{{ $organization->contact_email }}</a>
-                            @else
-                                Email Address Not Set
-                            @endif
-                        </h6>
-                    </div>
-                    <div class="col-3">
-                        <p>Phone Number</p>
-                        <h6 class="fw-bold">
-                            <a href="tel:{{ $organization->contact_number }}">{{ $organization->contact_number }}</a>
-                        </h6>
-                    </div>
-                    <div class="col-6">
-                        <p>Internal Notes</p>
-                        <h6 class="fw-bold">{{ $organization->internal_notes }}</h6>
-                    </div>
-                    <div class="col-6">
-                        <p>External Notes</p>
-                        <h6 class="fw-bold">{{ $organization->external_notes }}</h6>
-                    </div>
-                    <div class="col-12">
-                        <a href="{{ route('organizations.edit', ['organization' => $organization,]) }}" class="btn btn-success">
-                            {{ Icon::edit() }}
-                            Edit Organization
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <livewire:admin.organization.details :organization="$organization">
     <hr class="splitter"/>
     <div class="row">
         <div class="col-xl-12">
