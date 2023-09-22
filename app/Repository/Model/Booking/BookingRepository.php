2ad774eabe6f07a8df571041556d6fa537794549
Celeste Gateley <celeste.gateley@gmail.com>
2023-09-22 16:02:19 +0100

    Fix for sending the wrong total cost with booking

--- app/Repository/Model/Booking/BookingRepository.php
+++ app/Repository/Model/Booking/BookingRepository.php
@@ -481,7 +481,7 @@ class BookingRepository extends ModelRepository implements GeneratesFellohData
             'booking_reference' => $this->getReference(),
             'departure_date' => $this->booking->tour->date_from->format('Y-m-d'),
             'return_date' => $this->booking->tour->date_to->format('Y-m-d'),
-            'gross_amount' => $this->booking->total_cost,
+            'gross_amount' => $this->getTotalCost(),
         ];
     }
 
