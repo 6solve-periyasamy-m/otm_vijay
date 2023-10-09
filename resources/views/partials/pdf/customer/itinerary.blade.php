51261d92c3b1e566a256133a480fba412749b8f4
Celeste Gateley <celeste.gateley@gmail.com>
2023-10-09 15:16:28 +0100

    Changed styling to better match the original

--- resources/views/partials/pdf/customer/itinerary.blade.php
+++ resources/views/partials/pdf/customer/itinerary.blade.php
@@ -6,43 +6,45 @@
 @endphp
 <div class="card">
     <div class="component-header">
-        <div class="width-100 my-auto d-flex">
+        <div class="width-100 my-auto px-2 py-3 item-name">
             {!! $component->name !!}
         </div>
     </div>
     <div class="component-text">
         <div class="mx-2 row">
             @include('partials.pdf.customer.attributes', ['attributes' => $component->attributes])
-            @if($component->image !== null)
-                <div class="width-1-4 border-right">
-                    <img src="{{ asset($component->image) }}" class="booking-image"/>
-                </div>
-                <div class="width-3-4">
-                    <div>
-                        <span class="fw-bold">Description<br /></span>
-                        {{ $component->description }}
+            <div class="py-2">
+                @if($component->image !== null)
+                    <div class="width-1-9 inline-block border-right">
+                        <img src="{{ asset($component->image) }}" class="booking-image"/>
                     </div>
-                    @if($component->notes)
-                    <div class="border-top mt-1">
-                        <span class="fw-bold">Notes<br /></span>
-                        {{ $component->notes }}
-                    </div>
-                    @endif
-                </div>
-            @else
-                <div>
-                    <div>
-                        <span class="fw-bold">Description<br /></span>
-                        {{ $component->description }}
-                    </div>
-                    @if($component->notes)
+                    <div class="inline-block width-8-9 ml-1">
+                        <div>
+                            <span class="fw-bold">Description<br /></span>
+                            {{ $component->description }}
+                        </div>
+                        @if($component->notes)
                         <div class="border-top mt-1">
                             <span class="fw-bold">Notes<br /></span>
                             {{ $component->notes }}
                         </div>
-                    @endif
-                </div>
-            @endif
+                        @endif
+                    </div>
+                @else
+                    <div class="inline-block w-100 ml-1">
+                        <div>
+                            <span class="fw-bold">Description<br /></span>
+                            {{ $component->description }}
+                        </div>
+                        @if($component->notes)
+                            <div class="border-top mt-1">
+                                <span class="fw-bold">Notes<br /></span>
+                                {{ $component->notes }}
+                            </div>
+                        @endif
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 </div>
--- resources/views/pdf/itinerary.blade.php
+++ resources/views/pdf/itinerary.blade.php
@@ -13,11 +13,11 @@
 </head>
 <body>
 <div class="card">
-    <h1 class="mb-0" style="width: 100%; text-align: center;">{{$orderCustomer->order->tour->name}} - {{$orderCustomer->order->booking_reference}} - {{$orderCustomer->customer_name}}</h1>
+    <h1 class="label-card">{{$orderCustomer->order->tour->name}} - {{$orderCustomer->order->booking_reference}} - {{$orderCustomer->customer_name}}</h1>
 </div>
 @foreach($orderCustomer->repository->getComponentsForItinerary() as $day => $components)
     <div class="card">
-        <h2 class="mb-0" style="width: 100%; text-align: center;">{{ \Carbon\Carbon::createFromTimestamp($day)->format('l jS F Y') }}</h2>
+        <h2 class="label-card">{{ \Carbon\Carbon::createFromTimestamp($day)->format('l jS F Y') }}</h2>
     </div>
     @foreach($components as $component)
         @include('partials.pdf.customer.itinerary', ['orderComponent' => $component,])
