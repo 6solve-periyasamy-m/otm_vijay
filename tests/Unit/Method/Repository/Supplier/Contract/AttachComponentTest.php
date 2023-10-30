4afdb03baa0b9ca51dbabc85cac194bd039c5eb0
Celeste Gateley <celeste.gateley@gmail.com>
2023-10-30 11:10:48 +0000

    Adjusted tests for attach component (failing)

--- tests/Unit/Method/Repository/Supplier/Contract/AttachComponentTest.php
+++ tests/Unit/Method/Repository/Supplier/Contract/AttachComponentTest.php
@@ -2,10 +2,8 @@
 
 namespace Tests\Unit\Method\Repository\Supplier\Contract;
 
-use App\Models\Accommodation\AccommodationInventory;
-use App\Models\Activity\ActivityInventory;
-use App\Models\Flight\FlightInventory;
-use App\Models\Transport\TransportInventory;
+use App\Models\Supplier\SupplierContract;
+use Illuminate\Database\Eloquent\Model;
 use Tests\Bases\DatabaseTestCase;
 use Tests\Traits\Model\TestsSupplier;
 use Tests\Traits\Model\TestsTour;
@@ -16,41 +14,48 @@ class AttachComponentTest extends DatabaseTestCase
 
     public function testAttachAccommodationInventory()
     {
-        $inventory = $this->generateAccommodationInventory();
-        $contract = $this->generateContract();
-        $contract->repository->linkToComponent($inventory->repository);
-        $component = $contract->components->first();
-        $this->assertEquals(AccommodationInventory::class, $component->component_type);
-        $this->assertEquals($inventory->id, $component->component_id);
+        $this->runAttachTest($this->generateAccommodationInventory());
     }
 
     public function testAttachActivityInventory()
     {
-        $inventory = $this->generateActivityInventory();
-        $contract = $this->generateContract();
-        $contract->repository->linkToComponent($inventory->repository);
-        $component = $contract->components->first();
-        $this->assertEquals(ActivityInventory::class, $component->component_type);
-        $this->assertEquals($inventory->id, $component->component_id);
+        $this->runAttachTest($this->generateActivityInventory());
     }
 
     public function testAttachFlightInventory()
     {
-        $inventory = $this->generateFlightInventory();
-        $contract = $this->generateContract();
-        $contract->repository->linkToComponent($inventory->repository);
-        $component = $contract->components->first();
-        $this->assertEquals(FlightInventory::class, $component->component_type);
-        $this->assertEquals($inventory->id, $component->component_id);
+        $this->runAttachTest($this->generateFlightInventory());
     }
 
     public function testAttachTransportInventory()
     {
-        $inventory = $this->generateTransportInventory();
+        $this->runAttachTest($this->generateTransportInventory());
+    }
+
+    public function testAttachMultiple()
+    {
         $contract = $this->generateContract();
-        $contract->repository->linkToComponent($inventory->repository);
-        $component = $contract->components->first();
-        $this->assertEquals(TransportInventory::class, $component->component_type);
-        $this->assertEquals($inventory->id, $component->component_id);
+        $this->runAttachTest($this->generateAccommodationInventory(), $contract);
+        $this->runAttachTest($this->generateActivityInventory(), $contract);
+        $this->runAttachTest($this->generateFlightInventory(), $contract);
+        $this->runAttachTest($this->generateTransportInventory(), $contract);
+    }
+
+    private function runAttachTest(Model $inventory, SupplierContract|null $contract = null)
+    {
+        $contract = $contract ?? $this->generateContract();
+        \Log::info($inventory);
+        $attached = $contract->repository->linkToComponent($inventory->repository);
+        $this->assertTrue($attached, 'Attachment Failed');
+        $class = $inventory::class;
+        \Log::info($contract->components);
+        foreach ($contract->components as $component) {
+            \Log::info($component->toJson());
+            if ($component->component_id === $inventory->id && $component->component_type === $class) {
+                $found = true;
+                break;
+            }
+        }
+        $this->assertTrue($found ?? false, "Component of type {$class} {$inventory->id} not linked successfully");
     }
 }
