3fc6a92f8500df6b46e9be5fa1fbc1b2be5605c4
Celeste Gateley <celeste.gateley@gmail.com>
2023-11-23 14:13:19 +0000

    Changed accommodation standalone table to extend base standalone table

--- app/Http/Livewire/Admin/Accommodation/Inventory/StandaloneTable.php
+++ app/Http/Livewire/Admin/Accommodation/Inventory/StandaloneTable.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Livewire\Admin\Accommodation\Inventory;
 
+use App\Http\Livewire\Abstract\StandaloneDatatable;
 use App\Models\Accommodation\AccommodationInventory;
 use Carbon\Carbon;
 use Mediconesystems\LivewireDatatables\Column;
 use Mediconesystems\LivewireDatatables\DateColumn;
-use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
 use Mediconesystems\LivewireDatatables\NumberColumn;
 
-class StandaloneTable extends LivewireDatatable
+class StandaloneTable extends StandaloneDatatable
 {
     public Carbon|string|null $from = null;
     public Carbon|string|null $to = null;
@@ -20,13 +20,7 @@ class StandaloneTable extends LivewireDatatable
             ->join('accommodations', 'accommodations.id', '=', 'accommodation_inventories.accommodation_id')
             ->join('room_types', 'room_types.id', '=', 'accommodation_inventories.room_type_id')
             ->join('board_types', 'board_types.id', '=', 'accommodation_inventories.board_type_id');
-        if (!empty($this->from)) {
-            $query->where('check_in', '>', is_string($this->from) ? Carbon::parse($this->from) : $this->from);
-        }
-        if (!empty($this->to)) {
-            $query->where('check_in', '<', is_string($this->to) ? Carbon::parse($this->to) : $this->to);
-        }
-        return $query;
+        return $this->filter($query, $this->from, $this->to, 'check_in', 'check_out');
     }
 
     public function columns()
@@ -63,5 +57,10 @@ class StandaloneTable extends LivewireDatatable
                 ->sortable()
                 ->filterable(),
         ];
+    }
+
+    public function getClass(): string
+    {
+        return AccommodationInventory::class;
     }
 }
