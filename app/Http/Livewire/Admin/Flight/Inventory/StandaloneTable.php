88d32d2d6c8b302fd1e1bb7ad2531c56be9f7bea
Celeste Gateley <celeste.gateley@gmail.com>
2023-11-23 12:26:34 +0000

    Fix for flight details on flight standalone table

--- app/Http/Livewire/Admin/Flight/Inventory/StandaloneTable.php
+++ app/Http/Livewire/Admin/Flight/Inventory/StandaloneTable.php
@@ -38,7 +38,7 @@ class StandaloneTable extends LivewireDatatable
         return [
             Column::checkbox()
                 ->width('10rem'),
-            Column::name('name')
+            Column::callback(['departure.name', 'arrival.name'], function ($departure, $arrival) { return "$departure to $arrival"; })
                 ->label('Flight Details')
                 ->sortable()
                 ->searchable(),
