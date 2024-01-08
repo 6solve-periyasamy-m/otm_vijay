0929cc086d0084b437a8e9d75faf3a53a7e06161
Celeste Gateley <celeste.gateley@gmail.com>
2024-01-08 17:13:46 +0000

    Reworked transport views to use new structure

---
+++ resources/lang/en/transport.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'form' => [
+        'title' => [
+            'create' => 'Create Transport',
+            'update' => 'Update Transport'
+        ],
+        'fields' => [
+
+        ],
+    ],
+    'inventory' => [
+        'form' => [
+            'title' => [
+                'create' => 'Create Transport Inventory',
+                'update' => 'Update Transport Inventory'
+            ],
+            'fields' => [
+
+            ],
+        ],
+        'tour' => [
+            'form' => [
+                'title' => [
+                    'create' => 'Create Transport Inventory Tour',
+                    'update' => 'Update Transport Inventory Tour'
+                ],
+                'fields' => [
+
+                ],
+            ],
+        ],
+    ],
+];
