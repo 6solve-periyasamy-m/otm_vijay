e14898e8468abf143e8dd8df152af43e49df6ac8
Celeste Gateley <celeste.gateley@gmail.com>
2024-01-08 16:08:57 +0000

    Reworked activity inventory tour views to use new view structure

--- resources/views/pages/models/activity_inventory_tours/update.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.form', ['action' => route('activity-inventory-tours.update', ['tour' => $tour, 'activityInventoryTour' => $activityInventoryTour, ]),])
-
-@section('title', 'Update Activity Inventory Tour')
-
-@section('form-body')
-    @include('partials.models.activity_inventory_tours.form', ['activityInventoryTour' => $activityInventoryTour, ])
-@endsection
