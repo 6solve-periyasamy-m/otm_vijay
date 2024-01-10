e771db8b2372d817b9094b11cb803e59726f121e
Celeste Gateley <celeste.gateley@gmail.com>
2024-01-10 18:16:14 +0000

    Migrated order adjustment views to use new structure

--- resources/views/pages/models/manual_adjustments/create.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.form', ['action' => route('manual-adjustments.store', ['order' => $order, ]),])
-
-@section('title', 'Create Manual Adjustment')
-
-@section('form-body')
-    @include('partials.models.manual_adjustments.form')
-@endsection
