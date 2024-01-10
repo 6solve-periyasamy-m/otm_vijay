cef77ad0afa562149a79171bc9a0758a04e638a9
Celeste Gateley <celeste.gateley@gmail.com>
2024-01-10 21:15:59 +0000

    Fix for missing lang entries

--- resources/lang/en/order.php
+++ resources/lang/en/order.php
@@ -9,6 +9,30 @@ return [
                     'update' => 'Update Order Customer',
                 ],
             ],
+            'adjustment' => [
+                'form' => [
+                    'title' => [
+                        'create' => 'Create Order Customer Adjustment',
+                        'update' => 'Update Order Customer Adjustment',
+                    ],
+                ],
+            ],
+        ],
+        'payment' => [
+            'form' => [
+                'title' => [
+                    'create' => 'Create Payment',
+                    'update' => 'Update Payment',
+                ],
+            ],
+        ],
+        'adjustment' => [
+            'form' => [
+                'title' => [
+                    'create' => 'Create Manual Adjustment',
+                    'update' => 'Update Manual Adjustment',
+                ],
+            ],
         ],
     ],
 ];
