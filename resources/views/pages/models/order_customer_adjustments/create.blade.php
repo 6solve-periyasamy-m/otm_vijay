157f196b2edfd3d4760b9c36f146af8b4583d7b7
Celeste Gateley <celeste.gateley@gmail.com>
2024-01-10 21:05:16 +0000

    Migrated order customer adjustment views to use new structure

--- resources/views/pages/models/order_customer_adjustments/create.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.form', ['action' => route('order-customer-adjustments.store', ['order' => $order, 'orderCustomer' => $orderCustomer, ]),])
-
-@section('title', 'Create Order Customer Adjustment')
-
-@section('form-body')
-    @include('partials.models.order_customer_adjustments.form')
-@endsection
