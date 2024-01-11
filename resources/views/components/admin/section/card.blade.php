9f865bd98eeb0685e4f1e1e3be95e3e0b2033931
Celeste Gateley <celeste.gateley@gmail.com>
2024-01-11 18:55:28 +0000

    Fix for recursive card definition

--- resources/views/components/admin/section/card.blade.php
+++ resources/views/components/admin/section/card.blade.php
@@ -1,8 +1,10 @@
-<x-admin.section.card>
-    @if(isset($header))
-        <div class="card-title">
-            <h4 class="fw-bold">{{ $header }}</h4>
-        </div>
-    @endif
-    {{ $slot }}
-</x-admin.section.card>
+<div class="card">
+    <div class="card-body">
+        @if(isset($header))
+            <div class="card-title">
+                <h4 class="fw-bold">{{ $header }}</h4>
+            </div>
+        @endif
+        {{ $slot }}
+    </div>
+</div>
