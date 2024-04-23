9f2213d93ada7141d0d340828e7f89dda00718fe
parthpateloc <parthpatel@itoneclick.com>
2024-04-23 12:44:38 +0530

    add itinary route for invoice

--- routes/web/admin/order.php
+++ routes/web/admin/order.php
@@ -23,7 +23,7 @@ Route::prefix('{order}')->group(function () {
     Route::post('/delete/force', [OrderController::class, 'forceDelete'])->name('orders.delete.force')->middleware(['bouncer:Order\Order,delete', 'auth.otm']);
     Route::post('/restore/', [OrderController::class, 'restore'])->name('orders.restore')->middleware('bouncer:Order\Order,delete');
     Route::get('/invoice', [OrderController::class, 'latestInvoice'])->name('orders.invoice.latest')->middleware('bouncer:Order\Order,read');
-    Route::get('/invoice/itineary', [OrderController::class, 'itinearyInvoice'])->name('orders.invoice.latest')->middleware('bouncer:Order\Order,read');
+    Route::get('/invoice/itineary', [OrderController::class, 'itinearyInvoice'])->name('orders.itineary.latest')->middleware('bouncer:Order\Order,read');
     Route::get('/invoice/{version}', [OrderController::class, 'invoice'])->name('orders.invoice.view')->middleware('bouncer:Order\Order,read');
     Route::get('/atol', [OrderController::class, 'atol'])->name('orders.atol')->middleware('bouncer:Order\Order,read');
     Route::get('/occupancy', [OrderController::class, 'occupancy'])->name('orders.occupancy')->middleware('bouncer:Order\Order,update');
