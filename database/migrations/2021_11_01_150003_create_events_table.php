66cdbf4a721ee2cfd4a9ccd751fd0b72ae785a1f
Dhananjay <dhananjay@itoneclick.com>
2024-04-24 17:48:21 +0530

    Added event image

--- database/migrations/2021_11_01_150003_create_events_table.php
+++ database/migrations/2021_11_01_150003_create_events_table.php
@@ -20,6 +20,7 @@ class CreateEventsTable extends Migration
             $table->string('description')->nullable();
             $table->date('starts_at');
             $table->date('ends_at');
+            $table->string('image_url')->nullable();
             $table->string('booking_url')->nullable();
             $table->string('notes')->nullable();
             $table->timestamps();
