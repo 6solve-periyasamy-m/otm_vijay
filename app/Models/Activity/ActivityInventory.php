3c60a9146a56c90e172badff6dac097a727e8d94
Dhananjay <dhananjay@itoneclick.com>
2024-04-25 17:56:26 +0530

    Start date and end date added nullable for Create Activity Inventory

--- app/Models/Activity/ActivityInventory.php
+++ app/Models/Activity/ActivityInventory.php
@@ -93,6 +93,8 @@ class ActivityInventory extends Model
             'ticket_type_id' => 'required|exists:ticket_types,id',
             /*'starts_at' => 'date',
             'ends_at' => 'date',*/
+            'starts_at' => 'nullable|date',
+            'ends_at' => 'nullable|date',
             'stock' => 'required|numeric|integer',
             'purchase_price' => 'nullable|numeric',
             'sales_price' => 'nullable|numeric',
