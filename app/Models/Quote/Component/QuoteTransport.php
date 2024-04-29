75f84561e8ebf70a8898fc85b5dd72c212eed8d6
Celeste Gateley <celeste.gateley@gmail.com>
2024-04-29 11:14:53 +0100

    Added quantity field to quote components

--- app/Models/Quote/Component/QuoteTransport.php
+++ app/Models/Quote/Component/QuoteTransport.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Carbon;
  * @property string $tour_component_type
  * @property float $tour_sales_price
  * @property bool $price_shown
+ * @property int|null $quantity
  * @property Carbon|null $deleted_at
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
