81974eec443d7c0c286f8abc0927ca76bbb3be34
ChintanDesigner <chintan@itoneclick.com>
2024-05-01 14:55:12 +0530

    pin fix new design

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,5 +1,12 @@
 <div class="otm-sidebar d-block p-0 pt-3 col-12 col-md-3 col-xl-2 p-0">
     <div class="d-flex flex-column flex-shrink-0 h-100">
+        <div class="nav-item mb-2">
+            <a class="nav-link sidebar-toggle" href="javascript:toggleSidebar()">
+                <img src="images/logokpt.png" alt="Company Name" class="iconLogo hide" height="40px" width="100px" />
+                <!-- <x-icon icon="fa-regular fa-circle" class="sidebar-arrow" /> -->
+                <x-icon icon="fa-solid fa-bars" class="sidebar-arrow" />
+            </a>
+        </div>
         <div class="nav flex-column mb-auto overflow-y-auto flex-nowrap">
             @foreach(\App\View\Components\Admin\SidebarLink::getSidebarLinks() as $sidebarLink)
                 {{ $sidebarLink->render() }}
@@ -7,12 +14,7 @@
             @if(is_otm())
                 {{ \App\View\Components\Admin\SidebarLink::getLogsURL()->render() }}
             @endif
-        </div>
-        <div class="nav-item">
-            <a class="nav-link sidebar-toggle" href="javascript:toggleSidebar()">
-                <x-icon icon="arrow-right" class="sidebar-arrow" />
-            </a>
-        </div>
+        </div>      
     </div>
 </div>
 @push('footer-stack')
@@ -20,14 +22,17 @@
         function toggleSidebar() {
             let sidebar = $(".otm-sidebar");
             let arrow = $(".sidebar-arrow");
+            let icon = $(".iconLogo");
             if (sidebar.hasClass('shown')) {
                 sidebar.removeClass('shown');
-                arrow.removeClass('fa-arrow-left');
-                arrow.addClass('fa-arrow-right');
+                arrow.removeClass('fa-solid fa-xmark');
+                arrow.addClass('fa-solid fa-bars');
+                icon.removeClass('showLogo');
             } else {
                 sidebar.addClass('shown');
-                arrow.addClass('fa-arrow-left');
-                arrow.removeClass('fa-arrow-right');
+                arrow.addClass('fa-solid fa-xmark');
+                arrow.removeClass('fa-solid fa-bars');
+                icon.addClass('showLogo');
             }
         }
     </script>
