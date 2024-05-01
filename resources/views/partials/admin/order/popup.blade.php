f9111745876c3a43f4f7e91641a5b71426df34e2
parthpateloc <parthpatel@itoneclick.com>
2024-05-01 17:56:57 +0530

    cancel order confirmation  work completed

--- resources/views/partials/admin/order/popup.blade.php
+++ resources/views/partials/admin/order/popup.blade.php
@@ -47,7 +47,7 @@
                             <x-slot:icon>{{ Icon::delete() }}</x-slot:icon>
                             Cancel Order
                         </x-admin.popup-button>
-                        <form class="d-none" action="{{ route('orders.delete', ['order' => $order,]) }}" method="post" id="order-delete">
+                        <form class="d-none" onsubmit="return confirm('Are you sure you wish to Cancel this order?')" action="{{ route('orders.delete', ['order' => $order,]) }}" method="post" id="order-delete">
                             @csrf
                         </form>
                     @endif
