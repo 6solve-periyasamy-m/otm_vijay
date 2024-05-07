e2485dce2126e4d918a98cf51798f31324eb4ca3
ChintanDesigner <chintan@itoneclick.com>
2024-05-07 15:55:37 +0530

    menu change and logo layout

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,8 +1,8 @@
-<div class="otm-sidebar d-block p-0 pt-3 col-12 col-md-3 col-xl-2 p-0">
+<div class="otm-sidebar d-block p-0 pt-3 col-12 col-md-3 col-xl-2">
     <div class="d-flex flex-column flex-shrink-0 h-100">
         <div class="nav-item mb-2">
             <a class="nav-link sidebar-toggle" href="javascript:toggleSidebar()">
-                <img src="images/logokpt.png" alt="Company Name" class="iconLogo hide" height="40px" width="100px" />
+                <span class="sidebar-hide" style="color: #ffffff;">Menu</span>
                 <!-- <x-icon icon="fa-regular fa-circle" class="sidebar-arrow" /> -->
                 <x-icon icon="fa-solid fa-bars" class="sidebar-arrow" />
             </a>
@@ -18,22 +18,4 @@
     </div>
 </div>
 @push('footer-stack')
-    <script type="text/javascript">
-        function toggleSidebar() {
-            let sidebar = $(".otm-sidebar");
-            let arrow = $(".sidebar-arrow");
-            let icon = $(".iconLogo");
-            if (sidebar.hasClass('shown')) {
-                sidebar.removeClass('shown');
-                arrow.removeClass('fa-solid fa-xmark');
-                arrow.addClass('fa-solid fa-bars');
-                icon.removeClass('showLogo');
-            } else {
-                sidebar.addClass('shown');
-                arrow.addClass('fa-solid fa-xmark');
-                arrow.removeClass('fa-solid fa-bars');
-                icon.addClass('showLogo');
-            }
-        }
-    </script>
 @endpush
