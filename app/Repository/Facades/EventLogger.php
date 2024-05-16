5dbba760c2899eaebcebf02ffa92efc1d934bb07
Celeste Gateley <celeste.gateley@gmail.com>
2024-05-16 14:29:08 +0100

    Fix for populate not functioning correctly

--- app/Repository/Facades/EventLogger.php
+++ app/Repository/Facades/EventLogger.php
@@ -13,38 +13,45 @@ class EventLogger
 {
     public function simple(Model $model, ModelEventType $type): void
     {
-        $event = $this->populate(ModelEvent::make([
+        $event = ModelEvent::make([
             'target_id' => $model->id,
             'target_type' => $model::class,
             'action' => $type,
-        ]));
+            ...$this->populate(),
+        ]);
         $event->save();
     }
 
     public function event(Model|null $from, Model|null $to, ModelEventType $type): void
     {
-        $event = $this->populate(ModelEvent::make([
+        $event = ModelEvent::make([
             'target_id' => $to->id,
             'target_type' => $to::class,
             'action' => $type,
             'from' => $from->toJson(),
             'to' => $to->toJson(),
-        ]));
+            ...$this->populate(),
+        ]);
         $event->save();
     }
 
-    private function populate(ModelEvent $event): ModelEvent
+    private function populate(): array
     {
         $actor = Auth::user();
-        try { $location = Location::get(); } catch (\Exception $exception) { Log::error($exception); }
-        $event->update([
+
+        return [
             'actor_id' => $actor->id,
             'actor_type' => $actor::class,
             'occurred' => now(),
             'ip' => request()->ip(),
             'agent' => request()->userAgent(),
-            'location' => $location ?? null,
-        ]);
-        return $event;
+            'location' => $this->getLocationString(),
+        ];
+    }
+
+    private function getLocationString(): string|null
+    {
+        try { $location = Location::get(); } catch (\Exception $exception) { Log::error($exception); return null; }
+        return "{$location->cityName}, {$location->countryName}, {$location->areaCode}";
     }
 }
