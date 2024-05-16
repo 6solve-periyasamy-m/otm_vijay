92d1e26e92fca90a59ebfab5c83d84e28291dcb8
Celeste Gateley <celeste.gateley@gmail.com>
2024-05-16 13:15:14 +0100

    Updated ModelEventType to use Human Readable strings

--- app/Models/Helper/ModelEventType.php
+++ app/Models/Helper/ModelEventType.php
@@ -4,17 +4,18 @@ namespace App\Models\Helper;
 
 enum ModelEventType: string
 {
-    case CREATED = 'created';
-    case UPDATED = 'updated';
-    case DELETED = 'deleted';
-    case RESTORED = 'restored';
-    case LOGIN = 'login';
-    case LOGOUT = 'logout';
-    case REGISTER = 'register';
-    case PASSWORD_REMINDER = 'password-reminder';
-    case PASSWORD_CHANGED = 'password-changed';
-    case OTP_ENABLED = 'otp-enabled';
-    case OTP_DISABLED = 'otp-disabled';
-    case EMAIL_CHANGED = 'email-changed';
-
+    case CREATED = 'Created';
+    case UPDATED = 'Updated';
+    case DELETED = 'Deleted';
+    case RESTORED = 'Restored';
+    case LOGIN_SUCCESS = 'Login Successful';
+    case LOGIN_FAILED_PASSWORD = 'Login Failed: Incorrect Password';
+    case LOGIN_FAILED_OTP_MISSING = 'Login Failed: OTP Missing';
+    case LOGIN_FAILED_OTP_FAILED = 'Login Failed: Incorrect OTP';
+    case LOGOUT = 'Logout';
+    case REGISTER = 'Register';
+    case PASSWORD_RESET = 'Password Reset Request';
+    case PASSWORD_CHANGED = 'Password Changed';
+    case OTP_ENABLED = 'OTP Enabled';
+    case OTP_DISABLED = 'OTP Disabled';
 }
