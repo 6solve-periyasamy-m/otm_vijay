acf0c208f424b34451b930c15d28af078511a00b
Dhananjay <dhananjay@itoneclick.com>
2024-05-20 18:41:07 +0530

    Added event category

--- app/Http/Controllers/Admin/Tour/EventController.php
+++ app/Http/Controllers/Admin/Tour/EventController.php
@@ -30,6 +30,7 @@ class EventController extends Controller
             'booking_url' => $request->input('booking_url'),
             'tax_bracket_id' => $request->input('tax_bracket_id'),
             'notes' => $request->input('notes'),
+            'event_category' => $request->input('event_category'),
         ]);
 
         if ($request->has('image') && $request->file('image') != null) {
@@ -60,6 +61,7 @@ class EventController extends Controller
             'booking_url' => $request->input('booking_url'),
             'tax_bracket_id' => $request->input('tax_bracket_id'),
             'notes' => $request->input('notes'),
+            'event_category' => $request->input('event_category'),
         ]);
 
         if ($request->has('image') && $request->file('image') != null) {
