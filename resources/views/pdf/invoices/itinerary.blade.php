0f0a2b39ce2f92fbab00385c73f5562c37161595
Celeste Gateley <celeste.gateley@gmail.com>
2024-05-28 11:20:40 +0100

    Fix for accidentally removed code

--- resources/views/pdf/invoices/itinerary.blade.php
+++ resources/views/pdf/invoices/itinerary.blade.php
@@ -13,7 +13,7 @@
 @endphp
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 
 <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
