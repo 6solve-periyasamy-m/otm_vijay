3163328dd3b2432b55c7513cd5feb7e8199f04e0
Celeste Gateley <celeste.gateley@gmail.com>
2024-05-28 10:04:42 +0100

    Turned off autocomplete for all admin fields

--- resources/views/partials/fields/raw/text.blade.php
+++ resources/views/partials/fields/raw/text.blade.php
@@ -1,3 +1,3 @@
 @isset($name)<label for="{{ $field }}-input" class="{{ $labelClasses ?? "" }}">{{ $name }}</label>@endisset
 <input name="{{ $field }}" value="{{ old($field) ?? $value ?? "" }}" class="form-control {{ $classes ?? '' }}" id="{{ $field }}-input"
-       @if(isset($onChange)) onchange="{{ $onChange }}" @endif autocomplete="false">
+       @if(isset($onChange)) onchange="{{ $onChange }}" @endif autocomplete="off">
