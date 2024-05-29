04e00fb0c767d66a952f01d505ff09ce1877d314
Celeste Gateley <celeste.gateley@gmail.com>
2024-05-29 10:56:20 +0100

    Fix for cost calculations

--- app/Http/Livewire/Admin/Quote/Calculator.php
+++ app/Http/Livewire/Admin/Quote/Calculator.php
@@ -32,8 +32,9 @@ class Calculator extends Component
 
     public function calculate(): void
     {
-        $this->costToCompany = $this->quote->repository->getTotalCostToCompany($this->paying + $this->travelling + 1);
-        $costPerPerson = sigfig($this->costToCompany / ($this->paying + $this->travelling + 1));
+        $companyCostTravellers = ($this->paying + $this->travelling + ($this->leadTravelling()));
+        $this->costToCompany = $this->quote->repository->getTotalCostToCompany($companyCostTravellers);
+        $costPerPerson = $companyCostTravellers > 0 ? sigfig($this->costToCompany / $companyCostTravellers) : 0;
         $this->total = $this->quote->repository->getTotalCost($this->paying + ($this->quote->leadTraveller->paying ? 1 : 0));
         $this->profit = sigfig($this->total - $this->costToCompany);
         $this->margin = $this->costToCompany == 0 ? 100 : sigfig(($this->total / $this->costToCompany) * 100);
