de7b40f0bc69c522c36fc96bf2150c25c044f420
Celeste Gateley <celeste.gateley@gmail.com>
2024-06-02 22:05:45 +0100

    Implemented pretty checkboxes

--- resources/views/components/livewire/input/checkbox.blade.php
+++ resources/views/components/livewire/input/checkbox.blade.php
@@ -1,4 +1,8 @@
-<div class="form-group col-12 col-xl-{{ $attributes->get('width', 12) }} my-auto" style="padding-left: 5px;">
-    <input {{ $attributes->class(['form-check-input'])->merge(['type' => 'checkbox'])->except(['width', 'label']) }}>
-    <label class="form-check-label">{{ $attributes->get('label', "") ?? $slot }}</label>
+<div class="col-12 col-xl-{{ $attributes->get('width', 12) }} my-auto" style="padding-left: 5px;">
+    <div class="pretty p-switch p-fill">
+        <input {{ $attributes->merge(['type' => 'checkbox'])->except(['width', 'label']) }}>
+        <div class="state p-{{$attributes->get('color', 'primary')}}">
+            <label>{{ $attributes->get('label', "") ?? $slot }}</label>
+        </div>
+    </div>
 </div>
