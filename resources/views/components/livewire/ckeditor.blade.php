382a9d076f7afd12141029b45e9d8fc7a97c396d
Celeste Gateley <celeste.gateley@gmail.com>
2024-06-02 15:43:45 +0100

    Implemented livewire ckeditor

---
+++ resources/views/components/livewire/ckeditor.blade.php
@@ -0,0 +1,17 @@
+@php $id = $attributes->get('id', 'a' . \Str::uuid()); @endphp
+<div class="{{ $attributes->has('width') ? 'col-xl-' . $attributes->get('width', 12) : '' }}" wire:ignore>
+    @if($attributes->get('label') !== null)
+        <label>{{ $attributes->get('label', "") ?? $slot }} @if($attributes->has('required')) <x-admin.required /> @endif</label>
+    @endif
+    <textarea id="{{$id}}" {{ $attributes->except(['id', 'width', 'value', 'label']) }}>{{ $attributes->get('value', "") }}</textarea>
+    <script type="text/javascript">
+        ClassicEditor
+            .create(document.querySelector('#{{$id}}'))
+            .then(editor => {
+                editor.model.document.on('change:data', () => {
+                    @this.set('{{$attributes->get('name')}}', editor.getData());
+                });
+            })
+            .catch(error => console.error(error));
+    </script>
+</div>
