be9c450693050e7397a3a82011039111e6e6bdd5
Celeste Gateley <celeste.gateley@gmail.com>
2024-06-06 13:30:19 +0100

    Added import for accommodation inventory category

--- app/Imports/AccommodationInventoryImport.php
+++ app/Imports/AccommodationInventoryImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\Accommodation\Accommodation;
 use App\Models\Accommodation\AccommodationInventory;
 use App\Models\Accommodation\BoardType;
+use App\Models\Accommodation\RoomCategory;
 use App\Models\Accommodation\RoomType;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
@@ -20,20 +21,22 @@ class AccommodationInventoryImport implements ToCollection, WithHeadingRow, With
 
     /**
      * @param Collection $collection
-     * @return array
+     * @return array|null
      */
-    public function collection(Collection $collection)
+    public function collection(Collection $collection): ?array
     {
         $data = [];
         foreach ($collection as $row) {
             $accommodation = Accommodation::where('name', 'like', trim($row['accommodation']))->first();
             $parent = AccommodationInventory::find($row['parent_id']);
+            $category = empty($row['category']) ? null : RoomCategory::findOrCreate($row['category']);
             if ($accommodation == null) return null;
             $data[] = AccommodationInventory::create([
                 'accommodation_id' => $accommodation->id,
                 'stock_parent_id' => $parent?->id,
                 'room_type_id' => RoomType::findOrCreate(trim($row['room_type']), trim($row['size']))->id,
                 'board_type_id' => BoardType::findOrCreate(trim($row['board_type']))->id,
+                'room_category_id' => $category?->id,
                 'check_in' => Carbon::createFromFormat('d-m-Y H:i', trim($row['check_in'])),
                 'check_in_time_confirmed' => true,
                 'check_out' => Carbon::createFromFormat('d-m-Y H:i', trim($row['check_out'])),
@@ -53,6 +56,7 @@ class AccommodationInventoryImport implements ToCollection, WithHeadingRow, With
         return [
             'accommodation' => 'required|exists:accommodations,name',
             'room_type' => 'required',
+            'parent_id' => 'nullable|exists:accommodation_inventories,id',
             'size' => 'required|integer|gte:1',
             'check_in' => 'required|date|date_format:"d-m-Y H:i"',
             'check_out' => 'required|date|date_format:"d-m-Y H:i"|after:check_in',
