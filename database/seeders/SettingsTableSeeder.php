57e52374b3f27b6a5b8c624e547c581ed552b425
Celeste Gateley <celeste.gateley@gmail.com>
2024-06-06 16:10:28 +0100

    Updated default seeders

--- database/seeders/SettingsTableSeeder.php
+++ database/seeders/SettingsTableSeeder.php
@@ -21,100 +21,140 @@ class SettingsTableSeeder extends Seeder
         \DB::table('settings')->insert(array (
             0 => 
             array (
+                'key' => 'accommodation.lock',
+                'value' => '0',
+            ),
+            1 => 
+            array (
+                'key' => 'accommodation.unlock',
+                'value' => '0',
+            ),
+            2 => 
+            array (
+                'key' => 'activity.lock',
+                'value' => '0',
+            ),
+            3 => 
+            array (
+                'key' => 'activity.unlock',
+                'value' => '0',
+            ),
+            4 => 
+            array (
                 'key' => 'atol.enabled',
                 'value' => '1',
             ),
-            1 => 
+            5 => 
+            array (
+                'key' => 'atol.filter',
+                'value' => '244',
+            ),
+            6 => 
             array (
                 'key' => 'atol.issuer',
                 'value' => 'Octopus Travel Matrix',
             ),
-            2 => 
+            7 => 
             array (
                 'key' => 'atol.number',
                 'value' => '12345',
             ),
-            3 => 
+            8 => 
             array (
                 'key' => 'atol.stamp',
                 'value' => 'images/sample-atol.jpg',
             ),
-            4 => 
+            9 => 
+            array (
+                'key' => 'atol.year.start',
+                'value' => '2022-04-01',
+            ),
+            10 => 
             array (
                 'key' => 'authorization.reminders',
                 'value' => '1660819442',
             ),
-            5 => 
+            11 => 
             array (
                 'key' => 'booking.prefix',
                 'value' => 'OTM',
             ),
-            6 => 
+            12 => 
             array (
                 'key' => 'company.address.city',
                 'value' => 'Walton',
             ),
-            7 => 
+            13 => 
             array (
                 'key' => 'company.address.country',
                 'value' => 'United Kingdom',
             ),
-            8 => 
+            14 => 
             array (
                 'key' => 'company.address.line_1',
                 'value' => '89 Ivy Lane',
             ),
-            9 => 
+            15 => 
             array (
                 'key' => 'company.address.line_2',
                 'value' => 'Colderson',
             ),
-            10 => 
+            16 => 
             array (
                 'key' => 'company.address.postcode',
                 'value' => 'ST15 5WN',
             ),
-            11 => 
+            17 => 
             array (
                 'key' => 'company.address.region',
                 'value' => 'Stockport',
             ),
-            12 => 
+            18 => 
+            array (
+                'key' => 'company.bank_transfer',
+                'value' => '<p>OctopusTravelMatrix LTD&nbsp;<br>ABN: 11 222 333 444<br>BSB: 555-555&nbsp;<br>ACC: 666777&nbsp;<br>SWIFT: AAABBB8C<br>BANK: DemoData&nbsp;<br>BRANCH: Demo Branch</p>',
+            ),
+            19 => 
             array (
                 'key' => 'company.contact.email',
                 'value' => 'info@octopustravelmatrix.com',
             ),
-            13 => 
+            20 => 
             array (
                 'key' => 'company.contact.phone',
                 'value' => '01632960966',
             ),
-            14 => 
+            21 => 
             array (
                 'key' => 'company.logo',
                 'value' => 'images/octlogo.png',
             ),
-            15 => 
+            22 => 
             array (
                 'key' => 'company.name',
                 'value' => 'Octopus Travel Matrix Ltd.',
             ),
-            16 => 
+            23 => 
             array (
                 'key' => 'company.url',
                 'value' => 'https://octopustravelmatrix.com',
             ),
-            17 => 
+            24 => 
             array (
                 'key' => 'company.vat',
                 'value' => '6210102',
             ),
-            18 => 
+            25 => 
+            array (
+                'key' => 'components.lock',
+                'value' => '0',
+            ),
+            26 => 
             array (
                 'key' => 'email.additional-traveller-added.subject',
                 'value' => 'You have been added as an additional traveller on [TOUR_NAME]',
             ),
-            19 => 
+            27 => 
             array (
                 'key' => 'email.additional-traveller-added.template',
                 'value' => '<p>Greetings [CUSTOMER_TITLE] [CUSTOMER_FIRST_NAME] [CUSTOMER_LAST_NAME],</p>
@@ -127,12 +167,12 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            20 => 
+            28 => 
             array (
                 'key' => 'email.additional-traveller-removed.subject',
                 'value' => 'You have been removed as an additional traveller on [TOUR_NAME]',
             ),
-            21 => 
+            29 => 
             array (
                 'key' => 'email.additional-traveller-removed.template',
                 'value' => '<p>Greetings [CUSTOMER_TITLE] [CUSTOMER_FIRST_NAME] [CUSTOMER_LAST_NAME],</p>
@@ -145,12 +185,12 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            22 => 
+            30 => 
             array (
                 'key' => 'email.booking-confirmation.subject',
                 'value' => 'Booking confirmed for Order [BOOKING_REFERENCE] on Tour [TOUR_NAME]',
             ),
-            23 => 
+            31 => 
             array (
                 'key' => 'email.booking-confirmation.template',
                 'value' => '<p>Greetings [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME],</p>
@@ -161,12 +201,12 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            24 => 
+            32 => 
             array (
                 'key' => 'email.order-cancelled.subject',
                 'value' => 'Order [BOOKING_REFERENCE]: Your order has been cancelled',
             ),
-            25 => 
+            33 => 
             array (
                 'key' => 'email.order-cancelled.template',
                 'value' => '<p>Greetings [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME],</p>
@@ -177,12 +217,12 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            26 => 
+            34 => 
             array (
                 'key' => 'email.order-changed.subject',
                 'value' => 'Order [BOOKING_REFERENCE]: A change has been made to your order',
             ),
-            27 => 
+            35 => 
             array (
                 'key' => 'email.order-changed.template',
                 'value' => '<p>Greetings [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME],</p>
@@ -193,12 +233,12 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            28 => 
+            36 => 
             array (
                 'key' => 'email.payment-due.subject',
                 'value' => 'Order [BOOKING_REFERENCE]: Payment due on [DUE_PAYMENT_DATE]',
             ),
-            29 => 
+            37 => 
             array (
                 'key' => 'email.payment-due.template',
                 'value' => '<p>Dear [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME],</p>
@@ -211,12 +251,12 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            30 => 
+            38 => 
             array (
                 'key' => 'email.payment-made.subject',
                 'value' => 'Order [BOOKING_REFERENCE]: Payment accepted',
             ),
-            31 => 
+            39 => 
             array (
                 'key' => 'email.payment-made.template',
                 'value' => '<p>Dear [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME],</p>
@@ -229,12 +269,12 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            32 => 
+            40 => 
             array (
                 'key' => 'email.payment-overdue.subject',
                 'value' => 'Order [BOOKING_REFERENCE]: Payment overdue since [DUE_PAYMENT_DATE]',
             ),
-            33 => 
+            41 => 
             array (
                 'key' => 'email.payment-overdue.template',
                 'value' => '<p>Dear [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME],</p>
@@ -247,12 +287,12 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            34 => 
+            42 => 
             array (
                 'key' => 'email.refund-given.subject',
                 'value' => 'Order [BOOKING_REFERENCE]: Refund given',
             ),
-            35 => 
+            43 => 
             array (
                 'key' => 'email.refund-given.template',
                 'value' => '<p>Dear [LEAD_TITLE] [LEAD_FIRST_NAME] [LEAD_LAST_NAME],</p>
@@ -265,57 +305,120 @@ class SettingsTableSeeder extends Seeder
 
 <p>[SETTING_COMPANY_NAME]</p>',
             ),
-            36 => 
+            44 => 
+            array (
+                'key' => 'flight.lock',
+                'value' => '0',
+            ),
+            45 => 
+            array (
+                'key' => 'flight.unlock',
+                'value' => '0',
+            ),
+            46 => 
+            array (
+                'key' => 'installments.force',
+                'value' => '0',
+            ),
+            47 => 
+            array (
+                'key' => 'invoice.style',
+                'value' => '1',
+            ),
+            48 => 
+            array (
+                'key' => 'order-notes.lock',
+                'value' => '0',
+            ),
+            49 => 
+            array (
+                'key' => 'order-notes.unlock',
+                'value' => '0',
+            ),
+            50 => 
+            array (
+                'key' => 'passport.lock',
+                'value' => '0',
+            ),
+            51 => 
+            array (
+                'key' => 'passport.unlock',
+                'value' => '0',
+            ),
+            52 => 
             array (
                 'key' => 'payment.required',
                 'value' => '1',
             ),
-            37 => 
+            53 => 
             array (
                 'key' => 'quote.prefix',
                 'value' => 'OTMQ',
             ),
-            38 => 
+            54 => 
+            array (
+                'key' => 'quote.style',
+                'value' => '1',
+            ),
+            55 => 
+            array (
+                'key' => 'social.facebook',
+                'value' => 'https://www.facebook.com/OctopusTravelMatrix',
+            ),
+            56 => 
             array (
                 'key' => 'system.currency',
                 'value' => 'GBP',
             ),
-            39 => 
+            57 => 
             array (
                 'key' => 'system.format.date',
                 'value' => 'd/m/Y',
             ),
-            40 => 
+            58 => 
             array (
                 'key' => 'system.format.time',
                 'value' => 'H:i',
             ),
-            41 => 
+            59 => 
             array (
                 'key' => 'system.historic',
                 'value' => '6',
             ),
-            42 => 
+            60 => 
+            array (
+                'key' => 'system.installments.default',
+                'value' => '{"180":15,"150":15,"120":15,"90":15}',
+            ),
+            61 => 
+            array (
+                'key' => 'system.installments.deposit',
+                'value' => '10',
+            ),
+            62 => 
+            array (
+                'key' => 'system.installments.final',
+                'value' => '60',
+            ),
+            63 => 
             array (
                 'key' => 'system.mail.enabled',
                 'value' => '1',
             ),
-            43 => 
+            64 => 
             array (
                 'key' => 'system.year.start',
                 'value' => '2022-04-01',
             ),
-            44 => 
+            65 => 
             array (
-                'key' => 'company.bank_transfer',
-                'value' => 'Keith Prowse Travel PTY LTD <br />
-                ABN: 31 003 276 775 <br />
-                BSB: 032-298 <br />
-                ACC: 540726 <br />
-                SWIFT: WPACAU2S <br />
-                BANK: Westpac <br />
-                BRANCH: Crows Nest <br />
-                Reference Number: KPAD106102',
+                'key' => 'transport.lock',
+                'value' => '0',
+            ),
+            66 => 
+            array (
+                'key' => 'transport.unlock',
+                'value' => '0',
             ),
         ));
         
