a87a6cd08499d8f81660d64347889005a5729be9
Celeste Gateley <celeste.gateley@gmail.com>
2024-06-11 12:30:02 +0100

    Added room category to inventory table

--- resources/views/pages/admin/accommodation/view.blade.php
+++ resources/views/pages/admin/accommodation/view.blade.php
@@ -75,6 +75,7 @@
             <tr>
                 <th scope="col">Room Type</th>
                 <th scope="col">Board Type</th>
+                <th scope="col">Category</th>
                 <th scope="col">Check In Time</th>
                 <th scope="col">Check Out Time</th>
                 <th scope="col">FIT Selectable</th>
@@ -92,6 +93,7 @@
                 <tr>
                     <td>{{ $inventory->roomType->name }}</td>
                     <td>{{ $inventory->boardType->name }}</td>
+                    <td>{{ $inventory->category->name }}</td>
                     <td data-sort="{{$inventory->check_in->unix()}}">
                         {{ f_datetime($inventory->check_in) }}&nbsp
                         <input type="checkbox" disabled
