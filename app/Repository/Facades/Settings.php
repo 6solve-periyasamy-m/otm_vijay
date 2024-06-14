1ad4cb4710e588d94d30b52a1211848ccef1864e
Celeste Gateley <celeste.gateley@gmail.com>
2024-06-14 12:52:29 +0100

    Fix for null error on convert currency

--- app/Repository/Facades/Settings.php
+++ app/Repository/Facades/Settings.php
@@ -110,15 +110,16 @@ class Settings
     /**
      * Convert any amount from any currency to any other known in the system
      * @param float $amount The amount you with to convert
-     * @param Currency|string $from The currency to convert from
+     * @param Currency|string|null $from The currency to convert from
      * @param Currency|string|null $to The currency to convert to (leave null for system)
      * @return float|null The converted amount, or null if conversion is unavailable due to missing rate or the same currency
      */
-    public function convertCurrency(float $amount, Currency|string $from, Currency|string|null $to = null): ?float
+    public function convertCurrency(float $amount, Currency|string|null $from, Currency|string|null $to = null): ?float
     {
         $from = is_string($from) ? $from : $from->code;
         $to = $to ?? setting('system.currency', 'GBP');
         $to = is_string($to) ? $to : $to->code;
+        if ($from === null) { return $amount; }
         if ($from !== $to) {
             $conversion = Settings::getConversionRate($from, $to);
             if ($conversion !== null) {
