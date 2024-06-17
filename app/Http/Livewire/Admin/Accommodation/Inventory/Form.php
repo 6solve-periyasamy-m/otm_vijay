5b0ee07bb03263eee73f5181c11f66d663497856
Celeste Gateley <celeste.gateley@gmail.com>
2024-06-17 10:46:55 +0100

    Fix for inverted bool checking for stock parents

--- app/Http/Livewire/Admin/Accommodation/Inventory/Form.php
+++ app/Http/Livewire/Admin/Accommodation/Inventory/Form.php
@@ -27,7 +27,7 @@ class Form extends Component
         $this->validate();
         $this->inventory->accommodation_id = $this->inventory->accommodation_id ?? $this->accommodation->id;
         if (!empty($this->inventory->id)) {
-            if ($this->inventory->repository->validateParent($this->inventory->stock_parent_id)) {
+            if (!$this->inventory->repository->validateParent($this->inventory->stock_parent_id)) {
                 $this->toast('Cannot Save', 'You cannot use this stock parent, as it is a child of this inventory', 'danger');
                 return;
             }
