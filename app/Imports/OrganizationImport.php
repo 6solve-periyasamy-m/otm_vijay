a887b9ca5295d542343d1fd93040a64d934e2008
Celeste Gateley <celeste.gateley@gmail.com>
2024-06-19 13:34:56 +0100

    Updated organization import to reflect new fields

--- app/Imports/OrganizationImport.php
+++ app/Imports/OrganizationImport.php
@@ -3,6 +3,8 @@
 namespace App\Imports;
 
 use App\Models\Customer\Organization;
+use App\Models\Location\Address;
+use App\Models\Location\Country;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToCollection;
@@ -17,12 +19,31 @@ class OrganizationImport implements ToCollection, WithHeadingRow, WithValidation
     {
         $models = [];
         foreach ($collection as $row) {
+            $delivery = Address::create([
+                'address_line_1' => trim($row['delivery_line_1']),
+                'address_line_2' => trim($row['delivery_line_2']),
+                'town' => trim($row['delivery_town']),
+                'region' => trim($row['delivery_region']),
+                'country_id' => Country::where('name', 'like', trim($row['delivery_country']))->first()?->id,
+                'postcode' => trim($row['delivery_postcode']),
+            ]);
+            $billing = Address::create([
+                'address_line_1' => trim($row['billing_line_1']),
+                'address_line_2' => trim($row['billing_line_2']),
+                'town' => trim($row['billing_town']),
+                'region' => trim($row['billing_region']),
+                'country_id' => Country::where('name', 'like', trim($row['billing_country']))->first()?->id,
+                'postcode' => trim($row['billing_postcode']),
+            ]);
             $models[] = Organization::create([
-                'name' => $row['name'],
-                'contact_email' => $row['contact_email'],
-                'contact_number' => $row['contact_number'],
-                'internal_notes' => $row['internal_notes'],
-                'external_notes' => $row['external_notes'],
+                'name' => trim($row['name']),
+                'contact_email' => trim($row['contact_email']),
+                'contact_number' => trim($row['contact_number']),
+                'commission' => trim($row['commission']),
+                'delivery_address_id' => $delivery->id,
+                'billing_address_id' => $billing->id,
+                'internal_notes' => trim($row['internal_notes']),
+                'external_notes' => trim($row['external_notes']),
             ]);
         }
         return $models;
@@ -32,6 +53,7 @@ class OrganizationImport implements ToCollection, WithHeadingRow, WithValidation
     {
         return [
             'name' => 'required',
+            'commission' => 'nullable|numeric|min:0',
         ];
     }
 }
