eca5fde6ae9b90a90fb716f54360523ab939c39e
Celeste Gateley <celeste.gateley@gmail.com>
2024-07-31 14:23:03 +0100

    Added extra figures of precision to markup

--- app/Http/Livewire/Admin/Quote/Calculator.php
+++ app/Http/Livewire/Admin/Quote/Calculator.php
@@ -50,7 +50,7 @@ class Calculator extends Component
         $this->total = $this->quote->repository->getTotalCost($this->paying + ($this->quote->leadTraveller->paying ? 1 : 0));
         $this->profit = sigfig($this->total - $this->costToCompany);
         $this->margin = $this->costToCompany == 0 ? 100 : sigfig(($this->total / $this->costToCompany) * 100);
-        $this->markup = sigfig($this->markup ?? $this->margin - 100);
+        $this->markup = sigfig($this->markup ?? $this->margin - 100, 6);
         $this->marked_up_price = sigfig($costPerPerson + ($costPerPerson * ($this->markup / 100)));
         if ($this->quote->commission !== null) {
             $this->commission = sigfig($this->total * ($this->quote->commission / 100));
@@ -67,7 +67,7 @@ class Calculator extends Component
         if ($key === 'marked_up_price') {
             $companyCostTravellers = ($this->paying + $this->travelling + ($this->leadTravelling()));
             $costPerPerson = $companyCostTravellers > 0 ? sigfig($this->costToCompany / $companyCostTravellers) : 0;
-            $this->markup = sigfig(((($this->marked_up_price - $costPerPerson)/$costPerPerson) * 100), 2, true);
+            $this->markup = sigfig(((($this->marked_up_price - $costPerPerson)/$costPerPerson) * 100), 6, true);
         }
         $this->calculate();
     }
