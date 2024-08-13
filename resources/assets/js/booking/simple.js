c24f2a851afb25ff0dc2934d25999ad6a12939f8
nibble-digital <development@nibbledigital.com.au>
2024-08-13 17:15:12 +0530

    datepicker script

--- resources/assets/js/booking/simple.js
+++ resources/assets/js/booking/simple.js
@@ -213,6 +213,15 @@ jQuery(document).ready(function () {
         }
     });
 
+    
+    $("#custom-input-date").datepicker({
+        dateFormat: 'dd/mm/yy',
+        changeMonth: true,
+        changeYear: true,
+        yearRange: '1970:c', 
+        minDate: new Date(1970, 0, 1) 
+    });
+
     /* input scroll */
 
 
