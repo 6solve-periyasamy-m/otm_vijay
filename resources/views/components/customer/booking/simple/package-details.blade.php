42cdbd4aa680b3e258f962f779654668987c6a8e
nibble-digital <development@nibbledigital.com.au>
2024-08-14 17:36:23 +0530

    arrow update

--- resources/views/components/customer/booking/simple/package-details.blade.php
+++ resources/views/components/customer/booking/simple/package-details.blade.php
@@ -64,7 +64,7 @@
     </ul>
     {{ $slot }}
     @error('common')
-    <div class="submit-btn-cls upd-cls">
+    <div class="submit-btn-cls add-on">
         <div class="inner">
             <span style="color: red">{{ $message }}</span>
         </div>
