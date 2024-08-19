dc110d9573c9be232d4d4c98abfad46696d04c1d
Celeste Gateley <celeste.gateley@gmail.com>
2024-08-19 13:26:08 +0100

    Removed asterisk from date-of-birth

--- resources/views/livewire/customer/booking/simple/checkout.blade.php
+++ resources/views/livewire/customer/booking/simple/checkout.blade.php
@@ -53,7 +53,7 @@
                     </div>
                     <div class="form-field rap-las-cls">
                         <div wire:ignore>
-                            <input type="text" id="custom-input-date" class="calendar hasDatepicker" data-picker name="upload-release" placeholder="DATE OF BIRTH*">
+                            <input type="text" id="custom-input-date" class="calendar hasDatepicker" data-picker name="upload-release" placeholder="DATE OF BIRTH">
 
                             <script type="text/javascript">
                                 document.addEventListener('DOMContentLoaded', function() {
