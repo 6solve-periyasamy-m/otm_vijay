233b9da3bc7fc8cbaa849d63675374055235387d
nibble-digital <development@nibbledigital.com.au>
2024-08-20 20:17:10 +0530

    test class for pdf

--- resources/views/pdf/quotes/itinerary.blade.php
+++ resources/views/pdf/quotes/itinerary.blade.php
@@ -229,7 +229,7 @@
                     <tr>
                         <td colspan="2" class="text-left">
                             <div class="date-content">
-                                PAYMENT SCHEDULE
+                                PAYMENT SCHEDULE check
                             </div>
                         </td>
                     </tr>
