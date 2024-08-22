a1b5f00fcf7da7657436ed970992d7deed19290a
nibble-digital <development@nibbledigital.com.au>
2024-08-22 13:38:06 +0530

    dom pdf fixes for html

--- helpers/pdf.php
+++ helpers/pdf.php
@@ -23,22 +23,28 @@ if(!function_exists('puppeteer')) {
 
 }
 
-// if(!function_exists('dompdf')) {
-//     /**
-//      * @param \Illuminate\Contracts\View\View|Factory $view
-//      * @param bool $response Should it be a streamed response, or just formatted for other uses
-//      * @return StreamedResponse|string
-//      */
-//     function dompdf(\Illuminate\Contracts\View\View|Factory $view, bool $response = true): StreamedResponse|string
-//     {
-//         $dompdf = new Dompdf((new Options())->set('dpi', 96)->set('isHtml5ParserEnabled', true));
-//         $dompdf->setPaper('A4', 'portrait');
+if(!function_exists('dompdf')) {
+    /**
+     * @param \Illuminate\Contracts\View\View|Factory $view
+     * @param bool $response Should it be a streamed response, or just formatted for other uses
+     * @return StreamedResponse|string
+     */
+    function dompdf(\Illuminate\Contracts\View\View|Factory $view, bool $response = true): StreamedResponse|string
+    {
+        $html = $view->render();
+        
+        if (!$response) return $html;
 
-//         $dompdf->loadHtml($view->render());
-//         $dompdf->render();
+        return response()->stream(function () use ($html) { echo $html; }, 200, ['Content-Type' => 'text/html']);
 
-//         if (!$response) return $dompdf->output();
+        // $dompdf = new Dompdf((new Options())->set('dpi', 96)->set('isHtml5ParserEnabled', true));
+        // $dompdf->setPaper('A4', 'portrait');
 
-//         return response()->stream(function () use ($dompdf) { echo $dompdf->output(); }, 200, ['Content-Type' => 'application/pdf']);
-//     }
-// }
+        // $dompdf->loadHtml($view->render());
+        // $dompdf->render();
+
+        // if (!$response) return $dompdf->output();
+
+        // return response()->stream(function () use ($dompdf) { echo $dompdf->output(); }, 200, ['Content-Type' => 'application/pdf']);
+    }
+}
