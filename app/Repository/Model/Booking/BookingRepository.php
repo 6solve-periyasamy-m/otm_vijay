2c685da9d5553493a17b34b41eadee5ce2514336
Celeste Gateley <celeste.gateley@gmail.com>
2024-08-23 11:24:17 +0100

    Fix for rooming

--- app/Repository/Model/Booking/BookingRepository.php
+++ app/Repository/Model/Booking/BookingRepository.php
@@ -613,19 +613,19 @@ class BookingRepository extends ModelRepository implements GeneratesFellohData
         $key = -1;
         $group = null;
         foreach ($this->booking->travellers()->where('role', '!=', BookingTravellerRole::NOT_TRAVELLING)->get() as $traveller) {
-            $room = AccommodationInventoryTour::find($rooming[$key]['room']);
-            if ($room instanceof AccommodationInventoryTour) {
-                if ($group === null || $rooming[$key]['travellers'] === 0) {
-                    $key++;
-                    if ($key >= count($rooming)) { break; }
+            if ($group === null || $rooming[$key]['travellers'] === 0) {
+                $key++;
+                if ($key >= count($rooming)) { break; }
+                $room = AccommodationInventoryTour::find($rooming[$key]['room']);
+                if ($room instanceof AccommodationInventoryTour) {
                     $group = new BookingGroup(['booking_id' => $this->booking->id,]);
                     $group->save();
                     $group->repository->addRoomToGroup(AccommodationInventoryTour::find($rooming[$key]['room']));
                 }
-                if ($key >= count($rooming)) { break; }
-                $group->repository->addTravellerToGroup($traveller);
-                --$rooming[$key]['travellers'];
             }
+            if ($key >= count($rooming)) { break; }
+            $group->repository->addTravellerToGroup($traveller);
+            --$rooming[$key]['travellers'];
         }
     }
 
