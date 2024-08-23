17423c8364ab7995ad47e634208d1a2cec1e946a
nibble-digital <development@nibbledigital.com.au>
2024-08-23 23:03:08 +0530

    test for customer

--- resources/views/pdf/quotes/itinerary.blade.php
+++ resources/views/pdf/quotes/itinerary.blade.php
@@ -7,7 +7,7 @@
     
 @endphp
 <?php 
-  var_dump($itinerary);
+  var_dump($itinerary->booker);
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
