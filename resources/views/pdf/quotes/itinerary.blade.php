27caa9dca84c71d7eec908853a2dce028bc01438
nibble-digital <development@nibbledigital.com.au>
2024-08-24 20:46:43 +0530

    pdf update 5

--- resources/views/pdf/quotes/itinerary.blade.php
+++ resources/views/pdf/quotes/itinerary.blade.php
@@ -58,7 +58,7 @@
   .pdf-individual-block {margin-left:-48px;}
   .pdf-individual-block:first-child {margin-top:-50px;}
   .pdf-individual-block .paragraph {
-    max-width:740px!important;
+    max-width:750px!important;
   }
     @font-face {
     font-family: "PlayfairDisplay-Medium";
