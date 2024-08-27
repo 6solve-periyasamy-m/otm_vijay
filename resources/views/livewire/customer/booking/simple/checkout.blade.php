882bc8e80c7a2775ae4d7f1b5663def1f2ac69cc
nibble-digital <development@nibbledigital.com.au>
2024-08-27 16:30:16 +0530

    css update

--- resources/views/livewire/customer/booking/simple/checkout.blade.php
+++ resources/views/livewire/customer/booking/simple/checkout.blade.php
@@ -2,10 +2,11 @@
     <div class="second-form">
         <div class="left-col">
             <div class="contain">
-                <div class="purchase-info-block">
-                <div class="evnt-name">
-                    <h4 class="head-evnt">{{ $booking->tour->event?->name }}</h4>
+            <div class="evnt-name">
+                    <h4 class="head-evnt">{{ $tour->event?->name }}</h4>
                 </div>
+                <div class="purchase-info-block">
+               
                     <h3>Enter purchaser information</h3>
                     <div class="top-check-top-cls">
                         <div class="txt-cls-mod-fs">
