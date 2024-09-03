18776652cc325a43fd01b263f25542aaf824cb1f
nibble-digital <development@nibbledigital.com.au>
2024-09-03 13:22:16 +1000

    pdf feedback

--- resources/views/pdf/quotes/itinerary.blade.php
+++ resources/views/pdf/quotes/itinerary.blade.php
@@ -130,7 +130,7 @@
     font-weight: 500;
     line-height: 42px;
     margin-bottom: 16px;
-    margin-top: 10px;
+    margin-top: 15px;
   }
   .customer-details-block h6 {
     font-family: "PPNeueMontreal-Medium";
