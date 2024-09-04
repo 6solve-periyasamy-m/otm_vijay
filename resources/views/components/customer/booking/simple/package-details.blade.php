6170703c1187db9735cec43fbb1992bb3e7de55f
Celeste Gateley <celeste.gateley@gmail.com>
2024-09-04 12:12:36 +0100

    Increased inclusion amount to 4

--- resources/views/components/customer/booking/simple/package-details.blade.php
+++ resources/views/components/customer/booking/simple/package-details.blade.php
@@ -19,7 +19,7 @@
         <h6>{{ $tour->name }}</h6>
         <!--<p class="location"></p> TODO: Implement Location on Event -->
         <p class="date">{{ $tour->date_from?->format('M d, Y') }} - {{ $tour->date_to?->format('M d, Y') }}</p>
-        @foreach($tour->repository->getInclusions(3) as $inclusion)
+        @foreach($tour->repository->getInclusions(4) as $inclusion)
             <p class="inclusion">{{ $inclusion }}</p>
         @endforeach
         <p class="see-more">
