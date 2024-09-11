ec74d271eee612c5f2d8790ddb60704cf33a9131
Celeste Gateley <celeste.gateley@gmail.com>
2024-09-11 13:39:16 +0100

    Added ability to add merchandise to quote

--- app/Repository/Model/Merchandise/MerchandiseInventoryRepository.php
+++ app/Repository/Model/Merchandise/MerchandiseInventoryRepository.php
@@ -8,6 +8,7 @@ use App\Models\Merchandise\MerchandiseInventoryTour;
 use App\Models\Quote\Component\QuoteMerchandise;
 use App\Models\Quote\Quote;
 use App\Models\Tour\Tour;
+use App\Models\Transport\TransportInventory;
 use App\Repository\Abstracts\ComponentPackageRepository;
 use App\Repository\Abstracts\InventoryRepository;
 use App\Repository\Abstracts\InventoryTourRepository;
@@ -90,9 +91,22 @@ class MerchandiseInventoryRepository extends InventoryRepository
         return now();
     }
 
+    /**
+     * @param Carbon $from
+     * @param Carbon $to
+     * @param ComponentPackageRepository|null $repository
+     * @return Collection<MerchandiseInventory>
+     */
     public static function getBetweenDates(Carbon $from, Carbon $to, ComponentPackageRepository $repository = null): Collection
     {
-        return Merchandise::all();
+        $inventories = [];
+        if (isset($repository)) {
+            foreach ($repository->getComponents(false, false, false, false, true) as $inventoryTour) {
+                $inventories[] = $inventoryTour->getInventory()?->get()->id;
+            }
+        }
+        return MerchandiseInventory::whereNotIn('id', $inventories)->get();
+
     }
 
     public function get(): MerchandiseInventory
