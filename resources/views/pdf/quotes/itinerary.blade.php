9e8957065ae5c6e435010866a444e2aacf1fab3c
nibble-digital <development@nibbledigital.com.au>
2024-09-11 23:49:11 +1000

    fixes

--- resources/views/pdf/quotes/itinerary.blade.php
+++ resources/views/pdf/quotes/itinerary.blade.php
@@ -726,6 +726,12 @@ h5 span {
                       <td style="font-weight:400;min-width:128px;">GST (included):</td>
                       <td>{{ $itinerary->finances->tax > 0 ? f_currency($itinerary->finances->tax) : 'No Taxes Due' }}</td>
                   </tr>
+                  @if($itinerary->finances->commission > 0 )
+                  <tr>
+                      <td style="font-weight:400;min-width:128px;">Commission:</td>
+                      <td>{{ f_currency($itinerary->finances->commission) }}({{$itinerary->finances->commissionPercent)}})</td>
+                  </tr>
+                  @endif
                   <tr>
                       <td style="font-weight:400;min-width:128px;">Final Cost:</td>
                       <td>{{ f_currency($itinerary->finances->cost) }}</td>
