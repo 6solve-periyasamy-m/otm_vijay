317b6eb57adadd8e8f6ca5468d1d951de1232dae
nibble-digital <development@nibbledigital.com.au>
2024-09-13 00:34:21 +1000

    ss

--- resources/views/pdf/quotes/itinerary.blade.php
+++ resources/views/pdf/quotes/itinerary.blade.php
@@ -742,7 +742,7 @@ figure.table tr td:nth-child(2) {display:none;}
                   @endif      
                   <tr>
                       <td style="font-weight:400;min-width:128px;"><strong style="margin-top:10px">FINAL COST:
-                      <span style="width: 84%;height: 1px;display: block;margin: 0;margin-top: 2px;background-color: var(--head-text-background);"></span>
+                      <span style="width: 100%;height: 1px;display: block;margin: 0;margin-top: 2px;background-color: var(--head-text-background);"></span>
                       </strong></td>
                       <td><strong style="margin-top:10px">{{ f_currency($itinerary->finances->cost) }}</strong></td>
                   </tr>
