a9ace6495dadcc3123f8c91d23977246a7cba0db
Celeste Gateley <celeste.gateley@gmail.com>
2024-09-13 11:09:44 +0100

    Updated quote form to prefill more fields

--- resources/views/pages/admin/quote/create/basic.blade.php
+++ resources/views/pages/admin/quote/create/basic.blade.php
@@ -5,17 +5,18 @@
 @section('title', 'Create Basic Quote for ' . $tour->name)
 
 @section('form-body')
-    <x-livewire.input.select.customer name="customer_id" width="3" label="Lead Traveller" />
-    <x-livewire.input.select.brand name="brand_id" label="Quote Branding" width="3" />
+    <x-livewire.input.select.brand name="brand_id" value="{{$tour->brand_id}}" label="Quote Branding" width="3" />
     <x-livewire.input type="date" name="expires" width="3" value="{{\Settings::defaultQuoteExpiry()}}" label="Expiry Date"/>
-    <x-livewire.input name="single_occupancy_surcharge" width="3" label="Single Occupancy Surcharge" />
-    <x-livewire.input.select.organization name="organization_id" label="Organization (Optional)" width="6"/>
-    <x-livewire.input.select.tax-bracket name="tax_bracket_id" label="Tax Bracket" width="6"/>
-    <hr class="splitter">
-    <x-livewire.input.checkbox name="travelling" value="1" width="6" label="Lead Travelling?" />
-    <x-livewire.input.checkbox name="paying" value="1" width="6" label="Lead Paying?"/>
+    <x-livewire.input.select.customer name="customer_id" width="3" label="Lead Traveller" />
+    <div class="col-xl-3 row">
+        <x-livewire.input.checkbox name="travelling" value="1" width="6" checked label="Lead Travelling?" />
+        <x-livewire.input.checkbox name="paying" value="1" width="6" checked label="Lead Paying?"/>
+    </div>
+    <x-livewire.input name="single_occupancy_surcharge" width="4" label="Single Occupancy Surcharge" value="{{ $tour->single_occupancy_surcharge }}"/>
+    <x-livewire.input.select.organization name="organization_id" label="Organization (Optional)" width="4"/>
+    <x-livewire.input.select.tax-bracket name="tax_bracket_id" value="{{ $tour->tax_bracket_id }}" label="Tax Bracket" width="4"/>
     <hr class="splitter">
     <x-livewire.input.text-area name="internal_notes" width="6" label="Internal Notes" />
     <x-livewire.input.text-area name="external_notes" width="6" label="External Notes" />
-    <input type="submit" class="btn btn-primary text-white" name="Submit">
+    <input type="submit" class="btn btn-primary text-white" value="Submit">
 @endsection
