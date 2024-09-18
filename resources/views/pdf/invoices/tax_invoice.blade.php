81cc08d31e399fb4f7f83d7c66804a1ec1be5ff5
nibble-digital <development@nibbledigital.com.au>
2024-09-18 17:51:33 +0530

    pdf quotes final update

--- resources/views/pdf/invoices/tax_invoice.blade.php
+++ resources/views/pdf/invoices/tax_invoice.blade.php
@@ -430,7 +430,7 @@
         <tr>
             <td class="left-column"> <img src="data:image/png;base64,<?php echo base64_encode(file_get_contents('images/pdf_assets/images/KeithProwse-Travel-Logo.png'))?>"  alt="logo-ch"></td>
             <td class="right-column">
-                <p><span>Address:</span><span>{{$invoice->brand->address_line_1}} <br>{{$invoice->brand->address_line_2}}, {{$invoice->brand->town}}, {{$invoice->brand->region}}, {{$invoice->brand->postcode}}<br>
+                <p><span>Address:</span><span style="font-weight:400;margin-left:5px;">{{$invoice->brand->address_line_1}} <br>{{$invoice->brand->address_line_2}}, {{$invoice->brand->town}}, {{$invoice->brand->region}}, {{$invoice->brand->postcode}}<br>
                 {{$invoice->brand->vat_code}}</span></p>
                 <p><span>Company ABN:</span><span style="font-weight:400;margin-left:5px;">31 003 276 775</span></p>               
                 <p><span>Email:</span><span style="font-weight:400;margin-left:5px;">info@octopustravelmatrix.com</span></p>
