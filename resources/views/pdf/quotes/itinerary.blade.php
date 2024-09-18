a7649133f97c5f00a436056f1e1b6bf4971ff48d
nibble-digital <development@nibbledigital.com.au>
2024-09-18 17:28:27 +0530

    pdf update

--- resources/views/pdf/quotes/itinerary.blade.php
+++ resources/views/pdf/quotes/itinerary.blade.php
@@ -481,7 +481,7 @@ figure.table tr td:nth-child(2) {display:none;}
                 <div class="customer-details">
                 @if (!is_null($itinerary->organization) && !is_null($itinerary->organization->commission))
                     <h6>Organisation DETAILS</h6>
-                    <p>Organisation <span>{{ $itinerary->organization->name }}</span></p>
+                    <p>Organisation:<span>{{ $itinerary->organization->name }}</span></p>
                     <!-- <p>Organization Email: <span>{{ $itinerary->organization->contact_email }}</span></p> -->
                     <p>Travel Agent Name: <span>{{ $cusname }}</span></p>
                     <p>Travel Agent Email: <span>{{ $cusmail }}</span></p>
