e6cdb5abe0404f92445801a29dafc8df8184bea8
Celeste Gateley <celeste.gateley@gmail.com>
2024-09-25 10:00:55 +0100

    Hid notifications behind a permission

--- resources/views/livewire/admin/system/notification/badge.blade.php
+++ resources/views/livewire/admin/system/notification/badge.blade.php
@@ -1,5 +1,7 @@
-@if($unseen !== null && $unseen > 0)
-    <span class="badge badge-pill badge-danger">{{ $unseen > 99 ? "99+" : $unseen }}</span>
-@else
-    <span></span>
-@endif
+@can('read', \App\Models\System\Notification::class)
+    @if($unseen !== null && $unseen > 0)
+        <span class="badge badge-pill badge-danger">{{ $unseen > 99 ? "99+" : $unseen }}</span>
+    @else
+        <span></span>
+    @endif
+@endcan
