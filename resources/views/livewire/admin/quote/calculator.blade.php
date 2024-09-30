90a235831eb3b81df367c6a6bb0b2c35419d0f30
Celeste Gateley <celeste.gateley@gmail.com>
2024-09-30 11:20:48 +0100

    Adjusted quote calculator

--- resources/views/livewire/admin/quote/calculator.blade.php
+++ resources/views/livewire/admin/quote/calculator.blade.php
@@ -84,54 +84,48 @@
         </x-admin.section.otm-text>
     </x-admin.section.card>
     {{-- Component Costs --}}
-        <x-admin.section.card width="6">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col" class="fw-bold py-1">Type</th>
-                        <th scope="col" class="fw-bold py-1">Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.accommodation') }}<span style="text-decoration-line: underline; text-decoration-style: dotted;" title="{{ __('quotes.view.cards.quick.calculator.components.approximate') }}">*</span></th>
-                        <td class="py-1">{{ f_currency($quote->repository->getAccommodationCost(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.activities') }}</th>
-                        <td class="py-1">{{ f_currency($quote->repository->getActivityCost(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.flights') }}</th>
-                        <td class="py-1">{{ f_currency($quote->repository->getFlightCost(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.transport') }}</th>
-                        <td class="py-1">{{ f_currency($quote->repository->getTransportCost(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.merchandise') }}</th>
-                        <td class="py-1">{{ f_currency($quote->repository->getMerchandiseCost(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.per-customer') }}</th>
-                        <td class="py-1">{{ f_currency($quote->repository->getPerCustomerAdditionals(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.overall') }}</th>
-                        <td class="py-1">{{ f_currency($quote->repository->getWholeOrderAdditionals(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
-                    </tr>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <th scope="col" class="fw-bold py-1">{{ __('quotes.view.cards.quick.calculator.components.total') }}</th>
-                        <th scope="col" class="fw-bold py-1">{{ f_currency($quote->repository->getPurchaseTotal()) }}</th>
-                    </tr>
-                </tfoot>
-            </table>
-        </x-admin.section.card>
+    <x-admin.section.card width="3">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col" class="fw-bold py-1">Type</th>
+                    <th scope="col" class="fw-bold py-1">Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.accommodation') }}<span style="text-decoration-line: underline; text-decoration-style: dotted;" title="{{ __('quotes.view.cards.quick.calculator.components.approximate') }}">*</span></th>
+                    <td class="py-1">{{ f_currency($quote->repository->getAccommodationCost(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
+                </tr>
+                <tr>
+                    <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.activities') }}</th>
+                    <td class="py-1">{{ f_currency($quote->repository->getActivityCost(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
+                </tr>
+                <tr>
+                    <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.flights') }}</th>
+                    <td class="py-1">{{ f_currency($quote->repository->getFlightCost(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
+                </tr>
+                <tr>
+                    <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.transport') }}</th>
+                    <td class="py-1">{{ f_currency($quote->repository->getTransportCost(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
+                </tr>
+                <tr>
+                    <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.merchandise') }}</th>
+                    <td class="py-1">{{ f_currency($quote->repository->getMerchandiseCost(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
+                </tr>
+                <tr>
+                    <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.per-customer') }}</th>
+                    <td class="py-1">{{ f_currency($quote->repository->getPerCustomerAdditionals(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
+                </tr>
+                <tr>
+                    <th scope="row" class="py-1">{{ __('quotes.view.cards.quick.calculator.components.overall') }}</th>
+                    <td class="py-1">{{ f_currency($quote->repository->getWholeOrderAdditionals(($quote->leadTraveller->travelling) + $paying + $travelling)) }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </x-admin.section.card>
     {{-- Quote Costs --}}
-    <x-admin.section.card width="6">
+    <x-admin.section.card width="9">
         <div class="row">
             <div class="col-6">
                 <x-admin.section.otm-text class="ctc-updater">
