e382aa80aa9ca587a8efa0d4e5b11f15776ed6d9
Celeste Gateley <celeste.gateley@gmail.com>
2024-10-30 10:15:45 +0000

    Show 0 if no stock

--- resources/views/components/admin/accommodation/accommodation-by-date.blade.php
+++ resources/views/components/admin/accommodation/accommodation-by-date.blade.php
@@ -76,8 +76,12 @@
         @foreach($period as $date)
             @php $onNight = $storage->getInventoryOnNight($date) @endphp
             <div class="col-4">
-                @if($storage->getInventoryOnNight($date) !== null && $onNight->repository->getAvailableStock() > 0)
-                    <h4 class="badge badge-pill badge-success">{{ f_date($date) }}: {{ $onNight->repository->getAvailableStock() }}</h4>
+                @if($storage->getInventoryOnNight($date) !== null)
+                    @if($onNight->repository->getAvailableStock() > 0)
+                        <h4 class="badge badge-pill badge-success">{{ f_date($date) }}: {{ $onNight->repository->getAvailableStock() }}</h4>
+                    @else
+                        <h4 class="badge badge-pill badge-danger">{{ f_date($date) }}: {{ $onNight->repository->getAvailableStock() }}</h4>
+                    @endif
                 @else
                     <h4 class="badge badge-pill badge-danger">{{ f_date($date) }}: {{ Icon::cross() }}</h4>
                 @endif
