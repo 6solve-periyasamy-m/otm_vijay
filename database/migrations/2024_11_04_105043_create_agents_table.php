fa3277ca78d141c956f0690dbd92d5c4e8ca77ea
Nicholas Alexander <info@remotedevelopment.co.uk>
2024-11-05 07:18:49 +0000

    remove agent commission override, not required

--- database/migrations/2024_11_04_105043_create_agents_table.php
+++ database/migrations/2024_11_04_105043_create_agents_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->foreignId('organization_id')->nullable()->constrained()->onDelete('set null');
-            $table->decimal('commission', 5, 2)->nullable();
         });
     }
 
