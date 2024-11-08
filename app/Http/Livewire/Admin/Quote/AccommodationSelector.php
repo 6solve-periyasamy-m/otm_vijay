0eefbfc7d2e9035b2079b53057dad2cc4b6e1878
Celeste Gateley <celeste.gateley@gmail.com>
2024-11-08 13:49:50 +0000

    Implemented the ability to manipulate quantities for Quote Accommodation Selector

--- app/Http/Livewire/Admin/Quote/AccommodationSelector.php
+++ app/Http/Livewire/Admin/Quote/AccommodationSelector.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Admin\Quote;
 use App\Http\Livewire\Abstract\AccommodationByDateComponent;
 use App\Models\Accommodation\Accommodation;
 use App\Models\Quote\Quote;
+use App\Repository\Storage\Rooming\AccommodationByDateStorage;
 use Carbon\Carbon;
 
 class AccommodationSelector extends AccommodationByDateComponent
@@ -36,6 +37,58 @@ class AccommodationSelector extends AccommodationByDateComponent
             }
         }
         $this->toast('Accommodation Saved Successfully', 'Successfully removed accommodation and added new ones to the quote', 'success');
+    }
+
+    public function getQuantity(AccommodationByDateStorage $storage): int
+    {
+        foreach ($this->selected as $key => $data) {
+            if (($data['id'] ?? null) === $storage->accommodation->id
+                && ($data['room'] ?? null) === $storage->room->id
+                && ($data['board'] ?? null) === $storage->board->id
+                && ($data['category'] ?? null) === $storage->category?->id) {
+                return $data['quantity'] ?? 0;
+            }
+        }
+        return 0;
+    }
+
+    public function addQuantity(int $accommodation, int $room, int $board, int|null $category): void
+    {
+        foreach ($this->selected as $key => $data) {
+            if (($data['id'] ?? null) === $accommodation
+                && ($data['room'] ?? null) === $room
+                && ($data['board'] ?? null) === $board
+                && ($data['category'] ?? null) === $category) {
+                $data['quantity'] = ($data['quantity'] ?? 0) + 1;
+                $this->selected[$key] = $data;
+                return;
+            }
+        }
+        $this->selected[] = [
+            'id' => $accommodation,
+            'room' => $room,
+            'board' => $board,
+            'category' => $category,
+            'quantity' => 1,
+        ];
+    }
+
+    public function removeQuantity(int $accommodation, int $room, int $board, int|null $category): void
+    {
+        foreach ($this->selected as $key => $data) {
+            if (($data['id'] ?? null) === $accommodation
+                && ($data['room'] ?? null) === $room
+                && ($data['board'] ?? null) === $board
+                && ($data['category'] ?? null) === $category) {
+                $quantity = ($data['quantity'] ?? 0) - 1;
+                if ($quantity < 1) {
+                    unset($this->selected[$key]);
+                } else {
+                    $data[$quantity] = $quantity;
+                    $this->selected[$key] = $data;
+                }
+            }
+        }
     }
 
     public function getPackageType(): string
