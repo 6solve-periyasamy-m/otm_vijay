bc3c14a06f1aa1240efdaf43e4185cf7fdf5a1b3
Celeste Gateley <celeste.gateley@gmail.com>
2024-11-18 14:44:31 +0000

    Added component internal notes to the quote component tables

--- app/Repository/Model/Quote/Component/QuoteMerchandiseRepository.php
+++ app/Repository/Model/Quote/Component/QuoteMerchandiseRepository.php
@@ -152,4 +152,14 @@ class QuoteMerchandiseRepository extends QuoteComponentRepository
     {
         return $this->getInventory()?->getItineraryItem($this->quoteComponent->quantity ?? $travelling);
     }
+
+    public function getComponentInternalNotes(): string|null
+    {
+        return $this->quoteComponent->inventory->component->internal_notes;
+    }
+
+    public function getComponentExternalNotes(): string|null
+    {
+        return $this->quoteComponent->inventory->component->external_notes;
+    }
 }
