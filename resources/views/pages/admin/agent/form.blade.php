0f496a6af32338719a8a6051368ee1dd7e93d1be
Nicholas Alexander <info@remotedevelopment.co.uk>
2024-11-21 19:38:55 +0000

    use x-livewire in agent form

--- resources/views/pages/admin/agent/form.blade.php
+++ resources/views/pages/admin/agent/form.blade.php
@@ -10,8 +10,8 @@ $agent = $agent ?? null;
 @section('title', empty($agent) ? 'Create Agent': 'Update Agent')
 
 @section('form-body')
-<x-admin.input name="first_name" width="40" value="{{ $agent?->first_name }}">First Name</x-admin.input>
-<x-admin.input name="last_name" width="40" value="{{ $agent?->last_name }}">Last Name</x-admin.input>
-<x-admin.input name="email" width="4" value="{{ $organization?->contact_email }}">Contact Email</x-admin.input>
+<x-livewire.input name="first_name" width="40" value="{{ $agent?->first_name }}">First Name</x-livewire.input>
+<x-livewire.input name="last_name" width="40" value="{{ $agent?->last_name }}">Last Name</x-livewire.input>
+<x-livewore.input name="email" width="4" value="{{ $organization?->contact_email }}">Contact Email</x-livewire.input>
 <input type="submit" class="btn btn-primary text-white" name="Submit">
 @endsection
