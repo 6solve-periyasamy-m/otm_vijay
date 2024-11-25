62db9f2d47e2d0f556d33f80eeb6b3869696c440
Nicholas Alexander <info@remotedevelopment.co.uk>
2024-11-25 13:21:48 +0000

    remove trailing dash on agents list when no org, make input form from quote screen for agent clear that it is not an organization agent, and clear on the organization add agent that it is an organization agent

--- resources/views/livewire/admin/agent/form.blade.php
+++ resources/views/livewire/admin/agent/form.blade.php
@@ -3,7 +3,7 @@
         {{ __($agent?->id !== null ? 'agent.form.title.update' : 'agent.form.title.create') }}
     </x-slot:title>
     <x-slot:title>
-        Agent Details
+        {{ $organization ? 'Organization Agent' : 'Contact'}} Details
     </x-slot:title>
     <x-livewire.input tabindex="0" wire:model="agent.first_name" required width="40" label="{{__('agent.form.fields.first_name')}}" />
     <x-livewire.input tabindex="0" wire:model="agent.last_name" required width="40" label="{{__('agent.form.fields.last_name')}}" />
