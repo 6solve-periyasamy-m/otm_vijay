2131e26b780c8f8539e501f33c5eee4bdc36443c
balajinibbleedge <balaji@nibbleedge.com.au>
2024-12-03 11:12:48 +0530

    Corrected the total instead of the cost amount of the booking

--- resources/views/pdf/quotes/itinerary.blade.php
+++ resources/views/pdf/quotes/itinerary.blade.php
@@ -840,7 +840,7 @@ figure.table tr td:nth-child(2) {display:none;}
                       <td style="font-weight:400;min-width:128px;"><strong style="margin-top:15px">FINAL PRICE:
                       <span style="width: 100%;height: 1px;display: block;margin: 0;margin-top: 2px;background-color: var(--head-text-background);"></span>
                       </strong></td>
-                      <td><strong style="margin-top:15px">{{ f_currency($itinerary->finances->cost) }}</strong></td>
+                      <td><strong style="margin-top:15px">{{ f_currency($itinerary->finances->total) }}</strong></td>
                   </tr>
                                     
            </tbody>
