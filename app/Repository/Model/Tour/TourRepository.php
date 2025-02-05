88de2f1a7c981dcc9d86f419b6985509f2367abc
Celeste Gateley <celeste.gateley@gmail.com>
2025-02-05 04:02:51 +0000

    Updated dates

--- app/Repository/Model/Tour/TourRepository.php
+++ app/Repository/Model/Tour/TourRepository.php
@@ -670,9 +670,9 @@ class TourRepository extends ComponentPackageRepository implements HasStockContr
             }
             $inclusion = match (true) {
                 $component instanceof AccommodationInventoryRepository =>
-                    $component->get()->accommodation->name . ' - ' . $component->getNightsInTour($this->tour) . ' Nights',
+                $component->getStartTime()?->format('d M') . " - " . $component->get()->accommodation->name . ' - ' . $component->getNightsInTour($this->tour) . ' Nights',
                 $component instanceof ActivityInventoryRepository =>
-                    $component->getStartTime()?->format('d M Y') . " - " . $component->get()->activity->name,
+                    $component->getStartTime()?->format('d M') . " - " . $component->get()->activity->name,
                 $component instanceof MerchandiseInventoryRepository =>
                     $component->get()->component->name,
                 $component instanceof TransportInventoryRepository =>
