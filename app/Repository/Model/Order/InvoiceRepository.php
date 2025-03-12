db90b6cc7d7ee50e58f7b96eb60c2916291e46d8
balaji-6solve <balaji-6solve@gmail.com>
2025-03-12 12:30:35 +0530

    Revert to the accommodation entries merged in the invoice document.

--- app/Repository/Model/Order/InvoiceRepository.php
+++ app/Repository/Model/Order/InvoiceRepository.php
@@ -72,97 +72,15 @@ class InvoiceRepository
         if ($this->invoice->groups) {
             foreach ($this->invoice->groups as $group) {
                 foreach ($group->billables as $billable) {
-                    $qBillable = $accom_data->get($billable->shared_key, new QuantityBillable($billable->description, $billable->shared_key, $billable->amount, $billable->is_base));
-                    $accom_data->put($billable->shared_key, $qBillable->addQuantity());
+                    $qBillable = $data->get($billable->shared_key, new QuantityBillable($billable->description, $billable->shared_key, $billable->amount, $billable->is_base));
+                    $data->put($billable->shared_key, $qBillable->addQuantity());
                     if (strpos($billable->shared_key, "transport") !== false) {
                         $billable->description = $this->transportDescriptionFormat($billable->description);
                     }
                 }
             }
-            $accom_data = $this->mergeBillablesByQuantity($accom_data);
         }
-        $merged_data = $data->merge($accom_data);
-        return $merged_data;
-    }
-
-    public function mergeBillablesByQuantity(Collection $items): Collection
-    {
-        $merged_items = collect();
-        $previous_item = null;
-
-        foreach ($items->sortBy(fn($item) => $this->extractCheckInDate($item->description)) as $item) {
-            if ($previous_item && $this->canMerge($previous_item, $item)) {
-                preg_match('/\((\d{2}\/\d{2}\/\d{4}) (\d{2}:\d{2}) to (\d{2}\/\d{2}\/\d{4}) (\d{2}:\d{2})\)/', $previous_item->description, $prev_matches);
-                preg_match('/\((\d{2}\/\d{2}\/\d{4}) (\d{2}:\d{2}) to (\d{2}\/\d{2}\/\d{4}) (\d{2}:\d{2})\)/', $item->description, $curr_matches);
-
-                if ($prev_matches && $curr_matches) {
-                    $new_description = str_replace($prev_matches[0], "({$prev_matches[1]} {$prev_matches[2]} to {$curr_matches[3]} {$curr_matches[4]})", $previous_item->description);
-                    $previous_item->description = $new_description;
-                    //$previous_item->setQuantity($previous_item->getQuantity() + $item->getQuantity());
-                }
-            } else {
-                if ($previous_item) {
-                    $merged_items->push($previous_item);
-                }
-                $previous_item = clone $item;
-            }
-        }
-
-        if ($previous_item) {
-            $merged_items->push($previous_item);
-        }
-        return $merged_items;
-    }
-
-    /**
-     * Checks if two billable items can be merged.
-     */
-    private function canMerge($item1, $item2): bool
-    {
-        return $this->extractHotelName($item1->description) === $this->extractHotelName($item2->description)
-            && $this->extractRoomType($item1->description) === $this->extractRoomType($item2->description)
-            && $this->extractCheckOutDate($item1->description) === $this->extractCheckInDate($item2->description);
-    }
-
-    /**
-     * Extract hotel name from description.
-     */
-    private function extractHotelName($description): string
-    {
-        return trim(explode("(", $description)[0]);
-    }
-
-    /**
-     * Extract check-in date from description.
-     */
-    private function extractCheckInDate($description): ?string
-    {
-        if (preg_match('/\((\d{2}\/\d{2}\/\d{4}) \d{2}:\d{2} to/', $description, $matches)) {
-            return $matches[1];
-        }
-        return null;
-    }
-
-    /**
-     * Extract check-out date from description.
-     */
-    private function extractCheckOutDate($description): ?string
-    {
-        if (preg_match('/to (\d{2}\/\d{2}\/\d{4}) \d{2}:\d{2}\)/', $description, $matches)) {
-            return $matches[1];
-        }
-        return null;
-    }
-
-    /**
-     * Extract room type from description.
-     */
-    private function extractRoomType($description): string
-    {
-        if (preg_match('/\)([^)]+)$/', $description, $matches)) {
-            return trim($matches[1]);
-        }
-        return '';
+        return $data;
     }
 
     public function transportDescriptionFormat($invoice_description)
