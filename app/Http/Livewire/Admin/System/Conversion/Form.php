9ef248e27f0879aa5b8ae571348ce4b00f035cf3
Celeste Gateley <celeste.gateley@gmail.com>
2025-04-22 13:16:27 +0100

    Added button to estimate inverse rates for creating conversion rates

--- app/Http/Livewire/Admin/System/Conversion/Form.php
+++ app/Http/Livewire/Admin/System/Conversion/Form.php
@@ -14,6 +14,7 @@ class Form extends ModalComponent
 
     /** @var ConversionRate|null $rate */
     public ConversionRate|int|null $rate = null;
+    public bool $estimateInverse = false;
 
     public function mount(ConversionRate|int|null $rate = null)
     {
@@ -26,10 +27,32 @@ class Form extends ModalComponent
         $this->rate = $rate;
     }
 
+    public function canEstimateInverse(): bool
+    {
+        if ($this->rate->from === null || $this->rate->to === null) {
+            // if one or the other isn't set, then can't check
+            return false;
+        }
+        if ($this->rate->from_currency_id === $this->rate->to_currency_id) {
+            // if they're the same, can't check
+            return false;
+        }
+        // Make sure one doesn't already exist in the system
+        return ConversionRate::where('from_currency_id', $this->rate->to_currency_id)
+                ->where('to_currency_id', $this->rate->from_currency_id)->first() === null;
+    }
+
     public function save()
     {
         $this->validate();
         $this->rate->save();
+        if ($this->estimateInverse && $this->canEstimateInverse()) {
+            ConversionRate::create([
+               'from_currency_id' => $this->rate->to_currency_id,
+               'to_currency_id' => $this->rate->from_currency_id,
+               'rate' => sigfig(1.0 / $this->rate->rate),
+            ]);
+        }
         $this->refreshTables();
         $this->closeModal();
     }
--- resources/views/livewire/admin/system/conversion/form.blade.php
+++ resources/views/livewire/admin/system/conversion/form.blade.php
@@ -2,8 +2,16 @@
     <div class="row">
         <x-livewire.input.select.currency name="rate.from_currency_id" value="{{ $rate?->from_currency_id }}" label="From Currency" width="3" />
         <x-livewire.input.select.currency name="rate.to_currency_id" value="{{ $rate?->to_currency_id }}" label="To Currency" width="3" />
-        <x-livewire.input wire:model.debounce.300ms="rate.rate" label="Conversion Rate" width="3" type="number" step="0.01" />
-        <div class="col-xl-3">
+        <x-livewire.input wire:model.debounce.300ms="rate.rate" label="Conversion Rate" width="2" type="number" step="0.01" />
+        @if($this->canEstimateInverse())
+            <x-livewire.input.checkbox wire:model="estimateInverse" label="Estimate inverse rate" width="2" />
+        @else
+            <x-livewire.input.checkbox wire:model="estimateInverse" label="Estimate inverse rate" width="2" disabled />
+        @endif
+        <div class="col-xl-1">
+            Est. Rate: {{ sigfig(1 / ($rate->rate ?? 1)) }}
+        </div>
+        <div class="col-xl-1">
             <button class="btn btn-success" wire:click="save">Save</button>
         </div>
     </div>
