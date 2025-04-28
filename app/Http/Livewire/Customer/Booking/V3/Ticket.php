17a903697d0fdd5d832e97d658751d3961f9fb80
Celeste Gateley <celeste.gateley@gmail.com>
2025-04-28 09:45:16 +0100

    Added stubs for remaining pages

---
+++ app/Http/Livewire/Customer/Booking/V3/Ticket.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Livewire\Customer\Booking\V3;
+
+use App\Http\Livewire\Abstract\V3BookingComponent;
+
+class Ticket extends V3BookingComponent
+{
+
+    public function back()
+    {
+        // TODO: Implement back() method.
+    }
+
+    public function advance()
+    {
+        // TODO: Implement advance() method.
+    }
+
+    public function render()
+    {
+        return view('livewire.customer.booking.v3.ticket');
+    }
+}
