53a4db1c4820a7d9ca70ad3facde8e0c6707567e
Celeste Gateley <celeste.gateley@gmail.com>
2025-04-29 16:01:03 +0100

    Switched to using cookies for token storage

--- app/Http/Controllers/Customer/BookingV3Controller.php
+++ app/Http/Controllers/Customer/BookingV3Controller.php
@@ -4,42 +4,57 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use App\Models\Booking\Booking;
+use App\Models\Quote\Quote;
 use App\Models\Tour\Tour;
 use App\Repository\Model\Booking\BookingRepository;
-use App\Models\Quote\Quote;
+use Cookie;
+use Illuminate\Http\Request;
 
 class BookingV3Controller extends Controller
 {
-    public function guest(string $tour, string|null $token = null)
+    public function guest(Request $request, string $tour, string|null $token = null)
     {
         $tour = Tour::where('booking_form_url', '=', $tour)->firstOrFail();
+        $token = $token ?? $request->cookie($tour->booking_form_url);
         $booking = Booking::where('tour_id', '=', $tour->id)->where('token', '=', $token)->first();
         if ($booking === null) {
             $booking = BookingRepository::make($tour);
             $booking->save();
-            return redirect()->route('booking.v3.guest', ['tour' => $tour->booking_form_url, 'booking' => $booking->token]);
+            $this->setupCookie($tour, $booking);
+            return redirect()->route('booking.v3.guest', ['tour' => $tour->booking_form_url, ]);
         }
+        $this->setupCookie($tour, $booking);
         return view('pages.customer.booking.v3.guest', ['tour' => $tour, 'booking' => $booking]);
     }
 
-    public function hotel(string $tour, string|null $token = null)
+    public function hotel(Request $request, string $tour, string|null $token = null)
     {
         $tour = Tour::where('booking_form_url', '=', $tour)->firstOrFail();
+        $token = $token ?? $request->cookie($tour->booking_form_url);
         $booking = Booking::where('tour_id', '=', $tour->id)->where('token', '=', $token)->firstOrFail();
         if ($booking === null) {
             return redirect()->route('booking.v3.guest', ['tour' => $tour->booking_form_url, 'booking' => null]);
         }
+        $this->setupCookie($tour, $booking);
         return view('pages.customer.booking.v3.hotel', ['tour' => $tour, 'booking' => $booking]);
     }
 
-    public function ticket(string $tour, string|null $token = null)
+    public function ticket(Request $request, string $tour, string|null $token = null)
     {
         $tour = Tour::where('booking_form_url', '=', $tour)->firstOrFail();
+        $token = $token ?? $request->cookie($tour->booking_form_url);
         $booking = Booking::where('tour_id', '=', $tour->id)->where('token', '=', $token)->firstOrFail();
         $quote = Quote::find($booking->quote_id);
         if ($booking === null) {
             return redirect()->route('booking.v3.guest', ['tour' => $tour->booking_form_url, 'booking' => null]);
         }
+        $this->setupCookie($tour, $booking);
         return view('pages.customer.booking.v3.ticket', ['tour' => $tour, 'booking' => $booking, 'quote' => $quote]);
+    }
+
+    private function setupCookie(Tour $tour, Booking $booking): void
+    {
+        // Place cookie for 12hrs
+        Cookie::queue(Cookie::make($tour->booking_form_url, $booking->token, 12 * 60));
     }
 }
