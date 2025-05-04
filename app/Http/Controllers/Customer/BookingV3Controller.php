86fe68f4aaf1444b9ff1a9aa294ee964f1e29c66
Celeste Gateley <celeste.gateley@gmail.com>
2025-05-04 20:53:58 +0100

    Switched setup method to include all included components on setup

--- app/Http/Controllers/Customer/BookingV3Controller.php
+++ app/Http/Controllers/Customer/BookingV3Controller.php
@@ -17,7 +17,7 @@ class BookingV3Controller extends Controller
         $tour = Tour::where('booking_form_url', '=', $tour)->firstOrFail();
         $booking = Booking::where('tour_id', '=', $tour->id)->where('token', '=', $booking ?? $request->cookie($tour->booking_form_url))->first();
         if ($booking === null) {
-            $booking = BookingRepository::setupBooking($tour);
+            $booking = BookingRepository::make($tour);
             $this->setupCookie($tour, $booking);
             return redirect()->route('booking.v3.guest', ['tour' => $tour->booking_form_url, 'booking' => $booking->token]);
         }
