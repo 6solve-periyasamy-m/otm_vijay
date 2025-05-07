091080b3a6df7515fd1580a3788c682bb28d5391
Celeste Gateley <celeste.gateley@gmail.com>
2025-05-07 13:20:34 +0100

    Rounded values on booking form to nearest 5

--- app/Http/Livewire/Abstract/V3BookingComponent.php
+++ app/Http/Livewire/Abstract/V3BookingComponent.php
@@ -176,7 +176,7 @@ abstract class V3BookingComponent extends Component
 
     public function formatCurrency(float|int $value): string
     {
-        return fr_currency($value * $this->getFXRate(), $this->getCurrency(), true) . " " . $this->getCurrency()->code;
+        return fr_currency(round_to_five($value * $this->getFXRate()), $this->getCurrency(), true) . " " . $this->getCurrency()->code;
     }
 
     public function hasActivity(ActivityInventoryTour $tourComponent): bool
