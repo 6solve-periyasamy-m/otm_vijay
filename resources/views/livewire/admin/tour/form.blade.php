0cf4a6bb7de239492b65c6e47a66082acec7ad3e
Celeste Gateley <celeste.gateley@gmail.com>
2025-05-15 11:55:56 +0100

    Moved additional costs up

--- resources/views/livewire/admin/tour/form.blade.php
+++ resources/views/livewire/admin/tour/form.blade.php
@@ -42,6 +42,36 @@ $atol = [
             </div>
         </x-admin.section.card>
     </div>
+    <div class="col-xl-12">
+        <x-admin.section.card>
+            <div class="d-flex justify-content-between">
+                <div>
+                    <h4 class="fw-bold">
+                        Additional Costs
+                    </h4>
+                </div>
+                <div>
+                    <button wire:click="addCost" class="btn btn-success">{{ Icon::plus() }} Add Cost</button>
+                </div>
+            </div>
+        </x-admin.section.card>
+        <div class="row">
+            @foreach($costs as $key => $cost)
+                <div class="col-xl-4">
+                    <x-admin.section.card>
+                        <div class="row">
+                            <x-livewire.input wire:model="costs.{{$key}}.name" label="Name" width="3" />
+                            <x-livewire.input wire:model="costs.{{$key}}.amount" label="Amount" width="3" />
+                            <x-livewire.input.checkbox wire:model="costs.{{$key}}.per_customer" label="Per Customer" width="3" />
+                            <div class="col-3">
+                                <button class="btn btn-danger" wire:click="removeCost({{$key}})" title="Remove">{{Icon::trash()}}</button>
+                            </div>
+                        </div>
+                    </x-admin.section.card>
+                </div>
+            @endforeach
+        </div>
+    </div>
     <div class="col-xl-4">
         <x-admin.section.card>
             <div class="row">
@@ -76,36 +106,6 @@ $atol = [
             <x-livewire.input.select.large-text-template name="footerTemplate" label="Copy from Template" value="{{ $footerTemplate }}" />
             <x-livewire.ckeditor name="tour.invoice_footer" value="{{ $tour?->invoice_footer }}" label="Invoice Footer" />
         </x-admin.section.card>
-    </div>
-    <div class="col-xl-12">
-        <x-admin.section.card>
-            <div class="d-flex justify-content-between">
-                <div>
-                    <h4 class="fw-bold">
-                        Additional Costs
-                    </h4>
-                </div>
-                <div>
-                    <button wire:click="addCost" class="btn btn-success">{{ Icon::plus() }} Add Cost</button>
-                </div>
-            </div>
-        </x-admin.section.card>
-        <div class="row">
-            @foreach($costs as $key => $cost)
-                <div class="col-xl-4">
-                    <x-admin.section.card>
-                        <div class="row">
-                            <x-livewire.input wire:model="costs.{{$key}}.name" label="Name" width="3" />
-                            <x-livewire.input wire:model="costs.{{$key}}.amount" label="Amount" width="3" />
-                            <x-livewire.input.checkbox wire:model="costs.{{$key}}.per_customer" label="Per Customer" width="3" />
-                            <div class="col-3">
-                                <button class="btn btn-danger" wire:click="removeCost({{$key}})" title="Remove">{{Icon::trash()}}</button>
-                            </div>
-                        </div>
-                    </x-admin.section.card>
-                </div>
-            @endforeach
-        </div>
     </div>
     <div class="col-xl-12">
         <x-admin.section.card>
