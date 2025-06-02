2f5913f4c9590f5a40021312e485db359bd637b0
balaji-6solve <balaji-6solve@gmail.com>
2025-06-02 12:56:07 +0530

    Updated the steps navigation for 'Has Inclusions' based flow.

--- app/Http/Livewire/Customer/Booking/V3/Ticket.php
+++ app/Http/Livewire/Customer/Booking/V3/Ticket.php
@@ -37,7 +37,11 @@ class Ticket extends V3BookingComponent
     public function advance()
     {
         $this->booking->updateBookingProgressNotification('Tickets');
-        return redirect()->route('booking.v3.inclusions', ['tour' => $this->tour->booking_form_url, 'booking' => $this->booking->token]);
+        if ($this->hasInclusions()) {
+            return redirect()->route('booking.v3.inclusions', ['tour' => $this->tour->booking_form_url, 'booking' => $this->booking->token]);
+        } else {
+            return redirect()->route('booking.v3.details', ['tour' => $this->tour->booking_form_url, 'booking' => $this->booking->token]);
+        }
     }
 
     public function render()
