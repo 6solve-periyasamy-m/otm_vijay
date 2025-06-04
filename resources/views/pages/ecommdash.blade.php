4321799b809876ce8792d02ce169598a55371e4e
balaji-6solve <balaji-6solve@gmail.com>
2025-06-04 15:06:46 +0530

    Added the export option for Abandoned Bookings

--- resources/views/pages/ecommdash.blade.php
+++ resources/views/pages/ecommdash.blade.php
@@ -2,6 +2,7 @@
 @section('title', 'eCommerce Dashboard')
 @section('content')
 @php $xlsxExport = route('reports.booking-orders.export', ['extension' => 'xlsx']); @endphp
+@php $xlsxAbandonedExport = route('reports.abandoned-bookings.export', ['extension' => 'xlsx']); @endphp
     <div class="row">
 
         <div class="col-xl-12">
@@ -27,7 +28,17 @@
         <div class="col-xl-12">
             <x-admin.section.card>
                 <x-slot:title>
-                    Abandoned Bookings
+                    <div class="flex justify-between items-center">
+                        <div class="text-left">
+                            Abandoned Bookings
+                        </div>
+                        <div class="text-right">
+                            <a class="btn btn-success float-end" style="margin-left: 5px;" href="{{ $xlsxAbandonedExport }}">
+                                {{ Icon::create() }}
+                                <span>Export as Excel</span>
+                            </a>
+                        </div>
+                    </div>
                 </x-slot:title>
                 @include('partials.reports.tables.abandoned-bookings', ['data' => \App\Repository\Reporting\ReportRepository::getAbandonedBookingsReport(7, true)])
             </x-admin.section.card>
