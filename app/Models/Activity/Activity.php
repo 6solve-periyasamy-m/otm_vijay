7984725a28548f81e9dbfb45fbbc7af9df7c6846
Celeste Gateley <celeste.gateley@gmail.com>
2025-07-07 15:11:06 +0100

    Added archived fields to component tables

--- app/Models/Activity/Activity.php
+++ app/Models/Activity/Activity.php
@@ -36,6 +36,7 @@ use Staudenmeir\EloquentHasManyDeep\HasRelationships;
  * @property ActivityCategory $activity_category
  * @property int|null $currency_id
  * @property int|null $event_id
+ * @property boolean $archived
  * @property int|null $session_id
  * @property int|null $seating_id
  * @property string|null $name
@@ -81,7 +82,7 @@ class Activity extends Model
 
     protected $guarded = [];
     protected array $cascadeDeletes = ['activityInventory'];
-    protected $casts = ['activity_category' => ActivityCategory::class,];
+    protected $casts = ['activity_category' => ActivityCategory::class,, 'archived' => 'boolean'];
 
     public static function getValidationRules(): array
     {
