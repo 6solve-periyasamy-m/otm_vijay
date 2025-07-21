aed5d96b044cdbcc7673c4d2807ab2a7882aa88f
Celeste Gateley <celeste.gateley@gmail.com>
2025-07-21 12:56:35 +0100

    Fix for date formatting

--- app/Imports/Inventory/ActivityOverrideImport.php
+++ app/Imports/Inventory/ActivityOverrideImport.php
@@ -5,7 +5,6 @@ namespace App\Imports\Inventory;
 use App\Models\Activity\Activity;
 use App\Models\Activity\ActivityInventory;
 use App\Models\Activity\TicketType;
-use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Validation\Rule;
 use Maatwebsite\Excel\Concerns\Importable;
@@ -36,8 +35,8 @@ class ActivityOverrideImport implements ToCollection, WithHeadingRow, WithValida
             }
             $inventory->update([
                 'description' => trim($row['description']),
-                'starts_at' => Carbon::createFromFormat('d-m-Y H:i', trim($row['start'])),
-                'ends_at' => Carbon::createFromFormat('d-m-Y H:i', trim($row['end'])),
+                'starts_at' => get_date(trim($row['start'])),
+                'ends_at' => get_date(trim($row['end'])),
                 'fit_selectable' => (((trim($row['fit'] ?? "") === 'TRUE') || (trim($row['fit'] ?? "")) === 'YES')),
                 'ticket_type_id' => $ticketType,
                 'stock' => empty(trim($row['stock'] ?? "")) ? 0 : trim($row['stock']),
@@ -56,8 +55,8 @@ class ActivityOverrideImport implements ToCollection, WithHeadingRow, WithValida
         return [
             'id' => 'nullable|int|exists:activity_inventories,id',
             'description' => 'nullable|string',
-            'start' => 'nullable|date_format:d-m-Y H:i',
-            'end' => 'nullable|date_format:d-m-Y H:i',
+            'start' => 'nullable|date',
+            'end' => 'nullable|date',
             'fit' => ['nullable', Rule::in(['YES', 'TRUE', 'FALSE', 'NO'])],
             'ticket_type' => 'required|string|exists:ticket_types,name',
             'stock' => 'nullable|int',
