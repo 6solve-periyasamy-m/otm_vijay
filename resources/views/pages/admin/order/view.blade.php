dd3d4b0de2f55a71a26d6351e75caf36fcfdd7e0
Celeste Gateley <celeste.gateley@gmail.com>
2025-07-28 14:09:37 +0100

    Fix for null error with conversion rates

--- resources/views/pages/admin/order/view.blade.php
+++ resources/views/pages/admin/order/view.blade.php
@@ -5,7 +5,7 @@
 /** @var bool $nonSystem Is the order using a non-system currency */
 $nonSystem = $order->currency !== null && $order->currency !== Settings::currency();
 /** @var float|null $fromSystem conversion rate from system currency */
-$fromSystem = \Settings::getConversionRate(\Settings::currency(), $order->currency);;
+$fromSystem = \Settings::getConversionRate(\Settings::currency(), $order->currency);
 /** @var float|null $toSystem conversion rate to system currency */
 $toSystem = \Settings::getConversionRate($order->currency, \Settings::currency());
 @endphp
