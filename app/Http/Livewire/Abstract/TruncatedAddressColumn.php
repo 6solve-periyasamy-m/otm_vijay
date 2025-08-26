aeda8e979c1542458d86c2a1114c6a04cede0754
Celeste Gateley <celeste.gateley@gmail.com>
2025-08-26 11:31:55 +0100

    Updated transport table to properly show address name

---
+++ app/Http/Livewire/Abstract/TruncatedAddressColumn.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Livewire\Abstract;
+
+use App\Models\Location\Country;
+use Illuminate\Support\Collection;
+use Mediconesystems\LivewireDatatables\Column;
+
+class TruncatedAddressColumn extends Column
+{
+    public static function table(string $table, string|null $country = null): TruncatedAddressColumn
+    {
+        $country = $country ?? $table . '_country';
+        return parent::callback(
+            [
+                "$table.name",
+                "$country.name",
+                "$table.postcode",
+            ], 
+            static function (...$lines) {
+                $address = "";
+                foreach ($lines as $line) {
+                    if (empty($line)) { continue; }
+                    if (!empty($address)) { $address .= ", "; }
+                    $address .= $line;
+                }
+                return $address;
+        })->filterable(self::pluckCountries())->filterOn("$country.name");
+    }
+
+    private static function pluckCountries(): Collection
+    {
+        return Country::orderBy('priority', 'desc')->orderBy('name')->pluck('name');
+    }
+
+    public static function country(string $country): Column
+    {
+        return Column::name("$country.name")->filterable(self::pluckCountries());
+    }
+}
