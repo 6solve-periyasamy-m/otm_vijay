ddf3629574735a5eaca03f5fec3d3346e165a309
balaji-6solve <balaji.r@corp.6solve.com>
2025-09-03 15:11:13 +0530

    UI Updated – Fields rearranged and validation labels updated with error messages.

--- resources/views/pages/admin/activity/form.blade.php
+++ resources/views/pages/admin/activity/form.blade.php
@@ -21,13 +21,13 @@
 	<div class="form-group col-xl-4">
         @include('partials.fields.dropdown', ['name' => 'Activity Category', 'field' => 'activity_category', 'selected' => $activity?->activity_category->value, 'values' => \App\Models\Helper\Enum\ActivityCategory::toArray(),])
     </div>
-	<x-livewire.ckeditor name="description" value="{{ $activity?->description }}" label="Description" />
-	@include('partials.fields.textarea', ['name' => 'Internal Notes', 'field' => 'notes', 'value' => $activity?->internal_notes])
-	<x-livewire.input name="field1" value="{{ $activity?->field1 }}" label="Label 1" width="6" />
-    <x-livewire.input name="field2" value="{{ $activity?->field2 }}" label="Label 2" width="6" />
     <x-livewire.input.select.activity.seating name="seating_id" value="{{ $activity?->seating_id }}" label="Seating" width="4" clear />
     <x-livewire.input.select.activity.session name="session_id" value="{{ $activity?->session_id }}" label="Session" width="4" clear />
     @include('partials.fields.prefab.addresses.switcher', ['address' => $activity?->address,])
+    <x-livewire.ckeditor name="description" value="{{ $activity?->description }}" label="Description" />
+	@include('partials.fields.textarea', ['name' => 'Internal Notes', 'field' => 'notes', 'value' => $activity?->internal_notes])
+    <x-livewire.input name="field1" value="{{ $activity?->field1 }}" label="Label 1" width="6" />
+    <x-livewire.input name="field2" value="{{ $activity?->field2 }}" label="Label 2" width="6" />
 	@include('partials.fields.file', ['name' => 'Image', 'field' => 'image', 'width' => 3, 'value' => $activity?->image_url,])
     
     @include('partials.fields.submit')
