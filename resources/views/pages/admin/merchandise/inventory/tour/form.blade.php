5d1d78cb884d6c1b5d78c9ccce23949b628cde67
Celeste Gateley <celeste.gateley@gmail.com>
2025-09-18 12:37:41 +0100

    Added document order to inventory tour forms

--- resources/views/pages/admin/merchandise/inventory/tour/form.blade.php
+++ resources/views/pages/admin/merchandise/inventory/tour/form.blade.php
@@ -24,8 +24,9 @@
     @include('partials.fields.text',
         ['name' => 'Tour Sales Price', 'field' => 'tour_sales_price', 'value' => $inventoryTour?->tour_sales_price ?? null, 'width' => 6,])
     @include('partials.fields.checkbox',
-        ['name' => 'Bookable?', 'field' => 'is_bookable', 'value' => $inventoryTour?->is_bookable ?? false, ])
+        ['name' => 'Bookable?', 'field' => 'is_bookable', 'value' => $inventoryTour?->is_bookable ?? false, 'width' => 4,])
     @include('partials.fields.checkbox',
-        ['name' => 'Stock Controlled?', 'field' => 'stock_control_active', 'value' => $inventoryTour?->stock_control_active ?? false, ])
+        ['name' => 'Stock Controlled?', 'field' => 'stock_control_active', 'value' => $inventoryTour?->stock_control_active ?? false, 'width' => 4,])
+    @include('partials.fields.text', ['name' => 'document_order', 'field' => 'order', 'value' => $inventoryTour->document_order, 'width' => 4])
     @include('partials.fields.submit')
 @endsection
