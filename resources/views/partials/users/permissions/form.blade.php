57779588f979cb97142ec5723b71b4c67538d519
balaji-6solve <balaji.r@corp.6solve.com>
2025-10-10 11:46:50 +0530

    Updated style for the freeze top heading on roles page

--- resources/views/partials/users/permissions/form.blade.php
+++ resources/views/partials/users/permissions/form.blade.php
@@ -1,3 +1,9 @@
+<style>
+    .permissions-table-wrapper { max-height: 600px; overflow-y: auto; }
+    .permissions-table thead th {position: sticky; top: 0;background-color: #ced4da; color: #212529; z-index: 2; box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4); }
+</style>
+
+
 @include('partials.fields.text', ['name' => 'Name', 'field' => 'title', 'value' => $title ?? null, 'width' => 5,])
 @include('partials.fields.text', ['name' => 'Level', 'field' => 'level', 'value' => $level ?? null, 'width' => 5,])
 <div class="form-group col-12 col-xl-2">
@@ -6,37 +12,39 @@
 </div>
 
 <div class="mx-auto w-100 border text-center" style="margin-bottom: 4px;">
-    <table class="table table-striped">
-        <thead class="thead-dark">
-        <tr >
-            <th scope="col">Section</th>
-            <th scope="col">View</th>
-            <th scope="col">Create</th>
-            <th scope="col">Edit</th>
-            <th scope="col">Self-Edit</th>
-            <th scope="col">Delete</th>
-            <th scope="col">Self-Delete</th>
-            <th scope="col">Costing</th>
-            <th scope="col">All</th>
-        </tr>
-        </thead>
-        <tbody>
-            @foreach($permissions as $group => $classes)
-                <tr>
-                    <td colspan="8"><h4 style="text-decoration: underline">{{ $group }}</h4></td>
-                    <td>
-                        <div class="form-group">
-                            <input type="checkbox" name="{{ str_replace(' ', '', $group).'Group' }}-all"
-                                   class="form-check-input" id="{{ str_replace(' ', '', $group).'Group' }}-all"
-                                   onchange="multiChanger('{{ str_replace(' ', '', $group).'Group' }}-all', '{{ str_replace(' ', '', $group).'Group' }}')">
-                        </div>
-                    </td>
-                </tr>
-                @foreach($classes as $class => $data)
-                    @include('partials.users.permissions.form.row',
-                                ['field' => $class, 'data' => $data, 'group' => str_replace(' ', '', $group).'Group'])
+    <div class="permissions-table-wrapper">
+        <table class="table table-striped permissions-table">
+            <thead class="thead-dark">
+            <tr >
+                <th scope="col">Section</th>
+                <th scope="col">View</th>
+                <th scope="col">Create</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Self-Edit</th>
+                <th scope="col">Delete</th>
+                <th scope="col">Self-Delete</th>
+                <th scope="col">Costing</th>
+                <th scope="col">All</th>
+            </tr>
+            </thead>
+            <tbody>
+                @foreach($permissions as $group => $classes)
+                    <tr>
+                        <td colspan="8"><h4 style="text-decoration: underline">{{ $group }}</h4></td>
+                        <td>
+                            <div class="form-group">
+                                <input type="checkbox" name="{{ str_replace(' ', '', $group).'Group' }}-all"
+                                    class="form-check-input" id="{{ str_replace(' ', '', $group).'Group' }}-all"
+                                    onchange="multiChanger('{{ str_replace(' ', '', $group).'Group' }}-all', '{{ str_replace(' ', '', $group).'Group' }}')">
+                            </div>
+                        </td>
+                    </tr>
+                    @foreach($classes as $class => $data)
+                        @include('partials.users.permissions.form.row',
+                                    ['field' => $class, 'data' => $data, 'group' => str_replace(' ', '', $group).'Group'])
+                    @endforeach
                 @endforeach
-            @endforeach
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    </div>
 </div>
