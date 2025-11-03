945edab4c88f07b3d9a2ca7a6c61fcff2ad84051
balaji-6solve <balaji.r@corp.6solve.com>
2025-11-03 13:55:33 +0530

    Updated the changes

--- resources/views/pdf/invoices/itinerary.blade.php
+++ resources/views/pdf/invoices/itinerary.blade.php
@@ -276,7 +276,7 @@
                     return $flights->first();
                 });
             @endphp
-            @foreach($itinerary->items['Flights'] as $flight)
+            @foreach($groupedFlights as $flight)
                 @if(isset($flight->details['Quantity']) && $flight->details['Quantity'] > 0)
                 <div class="single-module mb-n15 component-break">
                     @if($firstLoop)
