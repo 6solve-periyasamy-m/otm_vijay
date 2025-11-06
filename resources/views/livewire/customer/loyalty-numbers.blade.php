929f4d051579e7320102f3463dfe3daca2eb1534
balaji-6solve <balaji.r@corp.6solve.com>
2025-11-06 22:39:33 +0530

    Updated the loyalty image

--- resources/views/livewire/customer/loyalty-numbers.blade.php
+++ resources/views/livewire/customer/loyalty-numbers.blade.php
@@ -52,7 +52,7 @@
 
     <div class="mt-3">
         <div wire:click="save" class="btn d-inline-flex align-items-center gap-2 btn-bg-border" role="button" tabindex="0" title="Update Loyalties" aria-label="Update Loyalties" style="border:1px solid #F35B15; border-color: #F35B15; height: 32px; background-color: #F9F4EE;">
-            <img  src="{{ asset('images/customer/images/merchandise.png') }}"   class="reservation_wht_icn"   alt="Update Loyalties" />
+            <img  src="{{ asset('images/customer/images/loyalty-program.png') }}"   class="reservation_wht_icn"   alt="Update Loyalties" />
             <span>Update Loyalties</span>
         </div>
     </div>
