eb0fc86db69de52e0ccdc86c75d4d738bf4f1b92
balaji-6solve <balaji.r@corp.6solve.com>
2025-11-06 11:47:24 +0530

    Updated the date format

--- resources/views/pages/customer/finances.blade.php
+++ resources/views/pages/customer/finances.blade.php
@@ -47,9 +47,9 @@
                                              <h6 class="btn btn-{{ $order->status->color() }} fw-bold">{{ $order->status->description() }}</h6>
                                             <h4>{{ $order->tour?->event?->name}}</h4>
                                             <!-- <h6>Lead Guest : {{$order->leadBooker->customer->first_name ?? '' . ' ' .$order->leadBooker->customer->last_name ?? ''}}</h6> -->
-                                             <h6 class="lead_guest">Lead Guest : {{ ($order->leadBooker->customer->first_name ?? '') . ' ' . ($order->leadBooker->customer->last_name ?? '') }}</h6>
-                                            <!-- <h6>Booking Reference : {{ $order->booking_reference }}</h6> -->
                                             <p class="calendar_date"><img src="{{ asset('images/customer/images/calendar.svg') }}" />{{ Carbon::parse($order->tour->date_from)->format('d M Y') }}  - {{ Carbon::parse($order->tour->date_to)->format('d M Y')}}</p>
+                                            <h6 class="lead_guest">Lead Guest : {{ ($order->leadBooker->customer->first_name ?? '') . ' ' . ($order->leadBooker->customer->last_name ?? '') }}</h6>
+                                            <!-- <h6>Booking Reference : {{ $order->booking_reference }}</h6> -->                                            
                                     </div> 
                                 </div>
                             </div>
