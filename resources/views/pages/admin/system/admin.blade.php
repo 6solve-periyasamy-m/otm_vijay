2e92da0db6672ad8ab0efda8646b927613041000
Celeste Gateley <celeste.gateley@gmail.com>
2025-11-19 11:26:02 +0000

    Moved attributes into admin panel

--- resources/views/pages/admin/system/admin.blade.php
+++ resources/views/pages/admin/system/admin.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'System Configuration')
 
 @section('content')
+    @include('partials.admin.system.attributes')
     @can('read', \App\Models\User::class)
         @include('partials.admin.system.users')
         @include('partials.admin.system.roles')
