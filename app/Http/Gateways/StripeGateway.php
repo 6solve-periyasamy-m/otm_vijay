5d73fe9a977e7f236667f3c5bd9ea4dfb4632537
Celeste Gateley <celeste.gateley@gmail.com>
2025-11-21 15:39:23 +0000

    Implemented using different API keys for different currencies

--- app/Http/Gateways/StripeGateway.php
+++ app/Http/Gateways/StripeGateway.php
@@ -7,7 +7,8 @@ use App\Models\Booking\BookingTraveller;
 use App\Models\Customer\Customer;
 use App\Models\Order\Order;
 use App\Models\Order\Payment\PaymentIntention;
-use Stripe\Checkout\Session;
+use Stripe\Exception\ApiErrorException;
+use Stripe\StripeClient;
 
 class StripeGateway extends Gateway implements SupportsRedirect
 {
@@ -22,16 +23,22 @@ class StripeGateway extends Gateway implements SupportsRedirect
 
     /**
      * @inheritDoc
+     * @throws ApiErrorException
+     * @noinspection PhpArrayKeyDoesNotMatchArrayShapeInspection
      */
     public function getRedirect(array $items, PaymentIntention $intention, Customer|BookingTraveller $customer, string $success = null): string
     {
         $currency =
             (($intention->getRelatedModel() instanceof Order) ? $intention->getRelatedModel()?->currency?->code : null) ?? config('app.currency');
+        $currencyKeys = config('app.gateways.stripe.currencies.' . strtoupper($currency), []);
+        $secret = $currencyKeys['secret'] ?? config('app.gateways.stripe.secret');
         $currency = strtoupper($currency);
         $lineItems = [];
         foreach ($items as $item) { $lineItems[] = $item->toStripe($currency); }
 
-        $session = Session::create([
+        $stripe = new StripeClient($secret);
+
+        $session = $stripe->checkout->sessions->create([
             'line_items' => $lineItems,
             'mode' => 'payment',
             'payment_intent_data' => [
