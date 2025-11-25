8a7cd0f182aec9e44f2ae4c9baad467b0eb6ab6f
Celeste Gateley <celeste.gateley@gmail.com>
2025-11-25 22:31:00 +0000

    Fix for 500 error

--- resources/views/partials/reports/tables/bulk-reminders.blade.php
+++ resources/views/partials/reports/tables/bulk-reminders.blade.php
@@ -13,9 +13,8 @@
     </thead>
     <tbody>
     @foreach($orders as $order)
-        @php $depositDue = $order->calculated_deposit < $order->paid; @endphp
-        @php $next = $order->repository->getNextPaymentDetails() @endphp
-        @continue($depositDue !== false && ($next === null || $next->remaining < setting('order.reminders.minimum', 1.0)))
+        @php $next = $order->repository->getDepositInstallmentForReminder() ?? $order->repository->getNextPaymentDetails() @endphp
+        @continue($next === null || $next->remaining < setting('order.reminders.minimum', 1.0))
         <tr order_id="{{ $order->id }}">
             <th scope="row">
                 <a href="{{ route('orders.view', ['order' => $order,]) }}">
