1c254e619b91c33003963bff80b55ccd0c923040
balaji-6solve <balaji.r@corp.6solve.com>
2026-01-23 13:56:37 +0530

    Added the travellers by ticket count

--- app/Repository/Model/Booking/BookingRepository.php
+++ app/Repository/Model/Booking/BookingRepository.php
@@ -1006,12 +1006,12 @@ class BookingRepository extends ModelRepository implements GeneratesFellohData
         $currentCount = $this->booking->travellers()->count();
         $difference = $targetCount - $currentCount;
         DB::transaction(function () use ($difference) {
-            if ($difference > 0) {
+            if ($difference > 1) {
                 for ($i = 0; $i < $difference; $i++) {
                     $this->addUnknownTraveller();
                 }
             }
-            if ($difference < 0) {
+            if ($difference < 1) {
                 for ($i = 0; $i < abs($difference); $i++) {
                     $this->removeUnknownTraveller();
                 }
