236f2b07912f46b215990115f3cf78c76595210d
balaji-6solve <balaji.r@corp.6solve.com>
2026-01-26 16:12:36 +0530

    Updted the request components in tickets

--- app/Http/Controllers/Api/Customer/BookingController.php
+++ app/Http/Controllers/Api/Customer/BookingController.php
@@ -103,7 +103,10 @@ class BookingController extends ApiController
         }
         $booking = $request->getBooking();
         try {
-            $components = $request->input('components');
+            $components = $request->get('components');
+            if (!$components) {
+                $components = $request->json('components');
+            }
             $booking->repository->processComponentsFromApi($components);
         } catch (BookingApiException $e) {
             return response()->json(['success' => false, 'error' => $e->getErrorCode(), 'message' => $e->getMessage()], 400);
