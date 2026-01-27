29a7968dcbdbbbc0742892776751a5db3565adc0
Celeste Gateley <celeste.gateley@gmail.com>
2026-01-27 09:46:46 +0000

    Fix for exchange rates being in the wrong order

--- app/Http/Livewire/Admin/Quote/Calculator.php
+++ app/Http/Livewire/Admin/Quote/Calculator.php
@@ -53,8 +53,8 @@ class Calculator extends Component
         $this->quote->repository->recache();
         $this->paying = $this->quote->paying ?? 0;
         $this->travelling = $this->quote->travelling ?? 0;
-        $this->fromRate = $this->quote->from_rate ?? Settings::getConversionRate($this->quote->currency, Settings::currency(), true) ?? Settings::getConversionRate($this->quote->currency, Settings::currency()) ?? 1;
-        $this->toRate = $this->quote->to_rate ?? Settings::getConversionRate(Settings::currency(), $this->quote->currency) ?? 1;
+        $this->fromRate = $this->quote->from_rate ?? Settings::getConversionRate(Settings::currency(), $this->quote->currency, true) ?? Settings::getConversionRate(Settings::currency(), $this->quote->currency) ?? 1;
+        $this->toRate = $this->quote->to_rate ?? Settings::getConversionRate($this->quote->currency, Settings::currency()) ?? 1;
         $this->calculate(false);
     }
 
