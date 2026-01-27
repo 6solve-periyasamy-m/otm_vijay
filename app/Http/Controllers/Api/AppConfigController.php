432f10de70bdc09efaee10058d36054442730aa1
balaji-6solve <balaji.r@corp.6solve.com>
2026-01-27 17:01:49 +0530

    Updated the public key into the config api

--- app/Http/Controllers/Api/AppConfigController.php
+++ app/Http/Controllers/Api/AppConfigController.php
@@ -36,8 +36,8 @@ class AppConfigController extends ApiController
             ],
 
             'stripe' => [
-                'publishable_aud' => config('app.gateways.stripe.currencies.aud.client', config('app.gateways.stripe.publishable')),
-                'publishable_usd' => config('app.gateways.stripe.currencies.usd.client', config('app.gateways.stripe.publishable')),
+                'publishable_aud' => config('app.gateways.stripe.currencies.AUD.client', config('app.gateways.stripe.publishable')),
+                'publishable_usd' => config('app.gateways.stripe.currencies.USD.client', config('app.gateways.stripe.publishable')),
             ],
 
             /*'contact' => $this->contactDetails($defaultCurrency), */
