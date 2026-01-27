d166828409a4ba7a8d573172abba413156771bde
Celeste Gateley <celeste.gateley@gmail.com>
2026-01-27 19:59:20 +0000

    Fix for quote conversion not correctly calculating

--- app/Http/Livewire/Admin/Quote/Calculator.php
+++ app/Http/Livewire/Admin/Quote/Calculator.php
@@ -55,8 +55,8 @@ class Calculator extends Component
         $this->quote->repository->recache();
         $this->paying = $this->quote->paying ?? 0;
         $this->travelling = $this->quote->travelling ?? 0;
-        $this->fromRate = $this->quote->from_rate ?? Settings::getConversionRate($this->quote->currency, Settings::currency(), true) ?? Settings::getConversionRate($this->quote->currency, Settings::currency()) ?? 1;
-        $this->toRate = $this->quote->to_rate ?? Settings::getConversionRate(Settings::currency(), $this->quote->currency, true) ?? Settings::getConversionRate(Settings::currency(), $this->quote->currency, true) ?? 1;
+        $this->fromRate = $this->quote->repository->getFromRate();
+        $this->toRate = $this->quote->repository->getToRate();
         $this->calculate(false);
     }
 
